67e8436c3249a1f361188f607ec84d8973abcaa4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-09 21:12:55 +0800

    Time: 160 ms (41.66%), Space: 47.6 MB (61.91%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const set = new Set();
+    let [start, end, maxLen] = [0, 0, 0];
+    
+    while (end < s.length) {
+        set.has(s[end]) ? set.delete(s[start++]) : set.add(s[end++]);
+        
+        maxLen = Math.max(maxLen, set.size);
+    }
+    
+    return maxLen;
+};
