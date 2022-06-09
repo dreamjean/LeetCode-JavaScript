df85b117a70df8f96d7468eb3020dedc14b73478
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-09 16:03:31 +0800

    Time: 114 ms (51.46%), Space: 43.1 MB (28.76%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        
+        if (diff in map) return [i, map[diff]];
+        
+        map[nums[i]] = i;
+    }
+    
+    return [];
+};
