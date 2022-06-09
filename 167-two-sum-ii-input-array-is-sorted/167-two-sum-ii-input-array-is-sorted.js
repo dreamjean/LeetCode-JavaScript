0fa028f5239534d70507633d595ecce4948c018f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-09 19:10:29 +0800

    Time: 102 ms (27.36%), Space: 43.1 MB (40.98%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let [left, right] = [0, numbers.length - 1];
+    
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) return [left + 1, right + 1];
+        
+        sum > target ? right-- : left++;
+    }
+    
+    return [];
+};
