b6101aa62b07494a2363a3bdb03a3f752df46476
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-09 20:58:47 +0800

    Time: 207 ms (14.01%), Space: 47.3 MB (62.76%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -16,13 +16,12 @@ var addTwoNumbers = function(l1, l2) {
     
     while (l1 || l2) {
         let sum = 0;
-        sum += (l1?.val || 0) + (l2?.val || 0);
-        l1 = l1?.next || null;
-        l2 = l2?.next || null;
-        
-        sum += carry;
+        sum += (l1?.val || 0) + (l2?.val || 0) + carry;
         curr.next = new ListNode(sum % 10);
         carry = Math.floor(sum / 10);
+        
+        l1 = l1?.next;
+        l2 = l2?.next;
         curr = curr.next;
     }
     
