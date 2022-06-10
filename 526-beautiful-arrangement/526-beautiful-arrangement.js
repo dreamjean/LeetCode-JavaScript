b7f67b49e96a52c807677cbd0b0e8b814337dfa9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-11 02:27:56 +0800

    Time: 290 ms (31.08%), Space: 42.4 MB (58.11%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countArrangement = function(n) {
+    const visited = new Array(n + 1).fill(false);
+    let count = 0;
+    
+    const backtrack = (num = 1) => {
+        if (num > n) return count++;
+        
+        for (let i = 1; i <= n; i++) {
+            if (!visited[i] && (!(num % i) || !(i % num))) {
+                visited[i] = true;
+                backtrack(num + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    backtrack();
+    
+    return count;
+};
