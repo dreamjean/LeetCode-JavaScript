e542907867f606e4b9c1a99b3a605bdee62ab77e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-14 03:45:26 +0800

    Time: 109 ms (77.09%), Space: 48.8 MB (38.80%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var findDiagonalOrder = function(mat) {
+    const [m, n] = [mat.length, mat[0].length];
+    const nums = [];
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            nums[i + j] ? nums[i + j].push(mat[i][j]) : nums[i + j] = [mat[i][j]];
+        }
+    }
+    
+    return nums.reduce((arr, curr, i) => {
+        !(i % 2) ? arr.push(...curr.reverse()) : arr.push(...curr);
+        
+        return arr;
+    }, [])
+};
