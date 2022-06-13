21abac53ffa8fe0175ee0310729b2ea068ec5031
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-14 02:44:25 +0800

    Time: 103 ms (17.95%), Space: 42.4 MB (31.30%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let [i, l, r] = [0, 0, nums.length - 1];
+    
+    while (i <= r) {
+        if (!nums[i]) [nums[i++], nums[l++]] = [nums[l], nums[i]];
+        else if (nums[i] === 2) [nums[i], nums[r--]] = [nums[r], nums[i]];        
+        else i++;                                    
+    }                                                                                                                                                        
+};
