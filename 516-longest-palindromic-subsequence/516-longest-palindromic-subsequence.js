7c2ff7fad23d2f9e94e6d940e6ab83189be9a734
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-15 02:52:52 +0800

    Time: 146 ms (98.31%), Space: 42 MB (98.99%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s) {
+    const n = s.length;
+    const dp = new Array(n).fill(0);
+    
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i] = 1;
+        let prev = 0;
+        for (let j = i + 1; j < n; j++) {
+            let tmp = dp[j];
+            dp[j] = Math.max(dp[j], dp[j - 1], prev + (s[i] === s[j] ? 2 : 0));
+            prev = tmp;
+        }
+    }
+    
+    return dp[n - 1];
+};
