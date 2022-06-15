e9499b7eac90d001fcc4fda3869f685968b2b660
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-16 02:45:34 +0800

    Time: 164 ms (81.74%), Space: 55.3 MB (81.74%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @param {number[][]} moveCost
+ * @return {number}
+ */
+var minPathCost = function(grid, moveCost) {
+    const [m, n] = [grid.length, grid[0].length];
+    const visited = Array.from({ length: m }, () => new Array(n).fill(Number.MAX_VALUE));
+    const pq = new MinPriorityQueue({ priority: x => x[0] });
+    
+    for (let j = 0; j < n; j++) 
+        pq.enqueue([grid[0][j], 0, j]);
+    
+    while (!pq.isEmpty()) {
+        const [cost, x, y] = pq.dequeue().element;
+        if (x === m - 1) return cost;
+        
+        for (let dy = 0; x + 1 < m && dy < n; dy++) {
+            const dx = grid[x][y];
+            const newCost = cost + moveCost[dx][dy] + grid[x + 1][dy];
+            if (newCost < visited[x + 1][dy]) {
+                visited[x + 1][dy] = newCost;
+                pq.enqueue([newCost, x + 1, dy]);
+            }
+        }
+    }
+         
+    return -1;
+};
