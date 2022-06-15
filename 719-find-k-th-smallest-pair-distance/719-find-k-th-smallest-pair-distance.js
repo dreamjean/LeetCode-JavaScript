5adbec50f8d3512ed73dd4b6af00e7c4a4e58bca
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-15 08:16:50 +0800

    Time: 143 ms (17.14%), Space: 44.4 MB (40.00%) - LeetHub

---
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestDistancePair = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let left = 0;
+    let right = nums[n - 1] - nums[0];
+    
+    while (left < right) {
+        const mid = (left + right) >>> 1;
+        if (check(mid, k, nums)) left = mid + 1;
+        else right = mid;
+    }
+    
+    return left;
+};
+
+const check = (mid, k, nums) => {
+    let [count, start] = [0, 0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        while (nums[i] - nums[start] > mid) start++;
+        
+        count += i - start;
+    }
+    
+    return count < k;
+}
