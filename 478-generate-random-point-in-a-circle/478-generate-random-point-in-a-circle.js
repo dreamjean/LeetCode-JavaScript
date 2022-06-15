fd7f4b36088ce2258e41c81615ab85d8d6ea7a09
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-15 10:25:28 +0800

    Time: 197 ms (66.67%), Space: 65.9 MB (54.17%) - LeetHub

---
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} radius
+ * @param {number} x_center
+ * @param {number} y_center
+ */
+var Solution = function(radius, x_center, y_center) {
+    this.radius = radius;
+    this.x_center = x_center;
+    this.y_center = y_center;
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.randPoint = function() {
+    const len = Math.sqrt(Math.random()) * this.radius;
+    const deg = Math.random() * Math.PI * 2;
+    const x = this.x_center + len * Math.cos(deg);
+    const y = this.y_center + len * Math.sin(deg);
+    
+    return [x, y];
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(radius, x_center, y_center)
+ * var param_1 = obj.randPoint()
+ */
