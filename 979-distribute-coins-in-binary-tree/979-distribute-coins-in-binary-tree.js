372d223c3f0b789b1ce8ef6937a746da344430c7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-17 22:40:31 +0800

    Time: 119 ms (19.39%), Space: 45.1 MB (5.10%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var distributeCoins = function(root) {
+    let ans = 0;
+    
+    const dfs = node => {
+        if (!node) return 0;
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        ans += Math.abs(left) + Math.abs(right);
+        
+        return node.val + left + right - 1;
+    }
+    
+    dfs(root);
+    
+    return ans;
+};
