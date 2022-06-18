d9ecf87b1178f4115d83871e668e82b92db3db17
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-18 08:52:46 +0800

    Time: 70 ms (57.89%), Space: 42.3 MB (10.53%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} radius
+ * @param {number} xCenter
+ * @param {number} yCenter
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @return {boolean}
+ */
+var checkOverlap = function(radius, xCenter, yCenter, x1, y1, x2, y2) {
+    const edgeX = (xCenter < x1) ? x1 : (xCenter > x2) ? x2 : xCenter;
+    const edgeY = (yCenter < y1) ? y1 : (yCenter > y2) ? y2 : yCenter;
+    const distX = xCenter - edgeX;
+    const distY = yCenter - edgeY;
+    
+    return distX ** 2 + distY ** 2 <= radius ** 2;
+};
