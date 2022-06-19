b79f03fa11498544f6b74f4247ff6fbc8926d23e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-20 03:52:14 +0800

    Time: 202 ms (51.56%), Space: 52.2 MB (50.89%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+    let [prev, small, big] = [null, null, null];
+    
+    const inorderTree = node => {
+        if (!node) return;
+        
+        inorderTree(node.left);
+        
+        if (prev && prev.val > node.val) {
+            small = node;
+            if (!big) big = prev;
+        }
+        
+        prev = node;
+        
+        inorderTree(node.right);
+    }
+    
+    inorderTree(root);
+    
+    [small.val, big.val] = [big.val, small.val];
+};
