0afffa85e4ad68cdae85f2256198891f8a5e92b7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-20 04:01:52 +0800

    Time: 77 ms (83.32%), Space: 44.4 MB (77.05%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    return isSameTree(root, root);
+};
+
+const isSameTree = (p, q) => {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    
+    return p.val === q.val && isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
+}
