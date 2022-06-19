c0101c2fe6b510129e985effabc46eee024c7faa
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-19 19:49:22 +0800

    Time: 111 ms (60.75%), Space: 50.9 MB (17.76%) - LeetHub

---
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findFrequentTreeSum = function(root) {
+    const cnt = {};
+
+    const dfs = node => {
+        if (!node) return 0;
+        
+        const s = node.val + dfs(node.left) + dfs(node.right);
+        cnt[s] ? cnt[s]++ : cnt[s] = 1;
+        
+        return s;
+    }
+    
+    dfs(root);
+    const maxCount = Math.max(...Object.values(cnt));
+    
+    return Object
+        .entries(cnt)
+        .reduce((arr, [num, count]) => {
+            if (count === maxCount) arr.push(+num);
+        
+            return arr;
+        }, [])
+};
