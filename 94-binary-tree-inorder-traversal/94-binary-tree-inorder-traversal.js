2f3e48c0a694a96702263d6e305711a6d4476142
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-20 02:51:22 +0800

    Time: 77 ms (58.53%), Space: 42.1 MB (55.83%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -11,5 +11,7 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    return !root? [] : [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+    if (!root) return [];
+    
+    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
 };
