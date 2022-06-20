7b9f9de67d21c18edd4483d128f73b78a0bc7022
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-21 02:28:19 +0800

    Time: 63 ms (100.00%), Space: 41.8 MB (100.00%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.js
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.js
@@ -6,7 +6,7 @@
 var minimumNumbers = function(num, k) {
     if (!num) return 0;
     
-    for (let i = 1; i * k <= num && i <= 10; ++i) {
+    for (let i = 1; i <= 10 && i * k <= num; i++) {
         if (i * k % 10 === num % 10) return i;
     }
     
