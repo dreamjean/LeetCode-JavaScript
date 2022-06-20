7d16f1a327ebd48052ac82cac1c9b582e707c913
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-21 02:55:07 +0800

    Time: 371 ms (100.00%), Space: 51.4 MB (100.00%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.js
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.js
@@ -7,14 +7,14 @@
 var sellingWood = function(m, n, prices) {
     const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
     
-    for (const [h, w, p] of prices) dp[h][w] = p;
+    for (const [h, w, price] of prices) dp[h][w] = price;
     
-    for (let h = 1; h <= m; h++) {
-        for (let w = 1; w <= n; w++) {
-            for (let nh = 1; nh <= h / 2; nh++) 
+    for (let h = 0; h <= m; h++) {
+        for (let w = 0; w <= n; w++) {
+            for (let nh = 0; nh <= h / 2; nh++) 
                 dp[h][w] = Math.max(dp[h][w], dp[nh][w] + dp[h - nh][w]);
             
-            for (let nw = 1; nw <= w / 2; nw++)
+            for (let nw = 0; nw <= w / 2; nw++)
                 dp[h][w] = Math.max(dp[h][w], dp[h][nw] + dp[h][w - nw]);
         }
     }
