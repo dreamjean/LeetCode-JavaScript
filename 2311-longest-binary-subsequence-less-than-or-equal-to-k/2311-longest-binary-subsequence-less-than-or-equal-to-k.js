9dfa99d5c04ecd611eb2cf26f79429bbfcb8d08c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-21 02:37:20 +0800

    Time: 87 ms (100.00%), Space: 44.2 MB (100.00%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.js
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.js
@@ -6,9 +6,9 @@
 var longestSubsequence = function(s, k) {
     let [cnt, val, pow] = [0, 0, 1];
     
-    for (let i = s.length - 1; i >= 0 && val + pow <= k; --i) {
+    for (let i = s.length - 1; i >= 0 && val + pow <= k; i--) {
         if (s[i] === '1') {
-            ++cnt;
+            cnt++;
             val += pow;
         }
         
