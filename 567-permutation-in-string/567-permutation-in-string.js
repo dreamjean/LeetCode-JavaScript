3cb040af5634be31495b3525eece41ea3cfda7cf
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-23 03:42:25 +0800

    Time: 143 ms (54.69%), Space: 43.3 MB (81.05%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    const cnt = new Array(26).fill(0);
+    const a = 'a'.charCodeAt();
+    const n = s1.length;
+    
+    for (let i = 0; i < s1.length; i++) {
+        cnt[s1.charCodeAt(i) - a]++;
+        cnt[s2.charCodeAt(i) - a]--;
+    }
+    
+    if (check(cnt)) return true;
+    
+    for (let i = n; i < s2.length; i++) {
+        cnt[s2.charCodeAt(i) - a]--;
+        cnt[s2.charCodeAt(i - n) - a]++;
+        if (check(cnt)) return true;
+    }
+    
+    return false;
+};
+
+const check = nums => nums.every(num => !num);
