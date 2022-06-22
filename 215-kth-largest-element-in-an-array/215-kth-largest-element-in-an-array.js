c152568e0fe2cee3e864490597bc6896e4705579
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-23 02:30:55 +0800

    Time: 149 ms (27.67%), Space: 43.7 MB (61.81%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return quickSelect(nums, k)
+    const n = nums.length;
+    const finalIdx = n - k;
+    let [low, high] = [0, n - 1];
+    
+    while (low <= high) {
+        const pivotIdx = partition(nums, low, high);
+        
+        if (pivotIdx === finalIdx) return nums[pivotIdx];
+        
+        if (pivotIdx < finalIdx) low = pivotIdx + 1;
+        else high = pivotIdx - 1;
+    }
 };
 
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -26,19 +37,4 @@ const partition = (arr, start, end) => {
     swap(arr, i, end);
     
     return i;
-}
-
-const quickSelect = (arr, k) => {
-    const n = arr.length;
-    const finalIdx = n - k;
-    let [low, high] = [0, n - 1];
-    
-    while (low <= high) {
-        const pivotIdx = partition(arr, low, high);
-        
-        if (pivotIdx === finalIdx) return arr[pivotIdx];
-        
-        if (pivotIdx < finalIdx) low = pivotIdx + 1;
-        else high = pivotIdx - 1;
-    }
 }
