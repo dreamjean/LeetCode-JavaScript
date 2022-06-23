012de0663ef1cba9c99f6e00aa022c3f2d1bc6af
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-23 11:07:38 +0800

    Time: 149 ms (42.30%), Space: 44.5 MB (85.81%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    const cnt = new Array(128).fill(0);
+    let [start, end] = [-Infinity, Infinity];
+    let [left, right, count] = [0, 0, t.length];
+    
+    for (let i = 0; i < t.length; i++)
+        cnt[t.charCodeAt(i)]++;
+    
+    while (right < s.length) {
+        if (cnt[s.charCodeAt(right)]-- > 0) count--;
+        
+        while (!count) {
+            if (end - start > right - left) [start, end] = [left, right];
+            if (++cnt[s.charCodeAt(left++)] > 0) count++;
+        }
+        
+        right++;
+    }
+    
+    return end === Infinity ? '' : s.substring(start, end + 1);
+};
