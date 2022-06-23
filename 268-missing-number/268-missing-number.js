6e8cfdc3d1f5f805d85ddba21045a22d8080f2a8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-24 03:57:23 +0800

    Time: 86 ms (73.85%), Space: 44.2 MB (76.61%) - LeetHub

---
+++ 268-missing-number/268-missing-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const n = nums.length;
+    
+    return n * (n + 1) / 2 - nums.reduce((a, b) => a + b);
+};
