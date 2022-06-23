b1bdd50a8c82efca8efd1921593f7e50f4e37a42
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-24 03:47:06 +0800

    Time: 88 ms (94.47%), Space: 46.8 MB (99.41%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    const dp = new Array(n + 1).fill(0);
+    
+    for (let i = 1; i <= n; i++) {
+        dp[i] = dp[i & (i - 1)] + 1;
+    }
+    
+    return dp;
+};
