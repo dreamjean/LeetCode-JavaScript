b07e82de028d1779afd7cceb8f1ea7804f28dc76
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-25 22:43:31 +0800

    Time: 165 ms (5.55%), Space: 44.8 MB (38.89%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function(nums) {
+    let cnt = 0;
+    
+    for (let i = 1; i < nums.length && cnt < 2; i++) {
+        if (nums[i - 1] <= nums[i]) continue;
+        if (i > 1 && nums[i - 2] > nums[i]) nums[i] = nums[i - 1];
+        
+        cnt++;
+    }
+    
+    return cnt < 2;
+};
