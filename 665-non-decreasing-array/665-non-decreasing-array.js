1472778bc4c2f0fc9e40f094cc8a253921fe9268
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-25 22:44:32 +0800

    Time: 97 ms (45.24%), Space: 45 MB (19.84%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.js
+++ 665-non-decreasing-array/665-non-decreasing-array.js
@@ -5,7 +5,7 @@
 var checkPossibility = function(nums) {
     let cnt = 0;
     
-    for (let i = 1; i < nums.length && cnt < 2; i++) {
+    for (let i = 1; i < nums.length; i++) {
         if (nums[i - 1] <= nums[i]) continue;
         if (i > 1 && nums[i - 2] > nums[i]) nums[i] = nums[i - 1];
         
