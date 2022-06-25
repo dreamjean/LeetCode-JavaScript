d167479991b21962a58b7b9486f19f8a89678042
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-26 04:35:10 +0800

    Time: 740 ms (100.00%), Space: 132.2 MB (100.00%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var countPairs = function(n, edges) {
+    const graph = Array.from({ length: n }, () => []);
+    const visite = new Array(n).fill(false);
+    let ans = n * (n - 1) / 2;
+    
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visite[i]) {
+            const cnt = dfs(i, visite, graph);
+            ans -= cnt * (cnt - 1) / 2;
+        }
+    }
+    
+    return ans;
+};
+
+const dfs = (start, visite, graph) => {
+    visite[start] = true;
+    let ans = 1;
+    
+    for (const v of graph[start]) {
+        if (!visite[v]) ans += dfs(v, visite, graph);
+    }
+    
+    return ans;
+}
