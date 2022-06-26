004613e192b12881556630988f0b9393826f7b99
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-26 12:12:43 +0800

    Time: 90 ms (100.00%), Space: 44.4 MB (100.00%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function(grid) {
+    const n = grid.length;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j || i + j === n - 1) {
+                if (!grid[i][j]) return false;
+            }
+                            
+            else {
+                if (grid[i][j] > 0) return false;
+            }
+                
+        }
+    }
+    
+    return true;
+};
