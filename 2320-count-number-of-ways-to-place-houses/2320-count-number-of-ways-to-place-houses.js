bff11d59fc62ffd21fb8811761809382f754c387
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-06-26 20:31:33 +0800

    Time: 208 ms (100.00%), Space: 47.6 MB (100.00%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countHousePlacements = function(n) {
+    const mod = 1e9 + 7;
+    const ll = BigInt;
+    let [a, b] = [1n, 1n];
+    
+    for (let i = 0; i < n; ++i) {
+        [a, b] = [b, a + b]
+    }
+    
+    return b * b % ll(mod);
+};
