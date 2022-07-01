5a3fd310c4b5eb872f5a196c83faa0e0477ee69f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-01 14:14:15 +0800

    Time: 104 ms (79.83%), Space: 44.2 MB (68.99%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -4,24 +4,22 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
+    const [len1, len2] = [s1.length, s2.length];
     const cnt = new Array(26).fill(0);
     const a = 'a'.charCodeAt();
-    const n = s1.length;
     
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < len1; i++) {
         cnt[s1.charCodeAt(i) - a]++;
         cnt[s2.charCodeAt(i) - a]--;
     }
     
-    if (check(cnt)) return true;
+    if (cnt.every(num => !num)) return true;
     
-    for (let i = n; i < s2.length; i++) {
+    for (let i = len1; i < len2; i++) {
         cnt[s2.charCodeAt(i) - a]--;
-        cnt[s2.charCodeAt(i - n) - a]++;
-        if (check(cnt)) return true;
+        cnt[s2.charCodeAt(i - len1) - a]++;
+        if (cnt.every(num => !num)) return true;
     }
-
+    
     return false;
 };
-
-const check = nums => nums.every(num => !num);
