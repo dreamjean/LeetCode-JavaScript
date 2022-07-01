5dc7256f877e4d6c0f90c2bd29d0581ba931da47
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-02 04:04:50 +0800

    Time: 197 ms (82.93%), Space: 51.4 MB (73.35%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+    const [m, n] = [mat.length, mat[0].length];
+    const dirt = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    const queue = [];
+    
+   for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            !mat[i][j] ? queue.push([i, j]) : mat[i][j] = -1;
+        }
+    }      
+    
+    while (queue.length) {
+        const [row, col] = queue.shift();
+        for (const [dx, dy] of dirt) {
+            const [x, y] = [row + dx, col + dy];
+            if (x < 0 || x >= m || y < 0 || y >= n || mat[x][y] !== -1) continue;
+            
+            mat[x][y] = mat[row][col] + 1;
+            queue.push([x, y]);
+        }
+    }
+ 
+    return mat;
+};
