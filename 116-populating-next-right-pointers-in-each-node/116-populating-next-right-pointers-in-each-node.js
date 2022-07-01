6a73086ac95e5dbb4aa42c7fea29d1f99b9308a3
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-01 23:07:53 +0800

    Time: 173 ms (5.25%), Space: 48.9 MB (48.05%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (!root) return root;
+    
+    const queue = [root];
+    
+    while (queue.length) {
+        let n = queue.length;
+        while (n--) {
+            const node = queue.shift();
+            node.next = !n ? null : queue[0];
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+    
+    return root;
+};
