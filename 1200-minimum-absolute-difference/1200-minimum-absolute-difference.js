c16362f1e36783797dfd0fc24ca198a27cbc356a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-04 01:56:15 +0800

    Time: 261 ms (36.20%), Space: 55.4 MB (24.04%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    arr.sort((a, b) => a - b);
+    let minDiff = arr.at(-1) - arr[0];
+    let ans = [];
+    
+    for (let i = 1; i < arr.length; i++) {
+        const [prev, curr] = [arr[i - 1], arr[i]];
+        const diff = curr - prev;
+        if (diff === minDiff) ans.push([prev, curr]);
+        if (diff < minDiff) {
+            ans = [[prev, curr]];
+            minDiff = diff;
+        }
+    }
+    
+    return ans;
+};
