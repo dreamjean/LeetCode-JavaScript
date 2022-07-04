c7c5439dc4889bfdaa42f324cd5e1cb825eabcfb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-05 03:00:42 +0800

    Time: 245 ms (18.71%), Space: 50.2 MB (34.63%) - LeetHub

--- 77-combinations/77-combinations.js
+++ 77-combinations/77-combinations.js
@@ -9,11 +9,12 @@ var combine = function(n, k) {
     const backtrack = (start = 1, path = []) => {
         if (path.length === k) return ans.push([...path]);
         
-        for (let i = start; i <= n; i++)
+        for (let i = start; i <= n; i++) {
             backtrack(i + 1, [...path, i]);
+        }
     }
     
-    backtrack()
+    backtrack();
     
     return ans;
 };
