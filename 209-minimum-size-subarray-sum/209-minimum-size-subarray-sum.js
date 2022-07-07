1fedf5a4fb4458f055a1ea4151fab7dbf80c333d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-07 16:24:49 +0800

    Time: 123 ms (20.16%), Space: 47.2 MB (5.32%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -4,12 +4,13 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let [left, right, sum, res] = [0, 0, 0, Infinity];
+    let [left, right, sum, res] = [0, -1, 0, Infinity];
     
-    while (right < nums.length) {
-        sum += nums[right++];
+    while (right++ < nums.length) {
+        sum += nums[right];
+        
         while (sum >= target) {
-            res = Math.min(res, right - left);
+            res = Math.min(res, right - left + 1);
             sum -= nums[left++];     
         }
     }
