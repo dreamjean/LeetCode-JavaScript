05c2962f5f2327d99f5dac5bb4cd697c3cdf34c4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-11 06:43:27 +0800

    Time: 224 ms (52.46%), Space: 53 MB (41.44%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -5,16 +5,16 @@
 var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
     const n = nums.length;
-    const ans = []
+    const ans = [];
     
     for (let i = 0; i < n - 2; i++) {
         if (nums[i] === nums[i - 1]) continue;
         
         let [l, r] = [i + 1, n - 1];
         while (l < r) {
-            let sum = nums[i] + nums[l] + nums[r]
+            const sum = nums[i] + nums[l] + nums[r];
             if (!sum) {
-                ans.push([nums[i], nums[l], nums[r]])
+                ans.push([nums[i], nums[l], nums[r]]);
                 while (l < r && nums[l] === nums[++l]);
                 while (l < r && nums[r] === nums[--r]);
             }
