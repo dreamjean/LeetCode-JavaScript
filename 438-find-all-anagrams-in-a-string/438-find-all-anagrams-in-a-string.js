3fba6fbd7aca6d9b23a405499ffee1eedc827225
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-14 07:04:46 +0800

    Time: 162 ms (59.89%), Space: 45.9 MB (70.38%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -6,16 +6,17 @@
 var findAnagrams = function(s, p) {
     const map = {};
     const ans = [];
-    let [l, r, cnt] = [0, 0, p.length];
+    let [l, r, n] = [0, 0, p.length];
     
     p.split('').forEach(char => map[char] ? map[char]++ : map[char] = 1);
     
     while (r < s.length) {
-        if (map[s[r++]]-- > 0) cnt--;
-        if (!cnt) ans.push(l);
+        if (map[s[r++]]-- > 0) n--;
+        if (!n) ans.push(l);
         
-        while (r - l === p.length) 
-            if (++map[s[l++]] > 0) cnt++;
+        while (r - l === p.length) {
+            if (++map[s[l++]] > 0) n++;     
+        }
     }
     
     return ans;
