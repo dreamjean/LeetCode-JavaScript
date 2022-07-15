9a09241ef76fb75be932ebacdeed4df2c5177c94
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-16 06:47:52 +0800

    Time: 83 ms (94.30%), Space: 45.2 MB (63.65%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -7,21 +7,24 @@ var maxAreaOfIsland = function(grid) {
     const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
     let max = 0;
     
-    const dfs = (row, col) => {
-        if (row < 0 || row >= m || col < 0 || col >= n || !grid[row][col]) return 0;
+    const dfs = (r, c) => {
+        if (r < 0 || r >= m || c < 0 || c >= n || !grid[r][c]) return 0;
         
-        grid[row][col] = 0;
-        let res = 1;
+        grid[r][c] = 0;
+        let count = 1;
         
-        for (const [dx, dy] of dirs) 
-            res += dfs(row + dx, col + dy);
+        for (const [dx, dy] of dirs) {
+            count += dfs(r + dx, c + dy);
+        }
         
-        return res;
+        return count;
     }
     
-    for (let i = 0; i < m; i++) 
-        for (let j = 0; j < n; j++)
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             if (grid[i][j] === 1) max = Math.max(max, dfs(i, j));
+        }
+    }
     
     return max;
 };
