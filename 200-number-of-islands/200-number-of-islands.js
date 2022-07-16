eaa7aa472958bd6b65f71d12695441d889bdba79
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-16 08:57:55 +0800

    Time: 109 ms (72.19%), Space: 45.5 MB (51.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -4,24 +4,24 @@
  */
 var numIslands = function(grid) {
     const [m, n] = [grid.length, grid[0].length];
-    const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
     let count = 0;
     
-    const fill = (row, col) => {
-        grid[row][col] = '0';
-
+    const dfs = (r, c) => {
+        grid[r][c] = '0';
+        
         for (const [dx, dy] of dirs) {
-            const [x, y] = [dx + row, dy + col];
+            const [x, y] = [r + dx, c + dy];
             if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] === '0') continue;
-
-            fill(x, y);
+            
+            dfs(x, y);
         }
     }
     
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === '1') {
-                fill(i, j);
+                dfs(i, j);
                 count++;
             }
         }
