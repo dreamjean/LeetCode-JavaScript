dad5af83f2e4c88aeac0fc423e2f2138fd12de28
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-18 11:38:20 +0800

    Time: 193 ms (87.96%), Space: 64.5 MB (51.85%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.js
+++ 1905-count-sub-islands/1905-count-sub-islands.js
@@ -14,20 +14,18 @@ var countSubIslands = function (grid1, grid2) {
   let count = 0;
 
   const dfs = (r, c) => {
-    if (r < 0 || r >= m || c < 0 || c >= n || !grid2[r][c]) return 1;
+    if (r < 0 || r >= m || c < 0 || c >= n || !grid2[r][c]) return true;
 
     grid2[r][c] = 0;
-    let res = 1;
+    let res = true;
 
-    for (const [dx, dy] of dirs) 
-        if (!dfs(dx + r, dy + c)) res = 0;
+    for (const [dx, dy] of dirs) if (!dfs(dx + r, dy + c)) res = false;
 
     return res && grid1[r][c];
   };
 
   for (let i = 0; i < m; i++)
-    for (let j = 0; j < n; j++) 
-        if (grid2[i][j] === 1) count += +dfs(i, j);
+    for (let j = 0; j < n; j++) if (grid2[i][j] === 1) count += +dfs(i, j);
 
   return count;
 };
