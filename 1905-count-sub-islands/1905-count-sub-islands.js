f3d9cad0465d23b757285f733d9739fab382978a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-18 11:35:21 +0800

    Time: 250 ms (65.74%), Space: 64.4 MB (53.24%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.js
+++ 1905-count-sub-islands/1905-count-sub-islands.js
@@ -3,28 +3,29 @@
  * @param {number[][]} grid2
  * @return {number}
  */
-var countSubIslands = function(grid1, grid2) {
-    const [m, n] = [grid1.length, grid1[0].length];
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    let count = 0;
-    
-    const dfs = (r, c) => {
-        if (r < 0 || r >= m || c < 0 || c >= n || !grid2[r][c]) return true;
-        
-        grid2[r][c] = 0;
-        let res = true;
-        
-        for (const [dx, dy] of dirs) {
-            if (!dfs(dx + r, dy + c)) res = false;
-        }
-        
-        return res && grid1[r][c];
-    }
-    
-    
-    for (let i = 0; i < m; i++) 
-        for (let j = 0; j < n; j++) 
-            if (grid2[i][j] === 1) count += +dfs(i, j);
-    
-    return count;
+var countSubIslands = function (grid1, grid2) {
+  const [m, n] = [grid1.length, grid1[0].length];
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  let count = 0;
+
+  const dfs = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || !grid2[r][c]) return true;
+
+    grid2[r][c] = 0;
+    let res = true;
+
+    for (const [dx, dy] of dirs) if (!dfs(dx + r, dy + c)) res = false;
+
+    return res && grid1[r][c];
+  };
+
+  for (let i = 0; i < m; i++)
+    for (let j = 0; j < n; j++) if (grid2[i][j] === 1) count += +dfs(i, j);
+
+  return count;
 };
