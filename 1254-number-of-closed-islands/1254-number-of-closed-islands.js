d80aa3e159c890efece17d3ebf910d4fcfd87d93
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-19 01:56:10 +0800

    Time: 89 ms (82.28%), Space: 44.8 MB (61.86%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var closedIsland = function(grid) {
+    const [m, n] = [grid.length, grid[0].length];
+    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    let count = 0;
+    
+    const dfs = (r, c) => {
+        if (grid[r][c]) return true;
+        if (!r || !c || r === m - 1 || c === n - 1) return false;
+        
+        grid[r][c] = 1;
+        let res = true;
+        
+        for (const [dx, dy] of dirs) {
+            if (!dfs(dx + r, dy + c)) res = false;
+        }
+        
+        return res;
+    }
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (!grid[i][j]) count += +dfs(i, j);
+        }
+    }
+    
+    return count;
+};
