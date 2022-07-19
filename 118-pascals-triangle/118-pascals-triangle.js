70867f1865af55a98ba2227678550750f4a203bb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 07:09:34 +0800

    Time: 58 ms (96.09%), Space: 42.3 MB (15.20%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const ans = [[1]];
+    
+    for (let i = 1; i < numRows; i++) {
+        const row = [];
+        for (let j = 0; j <= i; j++)
+            row.push((ans[i - 1][j - 1] || 0) + (ans[i - 1][j] || 0));
+        
+        ans.push(row);
+    }
+    
+    return ans;
+};
