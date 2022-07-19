a1f85bd7ac6afc3b2e8bd0fe5ef16a0a610051cc
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-19 10:57:15 +0800

    Time: 88 ms (77.28%), Space: 44.4 MB (36.15%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -7,7 +7,6 @@ var jump = function(nums) {
     
     for (let i = 0; i < nums.length - 1; i++) {
         maxPos = Math.max(maxPos, nums[i] + i);
-        
         if (end === i) {
             end = maxPos;
             steps++;
