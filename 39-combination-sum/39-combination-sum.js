30313e928824c9356c8d3718993c2311c2710f66
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:40:34 +0800

    Time: 102 ms (78.61%), Space: 48.9 MB (28.11%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -3,21 +3,23 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
-    const ans = [];
-    
-    const backtrack = (diff, start = 0, path = []) => {
-        if (!diff) return ans.push([...path]);
-        
-        for (let i = start; i < candidates.length; i++) {
-            const currDiff = diff - candidates[i];
-            if (currDiff < 0) continue;
-            
-            backtrack(currDiff, i, [...path, candidates[i]]);
-        }
+var combinationSum = function (candidates, target) {
+  candidates.sort((a, b) => a - b);
+  const ans = [];
+
+  const backtrack = (diff, start = 0, path = []) => {
+    if (!diff) return ans.push([...path]);
+
+    for (let i = start; i < candidates.length; i++) {
+      const currDiff = diff - candidates[i];
+      if ((i > start && candidates[i] === candidates[i - 1]) || currDiff < 0)
+        continue;
+
+      backtrack(currDiff, i, [...path, candidates[i]]);
     }
-    
-    backtrack(target);
-    
-    return ans;
+  };
+
+  backtrack(target);
+
+  return ans;
 };
