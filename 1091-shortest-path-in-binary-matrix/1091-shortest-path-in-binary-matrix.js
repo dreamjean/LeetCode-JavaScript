f87b7341caba9a292cc25b7abede83246b6ee2ad
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 08:49:58 +0800

    Time: 176 ms (60.59%), Space: 50 MB (47.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -2,29 +2,39 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var shortestPathBinaryMatrix = function(grid) {
-    if (grid[0][0] === 1) return -1;
-    
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]];
-    const n = grid.length;
-    const queue = [[0, 0]];
-    let step = 0;
-    
-    while (queue.length) {
-        step++;
-        for (let i = queue.length - 1; i >= 0; i--) {
-            const [r, c] = queue.shift();
-            if (r === n - 1 && c === n - 1) return step;
-            
-            for (const [dx, dy] of dirs) {
-                const [x, y] = [r + dx, c + dy];
-                if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] === 1) continue;
-                
-                grid[x][y] = 1;
-                queue.push([x, y]);
-            }
-        }
+var shortestPathBinaryMatrix = function (grid) {
+  if (grid[0][0] === 1) return -1;
+
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+    [-1, -1],
+  ];
+  const n = grid.length;
+  const queue = [[0, 0]];
+  let count = 0;
+
+  while (queue.length) {
+    count++;
+    const size = queue.length;
+    for (let i = size - 1; i >= 0; i--) {
+      const [r, c] = queue.shift();
+      if (r === n - 1 && c === n - 1) return count;
+
+      for (const [dx, dy] of dirs) {
+        const [x, y] = [dx + r, dy + c];
+        if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] === 1) continue;
+
+        grid[x][y] = 1;
+        queue.push([x, y]);
+      }
     }
-    
-    return -1;
+  }
+
+  return -1;
 };
