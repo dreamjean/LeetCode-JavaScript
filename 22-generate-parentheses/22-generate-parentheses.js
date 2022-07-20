884f69ba178c1a98be26d8c9d66cbd74eb6d9909
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:47:16 +0800

    Time: 100 ms (38.35%), Space: 42.8 MB (57.33%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const ans = [];
+    
+    const backtrack = (left = n, right = n, path = '') => {
+        if (!left && !right) return ans.push(path)
+        
+        if (left) backtrack(left - 1, right, path + '(');
+        if (right > left) backtrack(left, right - 1, path + ')');
+    }
+    
+    backtrack();
+    
+    return ans;
+};
