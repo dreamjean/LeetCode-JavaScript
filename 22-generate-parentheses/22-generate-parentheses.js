5eb2dd54a3d81519bbdbde7e35c8fdb106d65af6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:50:35 +0800

    Time: 116 ms (20.14%), Space: 42.8 MB (52.31%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -5,7 +5,7 @@
 var generateParenthesis = function (n) {
   const ans = [];
 
-  const backtrack = (left = n, right = n, path = "") => {
+  const backtrack = (left = n, right = n, path = '') => {
     if (!left && !right) return ans.push(path);
 
     if (left) backtrack(left - 1, right, path + "(");
