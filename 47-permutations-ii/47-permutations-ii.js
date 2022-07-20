72d0ad85fea036e8730dabd821750d3debe74c4c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:14:01 +0800

    Time: 77 ms (97.35%), Space: 46.3 MB (51.22%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -2,24 +2,24 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function(nums) {
+var permuteUnique = function (nums) {
   nums.sort((a, b) => a - b);
-    const ans = [];
-    
-    const backtrack = (arr, path = []) => {
-        if (!arr.length) return ans.push([...path]);
-        
-        let prev = Infinity;
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === prev) continue;
-            
-            const newArr = arr.slice(0, i).concat(arr.slice(i + 1));
-            backtrack(newArr, [...path, arr[i]]);
-            prev = arr[i];
-        }
+  const ans = [];
+
+  const backtrack = (arr, path = []) => {
+    if (!arr.length) return ans.push([...path]);
+
+    let prev = Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === prev) continue;
+
+      const newArr = arr.slice(0, i).concat(arr.slice(i + 1));
+      backtrack(newArr, [...path, arr[i]]);
+      prev = arr[i];
     }
-    
-    backtrack(nums);
-    
-    return ans;
+  };
+
+  backtrack(nums);
+
+  return ans;
 };
