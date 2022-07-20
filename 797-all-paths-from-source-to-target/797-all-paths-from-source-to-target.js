003491ea3028a6986e076be738d589b72c7d4fdc
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 14:33:01 +0800

    Time: 154 ms (54.63%), Space: 50.5 MB (64.29%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -2,18 +2,17 @@
  * @param {number[][]} graph
  * @return {number[][]}
  */
-var allPathsSourceTarget = function(graph) {
-    const n = graph.length;
-    const ans = [];
-    
-    const backtrack = (curr = 0, path = [0]) => {
-        if(curr === n - 1) return ans.push([...path]);
-        
-        for (const next of graph[curr])
-            backtrack(next, [...path, next]);
-    }
-    
-    backtrack();
-    
-    return ans;
+var allPathsSourceTarget = function (graph) {
+  const n = graph.length;
+  const ans = [];
+
+  const backtrack = (curr = 0, path = [0]) => {
+    if (curr === n - 1) return ans.push([...path]);
+
+    for (const next of graph[curr]) backtrack(next, [...path, next]);
+  };
+
+  backtrack();
+
+  return ans;
 };
