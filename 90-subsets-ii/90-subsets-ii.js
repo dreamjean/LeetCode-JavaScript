558420b92e5238d70f6b2e98cdf7ac9ede4b12cb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 08:42:25 +0800

    Time: 70 ms (95.62%), Space: 44.5 MB (72.95%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.js
+++ 90-subsets-ii/90-subsets-ii.js
@@ -2,21 +2,21 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsetsWithDup = function(nums) {
-    nums.sort((a, b) => a - b);
-    const ans = [];
-    
-    const backtrack = (start = 0, path = []) => {
-        ans.push([...path]);
-        
-        for (let i = start; i < nums.length; i++) {
-            if (i > start && nums[i] === nums[i - 1]) continue;
-            
-            backtrack(i + 1, [...path, nums[i]]);
-        }
+var subsetsWithDup = function (nums) {
+  nums.sort((a, b) => a - b);
+  const ans = [];
+
+  const backtrack = (start = 0, path = []) => {
+    ans.push([...path]);
+
+    for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] === nums[i - 1]) continue;
+
+      backtrack(i + 1, [...path, nums[i]]);
     }
-    
-    backtrack();
-    
-    return ans;
+  };
+
+  backtrack();
+
+  return ans;
 };
