98d94bd11d676bfee04eb00afea66c50f587ba71
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:55:31 +0800

    Time: 133 ms (43.12%), Space: 46 MB (90.85%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-    let maxPos = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (i > maxPos) return false;
-        
-        maxPos = Math.max(maxPos, nums[i] + i);
-    }
-    
-    return true;
+var canJump = function (nums) {
+  let maxPos = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxPos) return false;
+
+    maxPos = Math.max(maxPos, nums[i] + i);
+  }
+
+  return true;
 };
