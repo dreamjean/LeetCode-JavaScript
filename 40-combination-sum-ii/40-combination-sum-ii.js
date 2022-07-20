562a4a663a21d08625fc173a5254483abc769de9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 23:07:36 +0800

    Time: 115 ms (37.34%), Space: 44.2 MB (70.38%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.js
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -3,22 +3,23 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum2 = function(candidates, target) {
-    candidates.sort((a, b) => a - b);
-    const ans = [];
-    
-    const backtrack = (diff, start = 0, path = []) => {
-        if (!diff) return ans.push([...path]);
-        
-        for (let i = start; i < candidates.length; i++) {
-            const newDiff = diff - candidates[i];
-            if (i !== start && candidates[i] === candidates[i - 1] || newDiff < 0) continue;
-            
-            backtrack(newDiff, i + 1, [...path, candidates[i]]);
-        }
+var combinationSum2 = function (candidates, target) {
+  candidates.sort((a, b) => a - b);
+  const ans = [];
+
+  const backtrack = (diff, start = 0, path = []) => {
+    if (!diff) return ans.push([...path]);
+
+    for (let i = start; i < candidates.length; i++) {
+      const currDiff = diff - candidates[i];
+      if ((i > start && candidates[i] === candidates[i - 1]) || currDiff < 0)
+        continue;
+
+      backtrack(currDiff, i + 1, [...path, candidates[i]]);
     }
-    
-    backtrack(target);
-    
-    return ans;
+  };
+
+  backtrack(target);
+
+  return ans;
 };
