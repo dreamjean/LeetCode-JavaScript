11c485ae425f966a135ac16228fdc157704d67c9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-20 22:17:30 +0800

    Time: 113 ms (44.82%), Space: 44.5 MB (91.08%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -2,20 +2,20 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-    nums.sort((a, b) => a - b);
-    const n = nums.length;
-    const ans = [];
-    
-    const backtrack = (path = []) => {
-        if (path.length === n) return ans.push([...path]);
-        
-        nums.forEach(num => {
-            if (!path.includes(num)) backtrack([...path, num]);
-        })
-    }
-    
-    backtrack();
-    
-    return ans;
+var permute = function (nums) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  const ans = [];
+
+  const backtrack = (path = []) => {
+    if (path.length === n) return ans.push([...path]);
+
+    nums.forEach((num) => {
+      if (!path.includes(num)) backtrack([...path, num]);
+    });
+  };
+
+  backtrack();
+
+  return ans;
 };
