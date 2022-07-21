ccb525fada61bb90a1ab0bf034ed667789791398
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-22 06:00:23 +0800

    Time: 80 ms (67.92%), Space: 41.8 MB (85.13%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -3,10 +3,10 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
-    let [x, y, ans] = [n, 1, 1];
-    
-    while (y < m) ans = ~~(ans * x++ / y++)
-        
-    return ans;
+var uniquePaths = function (m, n) {
+  let [x, y, ans] = [n, 1, 1];
+
+  while (y < m) ans = (ans * x++) / y++;
+
+  return ans;
 };
