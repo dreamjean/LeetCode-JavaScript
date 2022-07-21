a9ac81ede471d3f5c81f63e6dfdeb2bc961d17d9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-22 05:51:52 +0800

    Time: 584 ms (53.85%), Space: 84.9 MB (7.69%) - LeetHub

--- 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.js
+++ 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.js
@@ -3,43 +3,46 @@
  * @param {number} maxValue
  * @return {number}
  */
-var idealArrays = function(n, maxValue) {
-    let ans = 0n;
-    const ll = BigInt;
-    const [MOD, N] = [ll(10 ** 9 + 7), n + maxValue];
-    const inv = new Array(N).fill(0);
-    const fact = new Array(N).fill(0);
-    const factinv = new Array(N).fill(0);
-    inv[1] = fact[0] = fact[1] = factinv[0] = factinv[1] = 1n;
-    
-    for (let i = 2; i < N; i++) {
-        inv[i] = MOD - MOD / ll(i) * inv[MOD % ll(i)] % MOD;
-        fact[i] = fact[i - 1] * ll(i) % MOD;
-        factinv[i] = factinv[i - 1] * inv[i] % MOD;
-    }
-    
-    for (let i = 1; i <= maxValue; i++) {
-        const map = numberOfFactor(i);
-        let mul = 1n;
-        Object.values(map).forEach(num => mul = mul * comb(n + num - 1, num, fact, factinv, MOD) % MOD);
-        ans += mul;
-    }
+var idealArrays = function (n, maxValue) {
+  let ans = 0n;
+  const ll = BigInt;
+  const [M, N] = [ll(1e9 + 7), n + maxValue];
+  const inv = new Array(N).fill(0);
+  const fact = new Array(N).fill(0);
+  const factinv = new Array(N).fill(0);
+  inv[1] = fact[0] = fact[1] = factinv[0] = factinv[1] = 1n;
 
-    
-    return ans % MOD;
+  for (let i = 2; i < N; i++) {
+    inv[i] = M - (((M / ll(i)) * inv[M % ll(i)]) % M);
+    fact[i] = (fact[i - 1] * ll(i)) % M;
+    factinv[i] = (factinv[i - 1] * inv[i]) % M;
+  }
+
+  for (let i = 1; i <= maxValue; i++) {
+    const map = numberOfFactor(i);
+    let mul = 1n;
+    Object.values(map).forEach(
+      (val) => (mul = (mul * comb(n + val - 1, val, M, fact, factinv)) % M)
+    );
+    ans += mul;
+  }
+
+  return ans % M;
 };
 
-const comb = (a, b, fact, factinv, M) => fact[a] * factinv[b] % M * factinv[a - b] % M;
+const comb = (a, b, M, fact, factinv) =>
+  (((fact[a] * factinv[b]) % M) * factinv[a - b]) % M;
 
-const numberOfFactor = n => {
-    const map = {};
-    for (let i = 2; i * i <= n; i++) {
-        while (!(n % i)) {
-            n /= i;
-            map[i] ? map[i]++ : map[i] = 1;
-        }        
+const numberOfFactor = (n) => {
+  const map = {};
+  for (let i = 2; i * i <= n; i++) {
+    while (!(n % i)) {
+      n /= i;
+      map[i] ? map[i]++ : (map[i] = 1);
     }
-    if (n > 1) map[n] = 1;
-    
-    return map;
-}
+  }
+
+  if (n > 1) map[n] = 1;
+
+  return map;
+};
