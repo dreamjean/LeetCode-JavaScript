d10cb65e60325eda7e6f762f3f1166d70b2f690c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-22 07:58:13 +0800

    Time: 277 ms (12.63%), Space: 52.3 MB (30.53%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+var nearestExit = function (maze, entrance) {
+  const [m, n] = [maze.length, maze[0].length];
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  const queue = [[...entrance]];
+  let steps = -1;
+
+  while (queue.length) {
+    steps++;
+    const size = queue.length;
+    for (let i = size - 1; i >= 0; i--) {
+      const [r, c] = queue.shift();
+      if (
+        (r !== entrance[0] || c !== entrance[1]) &&
+        (!r || r === m - 1 || !c || c === n - 1)
+      )
+        return steps;
+
+      for (const [dx, dy] of dirs) {
+        const [x, y] = [dx + r, dy + c];
+        if (x < 0 || x >= m || y < 0 || y >= n || maze[x][y] !== ".") continue;
+
+        maze[x][y] = "+";
+        queue.push([x, y]);
+      }
+    }
+  }
+
+  return -1;
+};
