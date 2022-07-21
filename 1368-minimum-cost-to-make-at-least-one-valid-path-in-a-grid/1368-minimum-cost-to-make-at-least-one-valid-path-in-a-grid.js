8c87c0f4142b618b54fee258f591942017ed4429
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-22 05:19:04 +0800

    Time: 312 ms (40.00%), Space: 53.1 MB (20.00%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minCost = function(grid) {
+    const [m, n] = [grid.length, grid[0].length];
+    const visited = Array.from({ length: m }, () => new Array(n).fill(false));
+    const dirs = [[0, 1, 1], [0, -1, 2], [1, 0, 3], [-1, 0, 4]];
+    const pq = new MinPriorityQueue({ priority: x => x[0] });
+    pq.enqueue([0, 0, 0]);
+    
+    while (!pq.isEmpty()) {
+        const [cost, r, c] = pq.dequeue().element;
+        if (r === m - 1 && c === n - 1) return cost;
+        
+        visited[r][c] = true;
+        
+        for (const [dx, dy, dr] of dirs) {
+            const [x, y] = [dx + r, dy + c];
+            if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) continue;
+            
+            grid[r][c] === dr ? pq.enqueue([cost, x, y]) : pq.enqueue([cost + 1, x, y]);
+        }
+    }
+};
