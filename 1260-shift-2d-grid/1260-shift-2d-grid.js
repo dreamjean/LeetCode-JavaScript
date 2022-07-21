e4284d23cca962275125069db004bc1d67d7a6a5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-22 06:12:53 +0800

    Time: 201 ms (8.51%), Space: 47.9 MB (40.43%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.js
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -3,15 +3,15 @@
  * @param {number} k
  * @return {number[][]}
  */
-var shiftGrid = function(grid, k) {
-    const [m, n] = [grid.length, grid[0].length];
-    const arr = grid.flat();
-    const ans = [];
-    k %= m * n;
-    
-    arr.unshift(...arr.splice(m * n - k))
-    
-    while (arr.length) ans.push(arr.splice(0, n));
-    
-    return ans;
+var shiftGrid = function (grid, k) {
+  const [m, n] = [grid.length, grid[0].length];
+  const arr = grid.flat();
+  const ans = [];
+  k %= m * n;
+
+  arr.unshift(...arr.splice(m * n - k));
+
+  while (arr.length) ans.push(arr.splice(0, n));
+
+  return ans;
 };
