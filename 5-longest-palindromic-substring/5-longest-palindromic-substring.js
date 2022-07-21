9653e8c2ca794f85b9c9add659c9e63677f6f075
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-21 13:02:04 +0800

    Time: 160 ms (67.40%), Space: 43.1 MB (95.69%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -2,24 +2,24 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-    let [start, maxLen] = [0, 0];
-    const n = s.length;
-    
-    const expendAroundMiddle = (l, r) => {
-        while (l >= 0 && r < n && s[l] === s[r]) {
-            const currLen = r - l + 1;
-            if (currLen > maxLen) [start, maxLen] = [l, currLen];
-            
-            l--;
-            r++;
-        }
+var longestPalindrome = function (s) {
+  let [start, maxLen] = [0, 0];
+  const n = s.length;
+
+  const expendAroundMiddle = (l, r) => {
+    while (l >= 0 && r < n && s[l] === s[r]) {
+      const currLen = r - l + 1;
+      if (currLen > maxLen) [start, maxLen] = [l, currLen];
+
+      l--;
+      r++;
     }
-    
-    for (let i = 0; i < n; i++) {
-        expendAroundMiddle(i, i);
-        expendAroundMiddle(i, i + 1);
-    }
-    
-    return s.slice(start, start + maxLen);
+  };
+
+  for (let i = 0; i < n; i++) {
+    expendAroundMiddle(i, i);
+    expendAroundMiddle(i, i + 1);
+  }
+
+  return s.slice(start, start + maxLen);
 };
