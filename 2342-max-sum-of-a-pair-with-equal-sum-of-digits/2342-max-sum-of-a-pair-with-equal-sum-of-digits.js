5efd917dfbe8d26a277585141f82e40f75df9701
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-23 20:33:50 +0800

    Time: 163 ms (100.00%), Space: 54.2 MB (100.00%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum = function (nums) {
+  const numOfSum = new Array(82).fill(0);
+  let res = -1;
+
+  for (let num of nums) {
+    const index = digitsSum(num);
+    if (numOfSum[index]) res = Math.max(res, numOfSum[index] + num);
+
+    numOfSum[index] = Math.max(numOfSum[index], num);
+  }
+
+  return res;
+};
+
+const digitsSum = (num) => {
+  let sum = 0;
+  while (num) {
+    sum += num % 10;
+    num = ~~(num / 10);
+  }
+
+  return sum;
+};
