edb56b913786735749e59ae6d59e5a3851ac1a4e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-23 19:42:27 +0800

    Time: 331 ms (45.45%), Space: 49.6 MB (75.76%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
@@ -3,28 +3,27 @@
  * @param {number} target
  * @return {number}
  */
-var numSubmatrixSumTarget = function(matrix, target) {
-    const [m, n] = [matrix.length, matrix[0].length];
-    const counter = new Map();
-    let ans = 0;
-    
-    for (let i = 0; i < m; i++) 
-        for (let j = 1; j < n; j++)
-            matrix[i][j] += matrix[i][j - 1];
-    
-    for (let i = 0; i < n; i++) {
-        for (let j = i; j < n; j++) {
-            counter.clear();
-            counter.set(0, 1);
-            let curr = 0;
-            
-            for (let k = 0; k < m; k++) {
-                curr += matrix[k][j] - (i ? matrix[k][i - 1] : 0);
-                ans += counter.get(curr - target) || 0;
-                counter.set(curr, (counter.get(curr) || 0) + 1);
-            }
-        }
+var numSubmatrixSumTarget = function (matrix, target) {
+  const [m, n] = [matrix.length, matrix[0].length];
+  const counter = new Map();
+  let ans = 0;
+
+  for (let i = 0; i < m; i++)
+    for (let j = 1; j < n; j++) matrix[i][j] += matrix[i][j - 1];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      counter.clear();
+      counter.set(0, 1);
+      let curr = 0;
+
+      for (let k = 0; k < m; k++) {
+        curr += matrix[k][j] - (i ? matrix[k][i - 1] : 0);
+        ans += counter.get(curr - target) || 0;
+        counter.set(curr, (counter.get(curr) || 0) + 1);
+      }
     }
-    
-    return ans;
+  }
+
+  return ans;
 };
