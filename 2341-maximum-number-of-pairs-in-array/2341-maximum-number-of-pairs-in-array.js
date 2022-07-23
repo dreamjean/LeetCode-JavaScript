5127026a0d748b542c0c1491e77c477e7ebad1bb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-23 21:14:59 +0800

    Time: 71 ms (100.00%), Space: 41.7 MB (100.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
@@ -6,7 +6,7 @@ var numberOfPairs = function (nums) {
   const cnt = new Array(101).fill(0);
   let pairs = 0;
 
-  nums.forEach((num) => (pairs += ++cnt[num] % 2 === 0 ? 1 : 0));
+  for (let num of nums) pairs += ++cnt[num] % 2 === 0 ? 1 : 0;
 
   return [pairs, nums.length - pairs * 2];
 };
