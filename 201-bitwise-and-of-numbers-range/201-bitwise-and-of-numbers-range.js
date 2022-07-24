f78df266e685cf8372dd56a360ceee553ced7f7b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 05:58:10 +0800

    Time: 225 ms (52.69%), Space: 48.6 MB (74.19%) - LeetHub

---
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function (left, right) {
+  while (left < right) right &= right - 1;
+
+  return left & right;
+};
