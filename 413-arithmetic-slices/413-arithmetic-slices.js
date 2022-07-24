953d23394634b60b80c70ab08ae2ad9c4daaadae
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 07:28:47 +0800

    Time: 92 ms (44.09%), Space: 43.9 MB (6.07%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.js
+++ 413-arithmetic-slices/413-arithmetic-slices.js
@@ -6,12 +6,11 @@ var numberOfArithmeticSlices = function (nums) {
   const n = nums.length;
   if (n < 3) return 0;
 
-  let [ans, count, diff] = [0, 0, nums[1] - nums[0]];
-
+  let [diff, count, res] = [nums[1] - nums[0], 0, 0];
   for (let i = 2; i < n; i++) {
     const currDiff = nums[i] - nums[i - 1];
-    currDiff === diff ? (ans += ++count) : ([diff, count] = [currDiff, 0]);
+    currDiff === diff ? (res += ++count) : ([diff, count] = [currDiff, 0]);
   }
 
-  return ans;
+  return res;
 };
