be676f55f0c62077d0dc18188e322ed96168b307
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-24 22:03:51 +0800

    Time: 108 ms (14.47%), Space: 41.6 MB (98.68%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.js
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.js
@@ -4,11 +4,14 @@
  * @param {number} destination
  * @return {number}
  */
-var distanceBetweenBusStops = function(distance, start, destination) {
-    const [min, max] = [Math.min(start, destination), Math.max(start, destination)];
-    let [res1, res2] = [0, 0];
-    
-    distance.forEach((num, i) => i >= min && i < max ? res1 += num : res2 += num);
-    
-    return Math.min(res1, res2);
+var distanceBetweenBusStops = function (distance, start, destination) {
+  let [res1, res2] = [0, 0];
+
+  if (start > destination) [start, destination] = [destination, start];
+
+  distance.forEach((val, i) =>
+    i >= start && i < destination ? (res1 += val) : (res2 += val)
+  );
+
+  return Math.min(res1, res2);
 };
