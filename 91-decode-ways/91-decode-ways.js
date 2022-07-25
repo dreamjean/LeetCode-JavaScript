541ea1a3b73491d0983b898e4e21b2e8be75a094
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 08:05:08 +0800

    Time: 74 ms (85.41%), Space: 42.4 MB (72.32%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -2,18 +2,20 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
-    const n = s.length;
-    let [last1, last2] = [1, 1];
-    
-    for (let i = 0; i < n; i++) {
-        if (s[i] === '0') last1 = 0;
-        if (s[i - 1] === '1' || (s[i - 1] === '2' && s[i] <= '6')) {
-            last1 += last2;
-            last2 = last1 - last2;
-        }
-        else last2 = last1;
-    }
-    
-    return last1;
+var numDecodings = function (s) {
+  const n = s.length;
+  let [last1, last2] = [1, 1];
+
+  if (s[0] === "0") return 0;
+  if (n === 1) return 1;
+
+  for (let i = 1; i < n; i++) {
+    if (s[i] === "0") last1 = 0;
+    if (s[i - 1] === "1" || (s[i - 1] === "2" && s[i] <= "6")) {
+      last1 += last2;
+      last2 = last1 - last2;
+    } else last2 = last1;
+  }
+
+  return last1;
 };
