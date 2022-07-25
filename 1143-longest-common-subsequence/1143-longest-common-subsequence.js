78a33f9872625b62e7a33424ce23c162a64b7618
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 22:21:29 +0800

    Time: 150 ms (52.78%), Space: 51.8 MB (90.58%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -3,15 +3,17 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(text1, text2) {
-    const [m, n] = [text1.length, text2.length];
-    const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1], dp[i][j] + (text1[i] === text2[j] ? 1 : 0));
-        }
-    }
-    
-    return dp[m][n];
+var longestCommonSubsequence = function (text1, text2) {
+  const [m, n] = [text1.length, text2.length];
+  const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+  for (let i = 0; i < m; i++)
+    for (j = 0; j < n; j++)
+      dp[i + 1][j + 1] = Math.max(
+        dp[i + 1][j],
+        dp[i][j + 1],
+        dp[i][j] + +(text1[i] === text2[j])
+      );
+
+  return dp[m][n];
 };
