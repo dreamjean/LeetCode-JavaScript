66d608eab9711e8d0b311eb53bfd234b655004e3
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 08:16:17 +0800

    Time: 134 ms (67.76%), Space: 44.3 MB (38.16%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.js
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.js
@@ -2,26 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNumberOfLIS = function (nums) {
-  const n = nums.length;
-  const dp = new Array(n).fill(1);
-  const cnt = new Array(n).fill(0);
-  let [res, max] = [0, 1];
+var findNumberOfLIS = function(nums) {
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+    const cnt = new Array(n).fill(0);
+    let [res, max] = [0, 1];
+    
+    for (let i = 0; i < n; i++) {
+        let count = 1;
+        for (let j = 0; j < i; j++) {
+            if (nums[i] <= nums[j]) continue;
+            
+            if(dp[j] + 1 > dp[i]) [dp[i], count] = [dp[j] + 1, cnt[j]]
+            else if (dp[j] + 1 === dp[i]) count += cnt[j];
+        }
+        
+        cnt[i] = count;
 
-  for (let i = 0; i < n; i++) {
-    let count = 1;
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        if (dp[j] + 1 > dp[i]) [count, dp[i]] = [cnt[j], dp[j] + 1];
-        else if (dp[j] + 1 === dp[i]) count += cnt[j];
-      }
+        if (dp[i] > max) [res, max] = [count, dp[i]];
+        else if (dp[i] === max) res += count;
     }
-
-    cnt[i] = count;
-
-    if (dp[i] > max) [res, max] = [count, dp[i]];
-    else if (dp[i] === max) res += count;
-  }
-
-  return res;
+    
+    return res;
 };
