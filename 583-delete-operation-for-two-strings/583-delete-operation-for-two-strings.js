b7a304416c1aa25cf54ded4b0808f4b054a12680
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 08:35:01 +0800

    Time: 180 ms (29.33%), Space: 48 MB (69.82%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
@@ -3,15 +3,17 @@
  * @param {string} word2
  * @return {number}
  */
-var minDistance = function(word1, word2) {
-    const [m, n] = [word1.length, word2.length];
-    const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1], dp[i][j] + (word1[i] === word2[j] ? 1 : 0));
-        }
-    }
-    
-    return m + n - 2 * dp[m][n];
+var minDistance = function (word1, word2) {
+  const [m, n] = [word1.length, word2.length];
+  const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+  for (let i = 0; i < m; i++)
+    for (let j = 0; j < n; j++)
+      dp[i + 1][j + 1] = Math.max(
+        dp[i + 1][j],
+        dp[i][j + 1],
+        dp[i][j] + +(word1[i] === word2[j])
+      );
+
+  return m + n - 2 * dp[m][n];
 };
