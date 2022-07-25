a090a88975c8631cfacce0ecd133e62bebae875e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-25 22:17:33 +0800

    Time: 90 ms (47.33%), Space: 42.6 MB (50.51%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,21 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-    let [start, end] = [0, nums.length - 1];
-    
-    while (start <= end) {
-        const mid = (start + end) >>> 1;
-        if (nums[mid] === target) {
-            [start, end] = [mid, mid];
-            break;
-        }
-        
-        nums[mid] > target ? (end = mid - 1) : (start = mid + 1);
+var searchRange = function (nums, target) {
+  let [start, end] = [0, nums.length - 1];
+
+  while (start <= end) {
+    const mid = (start + end) >>> 1;
+    if (nums[mid] === target) {
+      [start, end] = [mid, mid];
+      break;
     }
-    
-    while (nums[start - 1] === target) start--;
-    while (nums[end + 1] === target) end++;
-    
-    return start > end ? [-1, -1] : [start, end];
+
+    nums[mid] > target ? (end = mid - 1) : (start = mid + 1);
+  }
+
+  while (nums[start - 1] === target) start--;
+  while (nums[end + 1] === target) end++;
+
+  return start > end ? [-1, -1] : [start, end];
 };
