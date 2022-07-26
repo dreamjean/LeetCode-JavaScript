4eb5f24ea4dee6fa779a7f7910470c70ee729820
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-27 04:52:25 +0800

    Time: 70 ms (99.36%), Space: 51.8 MB (49.83%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -11,11 +11,11 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function (root, p, q) {
-  if (!root || root === p || root === q) return root;
-
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-
-  return !left ? right : !right ? left : root;
+var lowestCommonAncestor = function(root, p, q) {
+    if (!root || root === p || root === q) return root;
+    
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+    
+    return !left ? right : !right ? left : root;
 };
