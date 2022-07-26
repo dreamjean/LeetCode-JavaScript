32c811ecf434ee6577118a50de1e62ae9cf15940
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-26 16:43:21 +0800

    Time: 93 ms (91.77%), Space: 46.2 MB (81.57%) - LeetHub

--- 72-edit-distance/72-edit-distance.js
+++ 72-edit-distance/72-edit-distance.js
@@ -12,9 +12,11 @@ var minDistance = function (word1, word2) {
 
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
-      if (word1[i - 1] === word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-      else
-        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1,
+        dp[i][j - 1] + 1,
+        dp[i - 1][j - 1] + +(word1[i - 1] !== word2[j - 1])
+      );
     }
   }
 
