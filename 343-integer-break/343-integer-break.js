dbdf6bdc41f9cff076cf375b0c31c61c276e9ca0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-26 19:38:42 +0800

    Time: 91 ms (41.06%), Space: 42.4 MB (24.80%) - LeetHub

---
+++ 343-integer-break/343-integer-break.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+    if (n <= 3) return n - 1;
+    
+    const [a, b] = [~~(n / 3), n % 3];
+    if (!b) return 3 ** a;
+    if (b === 1) return 3 ** (a - 1) * 4;
+    
+    return 3 ** a * 2;
+};
