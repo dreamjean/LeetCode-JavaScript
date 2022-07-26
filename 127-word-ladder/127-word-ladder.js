bd93a503987ada935ae95ad1507d71a20a7072ce
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-27 07:33:44 +0800

    Time: 452 ms (46.06%), Space: 50 MB (73.84%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    const seen = new Set(wordList);
+    const queue = [[beginWord, 1]];
+    
+    if (!seen.has(endWord)) return 0;
+    
+    while (queue.length) {
+        const [word, cost] = queue.shift();
+        if (word === endWord) return cost;
+        
+        for (let i = 0; i < word.length; i++) {
+            for (let c = 97; c <= 122; c++) {
+                const newWord = word.slice(0, i) + String.fromCharCode(c) + word.slice(i + 1);
+                if (!seen.has(newWord)) continue;
+                
+                queue.push([newWord, cost + 1]);
+                seen.delete(newWord);
+            }
+        }
+    }
+    
+    return 0;
+};
