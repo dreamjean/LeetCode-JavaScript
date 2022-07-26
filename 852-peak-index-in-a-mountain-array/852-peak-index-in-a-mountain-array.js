39e0e1d1245706291024db9f504657a12bbfdb0b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-27 06:33:56 +0800

    Time: 123 ms (6.09%), Space: 51.4 MB (5.02%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -2,13 +2,13 @@
  * @param {number[]} arr
  * @return {number}
  */
-var peakIndexInMountainArray = function(arr) {
-    let [left, right] = [0, arr.length - 1];
-    
-    while (left < right) {
-        const mid = left + ((right - left) >> 1);        
-        arr[mid] < arr[mid + 1] ? (left = mid + 1) : (right = mid);
-    }
-    
-    return left;
+var peakIndexInMountainArray = function (arr) {
+  let [left, right] = [0, arr.length - 1];
+
+  while (left < right) {
+    const mid = left + ((right - left) >> 1);
+    arr[mid] < arr[mid + 1] ? (left = mid + 1) : (right = mid);
+  }
+
+  return left;
 };
