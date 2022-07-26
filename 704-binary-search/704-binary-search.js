44b5b952d6bd5ad6071606a008ebbf6e7fa627d0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-27 05:46:47 +0800

    Time: 106 ms (32.29%), Space: 45.3 MB (32.03%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,15 +3,15 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let [left, right] = [0, nums.length - 1];
-    
-    while (left <= right) {
-        const mid = (left + right) >> 1;
-        if (nums[mid] === target) return mid;
-        
-        nums[mid] > target ? (right = mid - 1) : (left = mid + 1);
-    }
-    
-    return -1;
+var search = function (nums, target) {
+  let [left, right] = [0, nums.length - 1];
+
+  while (left <= right) {
+    const mid = (left + right) >>> 1;
+    if (nums[mid] === target) return mid;
+
+    nums[mid] > target ? (right = mid - 1) : (left = mid + 1);
+  }
+
+  return -1;
 };
