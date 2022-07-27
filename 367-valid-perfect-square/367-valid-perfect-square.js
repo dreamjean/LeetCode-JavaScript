358e73790c03faca87b126fe13ef8aef0738dfbb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-28 05:22:36 +0800

    Time: 131 ms (15.09%), Space: 41.8 MB (62.36%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.js
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -3,15 +3,13 @@
  * @return {boolean}
  */
 var isPerfectSquare = function (num) {
-  if (num === 1) return true;
-
   let [left, right] = [1, num];
 
-  while (left < right) {
+  while (left <= right) {
     const mid = (left + right) >>> 1;
     if (mid * mid === num) return true;
 
-    mid * mid > num ? (right = mid) : (left = mid + 1);
+    mid * mid > num ? (right = mid - 1) : (left = mid + 1);
   }
 
   return false;
