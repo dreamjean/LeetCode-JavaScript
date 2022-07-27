5fd79ee43588b8389bf9c7e75d1ee809b091a617
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-28 06:43:10 +0800

    Time: 170 ms (35.38%), Space: 50.7 MB (51.89%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function (n, trust) {
+  const indegree = new Array(n + 1).fill(0);
+
+  for (const [a, b] of trust) {
+    --indegree[a];
+    ++indegree[b];
+  }
+
+  return indegree.reduce(
+    (acc, curr, i) => (curr === n - 1 ? (acc = i) : acc),
+    -1
+  );
+};
