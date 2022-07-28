eedd4d6f8023b53ba60787476e8fcee50897df27
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-28 08:48:16 +0800

    Time: 83 ms (86.34%), Space: 44.4 MB (51.20%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -5,25 +5,28 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    const [m, n, oldColor] = [image.length, image[0].length, image[sr][sc]];
-    
-    if (oldColor === color) return image;
-    
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    
-    const dfs = (r, c) => {      
-        image[r][c] = color;
-        
-        for (const [dx, dy] of dirs) {
-            const [x, y] = [r + dx, c + dy];
-            if (x < 0 || x >= m || y < 0 || y >= n || image[x][y] !== oldColor) continue;
-            
-            dfs(x, y);
-        }
+var floodFill = function (image, sr, sc, color) {
+  const [m, n, oldColor] = [image.length, image[0].length, image[sr][sc]];
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+
+  if (oldColor === color) return image;
+
+  const dfs = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || image[r][c] !== oldColor) return;
+
+    image[r][c] = color;
+
+    for (const [dx, dy] of dirs) {
+      dfs(dx + r, dy + c);
     }
-    
-    dfs(sr, sc);
-    
-    return image;
+  };
+
+  dfs(sr, sc);
+
+  return image;
 };
