bbe631e1b64823b99f93f8902b66002b888efb22
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-28 08:11:33 +0800

    Time: 153 ms (22.23%), Space: 47.9 MB (24.92%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return s.length === t.length && sorted(s) === sorted(t);
+};
+
+const sorted = str => str.split('').sort().join('');
