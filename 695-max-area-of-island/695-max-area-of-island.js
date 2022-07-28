4ceb518bee826e35faee7b4c9c339877909435d0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-29 05:55:48 +0800

    Time: 137 ms (38.46%), Space: 45.1 MB (63.65%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -2,29 +2,30 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var maxAreaOfIsland = function(grid) {
-    const [m, n] = [grid.length, grid[0].length];
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    let max = 0;
-    
-    const dfs = (r, c) => {
-        if (r < 0 || r >= m || c < 0 || c >= n || !grid[r][c]) return 0;
-        
-        grid[r][c] = 0;
-        let count = 1;
-        
-        for (const [dx, dy] of dirs) {
-            count += dfs(r + dx, c + dy);
-        }
-        
-        return count;
-    }
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1) max = Math.max(max, dfs(i, j));
-        }
-    }
-    
-    return max;
+var maxAreaOfIsland = function (grid) {
+  const [m, n] = [grid.length, grid[0].length];
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  let ans = 0;
+
+  const dfs = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] !== 1) return 0;
+
+    grid[r][c] = 0;
+    let count = 1;
+
+    for (const [dx, dy] of dirs) count += dfs(dx + r, dy + c);
+
+    return count;
+  };
+
+  for (let i = 0; i < m; i++)
+    for (let j = 0; j < n; j++)
+      if (grid[i][j] === 1) ans = Math.max(ans, dfs(i, j));
+
+  return ans;
 };
