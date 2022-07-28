f536764f705486ac4908e62ac1e6ffac0551a7de
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-28 08:52:55 +0800

    Time: 146 ms (37.94%), Space: 44.6 MB (91.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -2,30 +2,34 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
-    const [m, n] = [grid.length, grid[0].length];
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    let count = 0;
-    
-    const dfs = (r, c) => {
-        grid[r][c] = '0';
-        
-        for (const [dx, dy] of dirs) {
-            const [x, y] = [r + dx, c + dy];
-            if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] === '0') continue;
-            
-            dfs(x, y);
-        }
+var numIslands = function (grid) {
+  const [m, n] = [grid.length, grid[0].length];
+  const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  let count = 0;
+
+  const dfs = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] !== "1") return;
+
+    grid[r][c] = "0";
+
+    for (const [dx, dy] of dirs) {
+      dfs(dx + r, dy + c);
     }
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === '1') {
-                dfs(i, j);
-                count++;
-            }
-        }
+  };
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        dfs(i, j);
+        ++count;
+      }
     }
-    
-    return count;
+  }
+
+  return count;
 };
