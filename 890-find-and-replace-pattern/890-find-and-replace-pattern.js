9e355c30fa7495aef83198282bb3ca6c594809b7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-30 05:33:57 +0800

    Time: 69 ms (97.50%), Space: 44.2 MB (62.50%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.js
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.js
@@ -3,18 +3,20 @@
  * @param {string} pattern
  * @return {string[]}
  */
-var findAndReplacePattern = function(words, pattern) {
-    return words.filter(word => replaceStr(word) === replaceStr(pattern));
+var findAndReplacePattern = function (words, pattern) {
+  return words.filter((word) => countCharator(word) === countCharator(pattern));
 };
 
-const replaceStr = str => {
-    const cnt = new Map();
-    const res = [];
-    
-    for (let i = 0; i < str.length; ++i) {
-        if (!cnt.has(str[i])) cnt.set(str[i], cnt.size);
-        res[i] = cnt.get(str[i]);
-    }
-    
-    return res.join('');
-}
+const countCharator = (word) => {
+  const map = new Map();
+  const res = [];
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    if (!map.has(char)) map.set(char, map.size);
+
+    res[i] = map.get(char);
+  }
+
+  return res.join("");
+};
