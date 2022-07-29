62b76df70ab70c463a69c5e59769a989d4b0300d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-29 22:50:20 +0800

    Time: 210 ms (66.67%), Space: 44.9 MB (59.26%) - LeetHub

---
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} circles
+ * @return {number}
+ */
+var countLatticePoints = function (circles) {
+  let [maxX, maxY, count] = [0, 0, 0];
+
+  for (const [x, y, r] of circles)
+    [maxX, maxY] = [Math.max(maxX, x + r), Math.max(maxY, y + r)];
+
+  for (let i = 0; i <= maxX; i++) {
+    for (let j = 0; j <= maxY; j++) {
+      for (const [x, y, r] of circles) {
+        const [a, b] = [x - i, y - j];
+        if (a * a + b * b <= r * r) {
+          count++;
+          break;
+        }
+      }
+    }
+  }
+
+  return count;
+};
