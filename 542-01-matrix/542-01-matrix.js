8fcfae437ad3777ddaee2d93d11892285c39f6c5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-30 19:14:26 +0800

    Time: 202 ms (79.72%), Space: 49.5 MB (90.10%) - LeetHub

--- 542-01-matrix/542-01-matrix.js
+++ 542-01-matrix/542-01-matrix.js
@@ -2,28 +2,27 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var updateMatrix = function(mat) {
-    const [m, n] = [mat.length, mat[0].length];
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (!mat[i][j]) continue;
-                
-            mat[i][j] = Infinity;
-            if (i > 0) mat[i][j] = Math.min(mat[i][j], mat[i - 1][j] + 1);
-            if (j > 0) mat[i][j] = Math.min(mat[i][j], mat[i][j - 1] + 1);
-        }
+var updateMatrix = function (mat) {
+  const [m, n] = [mat.length, mat[0].length];
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!mat[i][j]) continue;
+
+      mat[i][j] = Infinity;
+      if (i > 0) mat[i][j] = Math.min(mat[i][j], mat[i - 1][j] + 1);
+      if (j > 0) mat[i][j] = Math.min(mat[i][j], mat[i][j - 1] + 1);
     }
-    
-    for (let i = m - 1; i >= 0; i--) {
-        for (let j = n - 1; j >= 0; j--) {
-            if (!mat[i][j]) continue;
-            
-            if (i + 1 < m) mat[i][j] = Math.min(mat[i][j], mat[i + 1][j] + 1);
-            if (j + 1 < n) mat[i][j] = Math.min(mat[i][j], mat[i][j + 1] + 1);
-        }
+  }
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      if (!mat[i][j]) continue;
+
+      if (i + 1 < m) mat[i][j] = Math.min(mat[i][j], mat[i + 1][j] + 1);
+      if (j + 1 < n) mat[i][j] = Math.min(mat[i][j], mat[i][j + 1] + 1);
     }
-    
-    return mat;
+  }
+
+  return mat;
 };
