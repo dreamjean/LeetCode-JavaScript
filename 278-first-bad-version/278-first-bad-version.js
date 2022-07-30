ab21dedc0c4a7ae7c2ee1ae2ba10c42ec6ad004a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-07-30 20:12:38 +0800

    Time: 60 ms (94.90%), Space: 41.7 MB (77.98%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -1,6 +1,6 @@
 /**
  * Definition for isBadVersion()
- * 
+ *
  * @param {integer} version number
  * @return {boolean} whether the version is bad
  * isBadVersion = function(version) {
@@ -12,20 +12,19 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        let [start, end] = [0, n];
-        
-        while (start < end) {
-            const mid = (start + end) >>> 1;
-            if (isBadVersion(mid)) end = mid;
-            else start = mid + 1;
-        }
-        
-        return start;
-    };
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let [low, high] = [1, n];
+
+    while (low <= high) {
+      const mid = (low + high) >>> 1;
+      isBadVersion(mid) ? (high = mid - 1) : (low = mid + 1);
+    }
+
+    return low;
+  };
 };
