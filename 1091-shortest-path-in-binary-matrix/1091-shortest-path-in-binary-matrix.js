10b5ad8d9927af7e1406d0932ae89523c52de120
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-01 07:27:23 +0800

    Time: 196 ms (57.14%), Space: 51 MB (43.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -11,30 +11,31 @@ var shortestPathBinaryMatrix = function (grid) {
     [-1, 0],
     [0, -1],
     [1, 1],
-    [1, -1],
     [-1, 1],
+    [1, -1],
     [-1, -1],
   ];
-  const n = grid.length;
+  const [m, n] = [grid.length, grid[0].length];
   const queue = [[0, 0]];
-  let count = 0;
+  let steps = 1;
 
   while (queue.length) {
-    count++;
-    const size = queue.length;
-    for (let i = size - 1; i >= 0; i--) {
+    let size = queue.length;
+    while (size--) {
       const [r, c] = queue.shift();
-      if (r === n - 1 && c === n - 1) return count;
+      if (r === m - 1 && c === n - 1) return steps;
 
       for (const [dx, dy] of dirs) {
         const [x, y] = [dx + r, dy + c];
-        if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] === 1) continue;
+        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] === 1) continue;
 
         grid[x][y] = 1;
         queue.push([x, y]);
       }
     }
+
+    steps++;
   }
 
-  return - 1;
+  return -1;
 };
