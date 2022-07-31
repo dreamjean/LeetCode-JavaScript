5f965ddf3cd4e1760bef250459fba692147d5c53
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-01 07:35:38 +0800

    Time: 78 ms (78.69%), Space: 41.3 MB (98.67%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,15 +2,16 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if (n <= 1) return n;
-    
-    let [a, b] = [0, 1];
-    for (let i = 2; i <= n; i++) {
-        let sum = a + b;
-        a = b;
-        b = sum;
-    }
-    
-    return b;
+var fib = function (n) {
+  if (n < 2) return n;
+
+  let [a, b] = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const sum = a + b;
+    a = b;
+    b = sum;
+  }
+
+  return b;
 };
