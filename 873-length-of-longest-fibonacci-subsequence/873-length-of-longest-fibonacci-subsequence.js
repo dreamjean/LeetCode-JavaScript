2e9c60b4625344e6cd1a5ffcf56ca50f1300dc37
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-01 08:01:46 +0800

    Time: 545 ms (62.50%), Space: 49.3 MB (31.25%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.js
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.js
@@ -4,20 +4,19 @@
  */
 var lenLongestFibSubseq = function(arr) {
     const n = arr.length;
-    const dp = Array.from({ length: n }, () => new Array(n).fill(0));
     const set = new Set(arr);
     let ans = 0;
     
     for (let i = 0; i < n; ++i) {
         for (let j = i + 1; j < n; ++j) {
-            let [num1, num2, l] = [arr[i], arr[j], 2];
+            let [num1, num2, k] = [arr[i], arr[j], 2];
             while (set.has(num1 + num2)) {
                 num2 += num1;
                 num1 = num2 - num1;
-                l++;
+                k++;
             }
             
-            ans = Math.max(ans, l);
+            ans = Math.max(ans, k);
         }
     }
     
