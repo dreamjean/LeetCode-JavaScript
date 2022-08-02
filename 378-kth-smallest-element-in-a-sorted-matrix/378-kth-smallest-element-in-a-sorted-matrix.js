d8b327e68727dccb6473f77973f68d28a5acf1dd
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-03 05:43:56 +0800

    Time: 99 ms (84.47%), Space: 44.7 MB (93.59%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
@@ -3,27 +3,29 @@
  * @param {number} k
  * @return {number}
  */
-var kthSmallest = function (matrix, k) {
-  const n = matrix.length;
+var kthSmallest = function(matrix, k) {
+    const n = matrix.length;
   let [low, high] = [matrix[0][0], matrix[n - 1][n - 1]];
-
+  
   while (low < high) {
     const mid = low + ((high - low) >> 1);
-    if (isValid(mid, k, n, matrix)) low = mid + 1;
+    if (isValid(k, n, mid, matrix)) low = mid + 1;
     else high = mid;
   }
-
+  
   return low;
 };
 
-const isValid = (mid, k, n, matrix) => {
+const isValid = (k, n, mid, matrix) => {
   let [i, j, count] = [n - 1, 0, 0];
+  
   while (i >= 0 && j < n) {
     if (matrix[i][j] <= mid) {
       count += i + 1;
       j++;
-    } else i--;
+    }
+    else i--;
   }
-
+  
   return count < k;
-};
+}
