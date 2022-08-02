e097c1b701c1fe5f0b4732244e1d083b9547176d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-03 07:01:12 +0800

    Time: 82 ms (89.11%), Space: 43.6 MB (49.92%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const cnt1 = new Array(256).fill(0);
+  const cnt2 = new Array(256).fill(0);
+  
+  for (let i = 0; i < s.length; i++) {
+    const [key1, key2] = [s[i].charCodeAt(), t[i].charCodeAt()];
+    if (cnt1[key1] !== cnt2[key2]) return false;
+    
+    cnt1[key1] = i + 1;
+    cnt2[key2] = i + 1;
+  }
+  
+  return true;
+};
+
+
