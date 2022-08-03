9b2f474870c113ea695060db8b2ebd1dd6d2d420
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-04 06:19:31 +0800

    Time: 275 ms (46.67%), Space: 51.5 MB (25.00%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.js
+++ 729-my-calendar-i/729-my-calendar-i.js
@@ -1,23 +1,22 @@
-
-var MyCalendar = function() {
-    this.books = [];
+var MyCalendar = function () {
+  this.books = [];
 };
 
-/** 
- * @param {number} start 
+/**
+ * @param {number} start
  * @param {number} end
  * @return {boolean}
  */
-MyCalendar.prototype.book = function(start, end) {
-    for (const [first, last] of this.books) 
-        if (Math.max(start, first) < Math.min(end, last)) return false;
-    
-    this.books.push([start, end]);
-    
-    return true;
+MyCalendar.prototype.book = function (start, end) {
+  if (this.books.length)
+    for (let [a, b] of this.books)
+      if (Math.max(a, start) < Math.min(b, end)) return false;
+
+  this.books.push([start, end]);
+  return true;
 };
 
-/** 
+/**
  * Your MyCalendar object will be instantiated and called as such:
  * var obj = new MyCalendar()
  * var param_1 = obj.book(start,end)
