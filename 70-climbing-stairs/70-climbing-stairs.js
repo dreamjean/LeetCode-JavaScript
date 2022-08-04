bbb0b1d0a7b2b02c1273d9349288d82711e4c4a2
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-04 08:36:14 +0800

    Time: 70 ms (79.11%), Space: 42.4 MB (6.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,13 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    let [p, q, r] = [0, 0, 1];
-    
-    for (let i = 1; i <= n; ++i) {
-        [p, q] = [q, r];
-        r = p + q;
-    }
-    
-    return r;
+var climbStairs = function (n) {
+  let [p, q, r] = [0, 0, 1];
+
+  for (let i = 1; i <= n; ++i) {
+    [p, q] = [q, r];
+    r = p + q;
+  }
+
+  return r;
 };
