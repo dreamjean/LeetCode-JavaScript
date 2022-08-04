47acb2cdee6b6746957346a3b388c8f291899f58
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-04 20:55:41 +0800

    Time: 73 ms (96.74%), Space: 45.2 MB (72.81%) - LeetHub

--- 135-candy/135-candy.js
+++ 135-candy/135-candy.js
@@ -6,10 +6,10 @@ var candy = function (ratings) {
   const n = ratings.length;
   const children = new Array(n).fill(1);
 
-  for (let i = 1; i < n; i++)
+  for (let i = 1; i < n; ++i)
     if (ratings[i - 1] < ratings[i]) children[i] = children[i - 1] + 1;
 
-  for (let i = n - 1; i > 0; i--)
+  for (let i = n - 1; i > 0; --i)
     if (ratings[i - 1] > ratings[i])
       children[i - 1] = Math.max(children[i - 1], children[i] + 1);
 
