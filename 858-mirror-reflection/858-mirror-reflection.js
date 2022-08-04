bbbd2a74c1be6724d566d532104ed060ac8fe539
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-05 06:19:56 +0800

    Time: 84 ms (33.33%), Space: 42.2 MB (33.33%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+var mirrorReflection = function(p, q) {
+  while (!(p % 2) && !(q % 2)) {
+    p >>= 1;
+    q >>= 1;
+  }
+  
+  return 1 - p % 2 + q % 2;
+};
