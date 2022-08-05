64e56e56c276ef581c9b90d793a478af21fbe115
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-05 16:46:16 +0800

    Time: 85 ms (55.25%), Space: 42.1 MB (45.82%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,14 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    let [prev1, prev2] = [0, 0]
-    
-    for (let num of nums) {
-        const tmp = prev1;
-        prev1 = Math.max(prev2 + num, prev1);
-        prev2 = tmp;
-    }
-    
-    return prev1;
+var rob = function (nums) {
+  let [prev1, prev2] = [0, 0];
+
+  for (let num of nums) [prev1, prev2] = [prev2, Math.max(prev2, prev1 + num)];
+
+  return prev2;
 };
