b52910d07e82ee16cf793a62c3e8da2e0464c6b4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-05 23:37:00 +0800

    Time: 159 ms (87.58%), Space: 66.9 MB (60.78%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
@@ -3,25 +3,26 @@
  * @param {number[][]} connections
  * @return {number}
  */
-var makeConnected = function(n, connections) {
-    if (connections.length < n - 1) return -1;
-    
-    const seen = new Array(n).fill(0);
-    const graph = Array.from({ length: n }, () => new Set());
-    let count = 0;
-    
-    for (const [a, b,] of connections) {
-        graph[a].add(b);
-        graph[b].add(a);
+var makeConnected = function (n, connections) {
+  if (connections.length < n - 1) return -1;
+
+  const graph = Array.from({ length: n }, () => new Set());
+  const seen = new Array(n).fill(0);
+  let count = 0;
+
+  const dfs = (curr) => {
+    for (let next of graph[curr]) {
+      if (!seen[next]++) dfs(next);
     }
-    
-    const dfs = (curr) => {
-        for (const next of graph[curr]) 
-            if (!seen[next]++) dfs(next);
-    }
-    
-    for (let i = 0; i < n; i++) 
-        if (!seen[i]++ && ++count) dfs(i);
-    
-    return count - 1;
+  };
+
+  for (let [a, b] of connections) {
+    graph[a].add(b);
+    graph[b].add(a);
+  }
+
+  for (let i = 0; i < n; i++) 
+    if (!seen[i]++ && ++count) dfs(i);
+
+  return count - 1;
 };
