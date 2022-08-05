e3a0e6f9c246ad13e97a016cd70ce70bcc5949b4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-05 21:34:28 +0800

    Time: 132 ms (9.98%), Space: 44 MB (77.60%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.js
+++ 841-keys-and-rooms/841-keys-and-rooms.js
@@ -2,21 +2,23 @@
  * @param {number[][]} rooms
  * @return {boolean}
  */
-var canVisitAllRooms = function(rooms) {
-    const n = rooms.length;
-    const visited = new Array(n).fill(false);
-    const queue = [rooms[0]];
-    visited[0] = true;
-    
-    while (queue.length) {
-        const curr = queue.shift();
-        for (let nextIndex of curr) {
-            if (visited[nextIndex]) continue;
-            
-            visited[nextIndex] = true;
-            queue.push(rooms[nextIndex]);
-        }
+var canVisitAllRooms = function (rooms) {
+  const n = rooms.length;
+  const set = new Set([0]);
+  const queue = [0];
+
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const curr = queue.shift();
+      for (let next of rooms[curr]) {
+        if (set.has(next)) continue;
+
+        set.add(next);
+        queue.push(next);
+      }
     }
-    
-    return visited.every(i => i);
+  }
+
+  return set.size === n;
 };
