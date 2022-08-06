3a252bea1a2ad41cbd43181894374b5ffc78ebb8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-06 19:07:21 +0800

    Time: 93 ms (45.47%), Space: 42.7 MB (12.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -3,23 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let [left, right] = [0, nums.length - 1];
-    
-    while (left <= right) {
-        const mid = (left + right) >>> 1;
-        if(nums[mid] === target) return mid;
-        
-        if (nums[mid] > nums[right]) {
-            if (nums[left] <= target && nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-        
-        else {
-            if (nums[mid] < target && nums[right] >= target) left = mid + 1;
-            else right = mid - 1;
-        }
+var search = function (nums, target) {
+  let [l, r] = [0, nums.length - 1];
+
+  while (l <= r) {
+    const mid = (l + r) >>> 1;
+    if (nums[mid] === target) return mid;
+
+    if (nums[mid] > nums[r]) {
+      if (nums[l] <= target && target < nums[mid]) r = mid - 1;
+      else l = mid + 1;
+    } else {
+      if (nums[mid] < target && target <= nums[r]) l = mid + 1;
+      else r = mid - 1;
     }
-    
-    return -1;
+  }
+
+  return -1;
 };
