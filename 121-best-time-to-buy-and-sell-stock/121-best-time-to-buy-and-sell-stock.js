a632770e9e0dabf2eeec877c9d7e413937713c6a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-06 22:13:38 +0800

    Time: 101 ms (79.58%), Space: 52.1 MB (11.28%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,13 +2,13 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-  let [min, max] = [Infinity, 0];
-
+var maxProfit = function(prices) {
+  let [min, res] = [prices[0], 0];
+  
   for (let price of prices) {
     min = Math.min(min, price);
-    max = Math.max(max, price - min);
+    res = Math.max(res, price - min);
   }
-
-  return max;
+  
+  return res;
 };
