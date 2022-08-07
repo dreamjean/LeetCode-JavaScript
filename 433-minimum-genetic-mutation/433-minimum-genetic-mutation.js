b75856712f6be82b820a72a6389b6a1ec09a3bb3
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-07 23:12:44 +0800

    Time: 116 ms (7.35%), Space: 44 MB (5.88%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function (start, end, bank) {
+  const seen = new Set(bank);
+  const queue = [[start, 0]];
+
+  while (queue.length) {
+    const [gen, cost] = queue.shift();
+    if (gen === end) return cost;
+
+    for (let i = 0; i < gen.length; i++) {
+      for (let c = 65; c <= 122; c++) {
+        const newGen =
+          gen.slice(0, i) + String.fromCharCode(c) + gen.slice(i + 1);
+        if (!seen.has(newGen)) continue;
+
+        seen.delete(newGen);
+        queue.push([newGen, cost + 1]);
+      }
+    }
+  }
+
+  return -1;
+};
