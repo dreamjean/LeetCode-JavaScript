a388ae5b8ea0ab5bdc9a8c9a7e0037fee332d573
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-10 07:35:23 +0800

    Time: 133 ms (71.11%), Space: 49.1 MB (77.78%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.js
+++ 1615-maximal-network-rank/1615-maximal-network-rank.js
@@ -12,12 +12,14 @@ var maximalNetworkRank = function (n, roads) {
     connect[b].add(a);
   }
 
-  for (let i = 0; i < n; i++)
-    for (let j = i + 1; j < n; j++)
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
       ans = Math.max(
         ans,
         connect[i].size + connect[j].size - +connect[i].has(j)
       );
+    }
+  }
 
   return ans;
 };
