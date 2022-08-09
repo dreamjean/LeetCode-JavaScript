ea1878d852b3e18268094dc02674e4f39cfae903
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-10 06:21:33 +0800

    Time: 121 ms (40.59%), Space: 50.3 MB (62.36%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.js
+++ 1306-jump-game-iii/1306-jump-game-iii.js
@@ -3,20 +3,11 @@
  * @param {number} start
  * @return {boolean}
  */
-var canReach = function (arr, start) {
-  const n = arr.length;
-  const queue = [start];
-
-  while (queue.length) {
-    const i = queue.shift();
-    if (arr[i] < 0) continue;
-    if (!arr[i]) return true;
-
-    arr[i] *= -1;
-    const [left, right] = [i - arr[i], i + arr[i]];
-    if (left >= 0 && arr[left] >= 0) queue.push(left);
-    if (right < n && arr[right] >= 0) queue.push(right);
-  }
-
-  return false;
+var canReach = function(arr, start) {
+  if (start < 0 || start >= arr.length || arr[start] < 0) return false;
+  if (!arr[start]) return true;
+  
+  arr[start] *= -1;
+  
+  return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
 };
