bb642955231afb1f0eed09aa625f8abda88acb39
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-10 07:58:23 +0800

    Time: 144 ms (10.98%), Space: 44.7 MB (68.63%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.js
+++ 785-is-graph-bipartite/785-is-graph-bipartite.js
@@ -2,28 +2,27 @@
  * @param {number[][]} graph
  * @return {boolean}
  */
-var isBipartite = function (graph) {
+var isBipartite = function(graph) {
   const n = graph.length;
   const colors = new Array(n).fill(0);
-
+  
   for (let i = 0; i < n; i++) {
     if (colors[i]) continue;
-
+    
     colors[i] = 1;
     const queue = [i];
-
+    
     while (queue.length) {
-      const node = queue[0];
-      for (const next of graph[node]) {
+      const curr = queue.shift();
+      for (let next of graph[curr]) {
         if (!colors[next]) {
-          colors[next] = -colors[node] ^ 1;
+          colors[next] = -colors[curr] ^ 1;
           queue.push(next);
-        } else if (colors[next] === colors[node]) return false;
+        }
+        else if (colors[next] === colors[curr]) return false;
       }
-
-      queue.shift();
     }
   }
-
+  
   return true;
 };
