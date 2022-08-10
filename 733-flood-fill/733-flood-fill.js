5b8e146c44643b1f2366092ff563ddf7803ec8c3
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-11 06:21:11 +0800

    Time: 98 ms (68.57%), Space: 44.6 MB (26.35%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -5,28 +5,22 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function (image, sr, sc, color) {
+var floodFill = function(image, sr, sc, color) {
   const [m, n, oldColor] = [image.length, image[0].length, image[sr][sc]];
-  const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
-
+  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+  
   if (oldColor === color) return image;
-
+  
   const dfs = (r, c) => {
     if (r < 0 || r >= m || c < 0 || c >= n || image[r][c] !== oldColor) return;
-
+    
     image[r][c] = color;
-
-    for (const [dx, dy] of dirs) {
+    
+    for (const [dx, dy] of dirs) 
       dfs(dx + r, dy + c);
-    }
-  };
-
+  }
+  
   dfs(sr, sc);
-
+  
   return image;
 };
