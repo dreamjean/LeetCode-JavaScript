29613ca8fa5547d1c943099b95dec60cbb59cc64
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-11 07:24:14 +0800

    Time: 68 ms (85.52%), Space: 42.1 MB (18.67%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.js
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var numTrees = function(n) {
-    let res = 1;
-    
-    for (let i = 0; i < n; ++i) 
-        res = res * 2 * (2 * i + 1) / (i + 2);
-    
-    return res;   
+  let res = 1;
+  
+  for (let i = 1; i < n; i++) 
+    res *= 2 * (2 * i + 1) / (i + 2);
+  
+  return res;
 };
