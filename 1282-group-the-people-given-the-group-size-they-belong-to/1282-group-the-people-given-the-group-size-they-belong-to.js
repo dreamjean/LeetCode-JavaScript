4b770642b6159c1225507bb02076060248e3fec6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-12 10:08:39 +0800

    Time: 120 ms (76.04%), Space: 45.7 MB (96.35%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.js
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.js
@@ -5,16 +5,13 @@
 var groupThePeople = function (groupSizes) {
   const map = {};
 
-  groupSizes.forEach((num, i) =>
-    map[num] ? map[num].push(i) : (map[num] = [i])
-  );
+  return groupSizes.reduce((acc, curr, i) => {
+    map[curr] ? map[curr].push(i) : (map[curr] = [i]);
 
-  return Object.entries(map).reduce((acc, [num, cnt]) => {
-    if (cnt.length > +num) {
-      while (cnt.length > +num) acc.push(cnt.splice(0, +num));
-
-      acc.push(cnt);
-    } else acc.push(cnt);
+    if (map[curr].length === curr) {
+      acc.push(map[curr]);
+      delete map[curr];
+    }
 
     return acc;
   }, []);
