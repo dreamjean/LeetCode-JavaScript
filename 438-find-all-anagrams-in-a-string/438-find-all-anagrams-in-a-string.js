1da23168d12219dc8d49fafbc27bf7b43024d0df
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-12 23:01:05 +0800

    Time: 123 ms (85.59%), Space: 45 MB (86.60%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -3,21 +3,21 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
-    const map = {};
-    const ans = [];
-    let [l, r, n] = [0, 0, p.length];
-    
-    p.split('').forEach(char => map[char] ? map[char]++ : map[char] = 1);
-    
-    while (r < s.length) {
-        if (map[s[r++]]-- > 0) n--;
-        if (!n) ans.push(l);
-        
-        while (r - l === p.length) {
-            if (++map[s[l++]] > 0) n++;     
-        }
-    }
-    
-    return ans;
+var findAnagrams = function (s, p) {
+  const cnt = new Array(26).fill(0);
+  const a = "a".charCodeAt();
+  const ans = [];
+  let [l, r, count] = [0, 0, p.length];
+
+  for (let ch of p) cnt[ch.charCodeAt() - a]++;
+
+  while (r < s.length) {
+    if (cnt[s.charCodeAt(r++) - a]-- > 0) count--;
+    if (!count) ans.push(l);
+
+    while (r - l === p.length) 
+      if (++cnt[s.charCodeAt(l++) - a] > 0) count++;
+  }
+
+  return ans;
 };
