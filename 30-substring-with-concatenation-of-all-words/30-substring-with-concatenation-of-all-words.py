587877d15399f831fb982612b194827bbba4bd7c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-14 06:28:49 +0800

    Time: 866 ms (41.44%), Space: 14.1 MB (77.75%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        cnts, size = len(words), len(words[0])
+        limit = cnts * size
+        mp = Counter(words)
+        return [i for i in range(len(s)-limit+1) if Counter([s[j:j+size] for j in range(i, i+limit, size)]) == mp]
