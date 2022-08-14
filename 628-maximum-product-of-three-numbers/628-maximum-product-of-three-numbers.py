d1930db7eb5c2416702876021913e7bf463b616e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-14 14:27:06 +0800

    Time: 443 ms (37.26%), Space: 15.2 MB (67.10%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
@@ -1,18 +1,16 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maximumProduct = function(nums) {
-  let [a, b, c, x, y] = [-Infinity, -Infinity, -Infinity, Infinity, Infinity];
-  
-  for (let num of nums) {
-    if (num > a) [a, b, c] = [num, a, b];
-    else if (num > b) [b, c] = [num, b];
-    else if (num > c) c = num;
-    
-    if (num < x) [x, y] = [num, x];
-    else if (num < y) y = num;
-  }
-  
-  return Math.max(a * b * c, a * x * y);
-};
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        a = b = c = -float('inf')
+        x = y = float('inf')
+        for num in nums:
+            if num > a:
+                a, b, c = num, a, b
+            elif num > b:
+                b, c = num, b
+            elif num > c:
+                c = num
+            if num < x:
+                x, y = num, x
+            elif num < y:
+                y = num
+        return max(a * b * c, a * x * y)
