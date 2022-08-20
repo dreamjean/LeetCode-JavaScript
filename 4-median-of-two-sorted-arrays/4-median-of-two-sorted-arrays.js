6c9fedf9e018c98be96383ddf528c0a9b7a99804
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-20 20:43:57 +0800

    Time: 137 ms (78.89%), Space: 47 MB (72.41%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -3,26 +3,26 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {    
-    if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
-    
-    const [m, n] = [nums1.length, nums2.length];
-    let [left, right, min, max] = [0, m, Infinity, -Infinity];
-    
-    while (left <= right) {
-        const mid1 = (left + right) >>> 1;
-        const mid2 = ((m + n + 1) >> 1) - mid1;
-        
-        const L1 = !mid1 ? max : nums1[mid1 - 1];
-        const L2 = !mid2 ? max : nums2[mid2 - 1];
-        const R1 = mid1 === m ? min : nums1[mid1];
-        const R2 = mid2 === n ? min : nums2[mid2];
-        
-        if (L1 <= R2 && L2 <= R1) {
-            const leftMax = Math.max(L1, L2);
-            return (m + n) % 2 === 1 ? leftMax : (leftMax + Math.min(R1, R2)) / 2;
-        }
-        
-        L1 < R2 ? (left = mid1 + 1) : (right = mid1 - 1);
+var findMedianSortedArrays = function (nums1, nums2) {
+  if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+  const [m, n, min, max] = [nums1.length, nums2.length, Infinity, -Infinity];
+  let [l, r] = [0, m];
+
+  while (l <= r) {
+    const midA = (l + r) >>> 1;
+    const midB = ((m + n + 1) >> 1) - midA;
+
+    const maxA = !midA ? max : nums1[midA - 1];
+    const maxB = !midB ? max : nums2[midB - 1];
+    const minA = midA === m ? min : nums1[midA];
+    const minB = midB === n ? min : nums2[midB];
+
+    if (maxA <= minB && maxB <= minA) {
+      const maxLeft = Math.max(maxA, maxB);
+      return (m + n) % 2 ? maxLeft : (maxLeft + Math.min(minA, minB)) / 2;
     }
+
+    maxA < maxB ? (l = midA + 1) : (r = midA - 1);
+  }
 };
