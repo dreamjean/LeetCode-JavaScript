1a5b59d2e364ed3c5671ae81fde59293ccb89e28
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-20 23:41:13 +0800

    Time: 134 ms (5.01%), Space: 43.3 MB (5.17%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.js
+++ 376-wiggle-subsequence/376-wiggle-subsequence.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var wiggleMaxLength = function(nums) {
-    let [up, down] = [1, 1];
-    
-    for (let i = 1; i < nums.length; i++) {
-        const [prev, curr] = [nums[i - 1], nums[i]];
-        if (prev < curr) up = down + 1;
-        else if (prev > curr) down = up + 1;
-    }
-    
-    return Math.max(up, down);
+var wiggleMaxLength = function (nums) {
+  let [up, down] = [1, 1];
+
+  for (let i = 1; i < nums.length; i++) {
+    const [prev, curr] = [nums[i - 1], nums[i]];
+    if (curr < prev) up = down + 1;
+    else if (curr > prev) down = up + 1;
+  }
+
+  return Math.max(up, down);
 };
