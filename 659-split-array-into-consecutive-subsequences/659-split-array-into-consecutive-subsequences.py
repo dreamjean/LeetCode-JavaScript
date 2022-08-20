76d4c71b3da8ba7ef9bce33663680d4ecf1138d6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-20 08:17:21 +0800

    Time: 803 ms (54.64%), Space: 15.3 MB (66.90%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -1,28 +1,16 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var isPossible = function (nums) {
-  const freq = {};
-  const end = new Map();
-
-  nums.forEach((num) => (freq[num] ? freq[num]++ : (freq[num] = 1)));
-
-  for (let num of nums) {
-    if (!freq[num]) continue;
-
-    freq[num]--;
-    if (end.get(num - 1)) {
-      end.set(num - 1, end.get(num - 1) - 1);
-      end.set(num, (end.get(num) || 0) + 1);
-    } 
-    else if (freq[num + 1] && freq[num + 2]) {
-      freq[num + 1]--;
-      freq[num + 2]--;
-      end.set(num + 2, (end.get(num + 2) || 0) + 1);
-    } 
-    else return false;
-  }
-
-  return true;
-};
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        freq = Counter(nums)
+        end = Counter()
+        for num in nums:
+            if freq[num] == 0: continue
+            freq[num] -= 1
+            if end[num - 1]:
+                end[num - 1] -= 1
+                end[num] += 1
+            elif freq[num + 1] and freq[num + 2]:
+                freq[num + 1] -= 1
+                freq[num + 2] -= 1
+                end[num + 2] += 1
+            else: return False
+        return True
