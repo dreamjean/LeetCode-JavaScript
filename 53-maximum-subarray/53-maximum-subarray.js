c225a07ad0c2376cca364485520ce92fa2f7a7cb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-20 16:01:27 +0800

    Time: 108 ms (70.33%), Space: 50.9 MB (22.18%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
   let [sum, max] = [0, nums[0]];
-  
+
   for (let num of nums) {
-    sum = Math.max(num, num + sum);
+    sum = Math.max(num, sum + num);
     max = Math.max(max, sum);
   }
-  
+
   return max;
 };
