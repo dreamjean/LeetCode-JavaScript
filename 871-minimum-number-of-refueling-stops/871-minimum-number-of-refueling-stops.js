c5132fc1d06875ccabfaabb57d66498ea4741db4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-21 14:09:42 +0800

    Time: 162 ms (33.33%), Space: 46.7 MB (42.86%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.js
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.js
@@ -4,17 +4,20 @@
  * @param {number[][]} stations
  * @return {number}
  */
-var minRefuelStops = function(target, startFuel, stations) {
+var minRefuelStops = function (target, startFuel, stations) {
+  const pq = new MaxPriorityQueue();
   const n = stations.length;
-  const dp = new Array(n + 1).fill(0);
-  dp[0] = startFuel;
-  
-  for (let i = 0; i < n; ++i) 
-    for (let j = i; j <= n && dp[j] >= stations[i][0]; --j)       
-        dp[j + 1] = Math.max(dp[j + 1], dp[j] + stations[i][1]);
-    
-  for (let i = 0; i <= n; i++) 
-    if (dp[i] >= target) return i;
-  
-  return -1;
+  let [currFuel, i, count] = [startFuel, 0, 0];
+
+  while (currFuel < target) {
+    while (i < n && currFuel >= stations[i][0]) 
+      pq.enqueue(stations[i++][1]);
+
+    if (pq.isEmpty()) return -1;
+
+    currFuel += pq.dequeue().element;
+    count++;
+  }
+
+  return count;
 };
