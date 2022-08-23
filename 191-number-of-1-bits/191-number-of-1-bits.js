02b0f7611c2d7f1f7fd8ca71f20ac42e773c8973
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-23 23:54:54 +0800

    Time: 129 ms (12.95%), Space: 42.8 MB (36.05%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let ones = 0;
-    
-    while (n) {
-        ones += (n & 1);
-        n >>>= 1;
-    }
-    
-    return ones;
+  let count = 0;
+  
+  while (n) {
+    count++;
+    n = n & (n - 1);
+  }
+  
+  return count;
 };
