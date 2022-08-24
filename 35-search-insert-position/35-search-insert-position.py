95c80d1ce6b66a24056a320c3958d34fd41d8912
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-24 23:27:57 +0800

    Time: 112 ms (6.05%), Space: 14.8 MB (6.74%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,17 +1,13 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var searchInsert = function (nums, target) {
-  let [l, r] = [0, nums.length - 1];
-
-  while (l <= r) {
-    const mid = (l + r) >>> 1;
-    if (nums[mid] === target) return mid;
-
-    nums[mid] < target ? (l = mid + 1) : (r = mid - 1);
-  }
-
-  return l;
-};
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) >> 1
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+                
+        return l
