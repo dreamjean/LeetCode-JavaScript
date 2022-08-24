d40c3a016f72ac6426eb4efe8869786258bd3b60
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-25 07:36:46 +0800

    Time: 479 ms (13.08%), Space: 50.8 MB (89.14%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,9 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-  if (n <= 0) return false;
-  
-  while (!(n % 3)) n /= 3;
-  
-  return n === 1;
+  return n > 0 && 3 ** 19 % n === 0;
 };
