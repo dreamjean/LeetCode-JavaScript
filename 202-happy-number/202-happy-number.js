b8e651986120a055b4a07a68dc1cb688c17649f7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-25 23:16:22 +0800

    Time: 100 ms (52.86%), Space: 44 MB (48.80%) - LeetHub

--- 202-happy-number/202-happy-number.js
+++ 202-happy-number/202-happy-number.js
@@ -5,13 +5,21 @@
 var isHappy = function (n) {
   const set = new Set();
 
-  while (n && !set.has(n)) {
+  while (n > 1 && !set.has(n)) {
     set.add(n);
-    n = digitsSquaresSum(n);
+    n = digitsSqrSum(n);
   }
 
   return n === 1;
 };
 
-const digitsSquaresSum = (n) =>
-  [...`${n}`].reduce((acc, cur) => (acc += +cur * +cur), 0);
+const digitsSqrSum = (n) => {
+  let sum = 0;
+
+  while (n) {
+    sum += (n % 10) ** 2;
+    n = ~~(n / 10);
+  }
+
+  return sum;
+};
