d5c4660174887b1067d7d9ac1eb5ddb7e033ed7c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-25 20:04:44 +0800

    Time: 415 ms (67.11%), Space: 15.5 MB (45.27%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            mid = (l + r) >> 1
+            if x - arr[mid] > arr[mid+k] - x:
+                l = mid + 1
+            else:
+                r = mid
+        return arr[l:l+k]
