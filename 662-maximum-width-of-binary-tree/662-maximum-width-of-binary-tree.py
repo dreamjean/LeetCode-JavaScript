5f85597039f40ac8afad78ea32aa52be96179699
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-27 20:03:27 +0800

    Time: 85 ms (25.04%), Space: 14.7 MB (75.06%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -1,31 +1,21 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-var widthOfBinaryTree = function (root) {
-  const queue = [[root, 0]];
-  let maxWidth = 1;
-
-  while (queue.length) {
-    const size = queue.length;
-    const [start, end] = [queue[0][1], queue[size - 1][1]];
-    maxWidth = Math.max(maxWidth, end - start + 1);
-
-    for (let i = 0; i < size; i++) {
-      let [node, index] = queue.shift();
-
-      if (node.left) queue.push([node.left, (index - start) * 2]);
-      if (node.right) queue.push([node.right, (index - start) * 2 + 1]);
-    }
-  }
-
-  return maxWidth;
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        leave = [(root, 0)]
+        res = 0
+        while leave:
+            res = max(res, leave[-1][1] - leave[0][1] + 1)
+            row = []
+            for node, index in leave:
+                if node.left:
+                    row.append((node.left, index * 2))
+                if node.right:
+                    row.append((node.right, index * 2 + 1))
+            leave = row
+        return res
+        
