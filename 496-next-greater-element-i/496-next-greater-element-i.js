c0fbb682b8d4a7556866d1ec07a14f525e965272
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-28 23:45:49 +0800

    Time: 92 ms (68.09%), Space: 44.3 MB (38.96%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.js
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -3,17 +3,18 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(nums1, nums2) {
-    const map = {};
-    const stack = [];
-    
-    for (let i = nums2.length - 1; i >= 0; --i) {
-        const num = nums2[i];
-        while (stack.length && stack.at(-1) < num) stack.pop();
-        
-        map[num] = stack.length ? stack.at(-1) : -1;
-        stack.push(num);
-    }
-    
-    return nums1.map(num => map[num]);
+var nextGreaterElement = function (nums1, nums2) {
+  const map = {};
+  const stack = [];
+
+  for (let i = nums2.length - 1; i >= 0; i--) {
+    const num = nums2[i];
+    while (stack.length && stack.at(-1) < num) stack.pop();
+
+    map[num] = stack.length ? stack.at(-1) : -1;
+
+    stack.push(num);
+  }
+
+  return nums1.map((num) => map[num]);
 };
