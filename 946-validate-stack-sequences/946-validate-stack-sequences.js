87ed943fbd9ea4bd4c39ca54610edaf26bc2b8ee
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-31 06:43:28 +0800

    Time: 119 ms (20.66%), Space: 44.9 MB (11.57%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function(pushed, popped) {
+  const stack = [];
+  let i = 0;
+  
+  for (let num of pushed) {
+    stack.push(num);
+    while (stack.length && stack.at(-1) === popped[i]) {
+      stack.pop();
+      i++;
+    }
+  }
+  
+  return i === pushed.length;
+};
