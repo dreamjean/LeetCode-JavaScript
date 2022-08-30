5ab4bbca22c63d5cbc6414f8de5d91946dc69f66
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-08-30 23:48:31 +0800

    Time: 147 ms (6.06%), Space: 43.9 MB (60.61%) - LeetHub

---
+++ 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoMaxTree = function (root, val) {
+  if (root?.val > val) {
+    root.right = insertIntoMaxTree(root.right, val);
+    return root;
+  }
+
+  return new TreeNode(val, root, null);
+};
