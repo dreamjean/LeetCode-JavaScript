181855c79bb6d59e04432a2131f403c5358d5604
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-03 22:02:43 +0800

    Time: 70 ms (100.00%), Space: 42 MB (82.86%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
@@ -3,15 +3,14 @@
  * @param {number} k
  * @return {character}
  */
-var findKthBit = function (n, k) {
-  if (n === 1) return "0";
-
+var findKthBit = function(n, k) {
+  if (n === 1) return '0';
+  
   const mid = 1 << (n - 1);
-  if (k === mid) return "1";
-  if (k < mid) return findKthBit(n - 1, k);
-
-  k = mid * 2 - k;
-  return invert(findKthBit(n - 1, k));
+  if (mid === k) return '1';
+  if (mid > k) return findKthBit(n - 1, k);
+  
+  return invert(findKthBit(n - 1, mid * 2 - k));
 };
 
-const invert = (bit) => (bit === "0" ? "1" : "0");
+const invert = bit => bit === '0' ? '1' : '0';
