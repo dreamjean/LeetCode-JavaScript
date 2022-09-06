628cdabbdf380ae13be2c4562295330c92bbce42
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-07 05:54:37 +0800

    Time: 59 ms (17.77%), Space: 13.8 MB (98.35%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -1,13 +1,12 @@
-/**
- * @param {string} text
- * @return {string}
- */
-var reorderSpaces = function(text) {
-  const spaces = text.split('').filter(x => x === ' ').length;
-  const words = text.trim().split(' ').filter(word => word);
-  const n = words.length;
+class Solution:
+    def reorderSpaces(self, text: str) -> str:
+        spaces = text.count(' ')
+        words = text.split()
+        n = len(words) - 1
+        x, y = divmod(spaces, n) if n else (0, spaces)
+        
+        return (' ' * x).join(words) + ' ' * y
+        
+        
+        
   
-  return n === 1
-    ? words + ' '.repeat(spaces)
-    : words.join(' '.repeat(~~(spaces / (n - 1)))) + ' '.repeat(spaces % (n - 1));
-};
