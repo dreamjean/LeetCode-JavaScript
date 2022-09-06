5730cb47d2def139a2fc392b00952adc0714200c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-06 17:59:03 +0800

    Time: 161 ms (75.88%), Space: 57.1 MB (84.22%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    const set = new Set(nums);
-    let res = 0;
+  const set = new Set(nums);
+  let ans = 0;
+  
+  for (let num of set) {
+    if (set.has(num - 1)) continue;
     
-    for (let start of Array.from(set)) {
-        if (set.has(start - 1)) continue;
-        
-        let end = start + 1;
-        while (set.has(end)) end++;
-        
-        res = Math.max(end - start, res);
-    }
+    let next = num + 1;
+    while (set.has(next)) next++;
     
-    return res;
+    ans = Math.max(ans, next - num);
+  }
+  
+  return ans;
 };
