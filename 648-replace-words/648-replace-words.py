3ea4edfdff9697ced65dca79ba7b71b755f96aae
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-07 18:52:48 +0800

    Time: 336 ms (37.69%), Space: 50.3 MB (94.23%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function (dictionary, sentence) {
+  dictionary.sort((a, b) => a.length - b.length);
+
+  return sentence
+    .split(" ")
+    .map((word) => getNewWord(dictionary, word) ?? word)
+    .join(" ");
+};
+
+const getNewWord = (dictionary, word) =>
+  dictionary.find((w) => word.startsWith(w));
