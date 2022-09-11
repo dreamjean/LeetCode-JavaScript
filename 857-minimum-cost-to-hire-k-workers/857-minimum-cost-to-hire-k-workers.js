65be4920ef22479d1694472f8566536053f7f5b1
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-11 12:12:50 +0800

    Time: 164 ms (76.47%), Space: 52 MB (50.00%) - LeetHub

---
+++ 857-minimum-cost-to-hire-k-workers/857-minimum-cost-to-hire-k-workers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} quality
+ * @param {number[]} wage
+ * @param {number} k
+ * @return {number}
+ */
+var mincostToHireWorkers = function (quality, wage, k) {
+  const persons = quality
+    .map((item, i) => [item, wage[i]])
+    .sort(([a1, b1], [a2, b2]) => b1 / a1 - b2 / a2);
+  const pq = new MaxPriorityQueue();
+  let [sum, ans] = [0, Infinity];
+
+  for (const [q, w] of persons) {
+    sum += q;
+    pq.enqueue(q);
+    if (pq.size() > k) sum -= pq.dequeue().element;
+    if (pq.size() === k) {
+      const unitWage = w / q;
+      ans = Math.min(ans, unitWage * sum);
+    }
+  }
+
+  return ans;
+};
