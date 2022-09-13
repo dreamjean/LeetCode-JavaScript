c125296c02e1fbfd9a717b3d529fee70f7819f2a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-13 09:34:37 +0800

    Time: 115 ms (10.34%), Space: 42.7 MB (10.34%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function (num) {
+  const nums = num.toString().split("");
+  const n = nums.length;
+  let [maxIdx, low, high] = [n - 1, 0, 0];
+
+  for (let i = n - 1; i >= 0; i--) {
+    const [curr, max] = [+nums[i], +nums[maxIdx]];
+    if (curr > max) maxIdx = i;
+    else if (curr < max) [low, high] = [i, maxIdx];
+  }
+
+  [nums[low], nums[high]] = [nums[high], nums[low]];
+
+  return +nums.join("");
+};
