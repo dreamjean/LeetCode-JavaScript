e044df2b701e38ae5c28a2c4f1aa5e1cb915a553
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-14 23:42:12 +0800

    Time: 502 ms (53.33%), Space: 115.2 MB (64.44%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var pseudoPalindromicPaths = function (root) {
+  return helper(root);
+};
+
+const helper = (node, count = 0) => {
+  if (!node) return 0;
+
+  count ^= 1 << node.val;
+  if (!node.left && !node.right) return count & (count - 1) ? 0 : 1;
+
+  return helper(node.left, count) + helper(node.right, count);
+};
