cc9729cd89b6d5934671a03a589c762bc9236dac
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-16 19:35:15 +0800

    Time: 112 ms (100.00%), Space: 48.3 MB (25.00%) - LeetHub

---
+++ 850-rectangle-area-ii/850-rectangle-area-ii.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
+var rectangleArea = function(rectangles) {
+  const events = [];
+  const active = [];
+  const ll = BigInt;
+  const MOD = ll(10 ** 9 + 7);
+  let [area, currY] = [0n, 0];
+  
+  for (const [x1, y1, x2, y2] of rectangles) {
+    events.push([y1, 1, x1, x2]);
+    events.push([y2, -1, x1, x2]);
+  }
+  
+  events.sort((a, b) => a[0] - b[0]);
+  
+  for (const [y, sig, x1, x2] of events) {
+    let [maxWidth, currX] = [0, -1];
+    for (let [cx1, cx2] of active) {
+      currX = Math.max(currX, cx1);
+      maxWidth += Math.max(0, cx2 - currX);
+      currX = Math.max(currX, cx2);
+    }
+    
+    area += (ll(maxWidth) * ll(y - currY)) % MOD;
+    
+    if (sig === 1) {
+      active.push([x1, x2]);
+      active.sort((a, b) => a[0] - b[0]);
+    }
+    else {
+      for (let i = 0; i < active.length; i++) {
+        const [cx1, cx2] = active[i];
+        if (cx1 === x1 && cx2 === x2) {
+          active.splice(i, 1);
+          break;
+        }
+      }
+    }
+    
+    currY = y;
+  }
+  
+  return area % MOD;
+};
