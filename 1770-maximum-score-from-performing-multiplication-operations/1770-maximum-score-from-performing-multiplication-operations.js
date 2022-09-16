248af4328a8e4a00ec04857c151cac87c1183b95
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-17 07:03:00 +0800

    Time: 331 ms (79.17%), Space: 83.2 MB (81.67%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -3,19 +3,19 @@
  * @param {number[]} multipliers
  * @return {number}
  */
-var maximumScore = function(nums, multipliers) {
+var maximumScore = function (nums, multipliers) {
   const [m, n] = [multipliers.length, nums.length];
-  const dp = Array.from({ length: m }, () => new Array(m).fill(0));
-  
-  const dfs = (l = 0, i = 0) => {
-    if (i === m) return 0;
-    if (dp[l][i]) return dp[l][i];
-    
-    const pickLeft = dfs(l + 1, i + 1) + nums[l] * multipliers[i];
-    const pickRight = dfs(l, i + 1) + nums[n - 1 - (i - l)] * multipliers[i];
-    
-    return dp[l][i] = Math.max(pickLeft, pickRight);
+  const dp = Array.from({ length: m + 1 }, () => new Array(m + 1).fill(0));
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let l = i; l >= 0; l--) {
+      const r = n - 1 - (i - l);
+      dp[l][i] = Math.max(
+        nums[l] * multipliers[i] + dp[l + 1][i + 1],
+        nums[r] * multipliers[i] + dp[l][i + 1]
+      );
+    }
   }
-  
-  return dfs();
+
+  return dp[0][0];
 };
