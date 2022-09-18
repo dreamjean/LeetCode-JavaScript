4ac2e94ef384f94bdd41d89697af5e5ef3a3f5f4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-19 06:05:47 +0800

    Time: 132 ms (41.04%), Space: 44.4 MB (58.96%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -2,16 +2,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var frequencySort = function(nums) {
+var frequencySort = function (nums) {
   const map = {};
-  
-  nums.forEach((num) => map[num] ? map[num]++ : map[num] = 1);
-  
-  return Object
-    .entries(map)
-    .sort((a, b) => a[1] === b[1] ? +b[0] - +a[0] : a[1] - b[1])
-    .reduce((acc, [a, b]) => {
-      while (b--) acc.push(+a);
-      return acc;
-    }, []) 
+
+  nums.forEach((num) => (map[num] ? map[num]++ : (map[num] = 1)));
+
+  return nums.sort((a, b) => (map[a] === map[b] ? b - a : map[a] - map[b]));
 };
