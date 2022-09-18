d07d2eb005fb5b7a27011df443f5de87f00188cc
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-19 06:04:54 +0800

    Time: 117 ms (10.09%), Space: 13.8 MB (67.44%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,17 +1,4 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var frequencySort = function(nums) {
-  const map = {};
-  
-  nums.forEach((num) => map[num] ? map[num]++ : map[num] = 1);
-  
-  return Object
-    .entries(map)
-    .sort((a, b) => a[1] === b[1] ? +b[0] - +a[0] : a[1] - b[1])
-    .reduce((acc, [a, b]) => {
-      while (b--) acc.push(+a);
-      return acc;
-    }, []) 
-};
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        return sorted(nums, key=lambda x: (count[x], -x))
