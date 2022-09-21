b532e668db00877088ca0d95aec7d7052a49f5e5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-21 11:15:23 +0800

    Time: 124 ms (11.56%), Space: 14.2 MB (24.96%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,10 +1,3 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-var intersection = function (nums1, nums2) {
-  const set = new Set(nums1);
-
-  return nums2.filter((num) => set.delete(num));
-};
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(x for x in nums1 if x in nums2)
