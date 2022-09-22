4d267f990a4f29327e9e246d7b7dc886d2fa36d5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-22 18:11:34 +0800

    Time: 115 ms (56.93%), Space: 48.3 MB (64.67%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -2,17 +2,9 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    return s.split(' ').map(word => word = reverseString(word)).join(' ');
+var reverseWords = function (s) {
+  return s
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
 };
-
-const reverseString = str => {
-    const arr = str.split('');
-    let [l, r] = [0, arr.length - 1];
-    
-    while (l < r) {
-        [arr[l++], arr[r--]] = [arr[r], arr[l]];
-    }
-    
-    return arr.join('');
-}
