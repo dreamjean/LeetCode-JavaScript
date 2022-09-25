eeb2e793ac84707b3c24c307ba7e2154f7f99435
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-25 17:32:15 +0800

    Time: 178 ms (33.33%), Space: 50.3 MB (33.33%) - LeetHub

--- 850-rectangle-area-ii/850-rectangle-area-ii.js
+++ 850-rectangle-area-ii/850-rectangle-area-ii.js
@@ -2,35 +2,34 @@
  * @param {number[][]} rectangles
  * @return {number}
  */
-var rectangleArea = function(rectangles) {
+var rectangleArea = function (rectangles) {
   const events = [];
   const active = [];
   const ll = BigInt;
-  const MOD = ll(10 ** 9 + 7);
-  let [area, currY] = [0n, 0];
-  
+  const mod = ll(10 ** 9 + 7);
+  let [res, currY] = [0n, 0];
+
   for (const [x1, y1, x2, y2] of rectangles) {
-    events.push([y1, 1, x1, x2]);
-    events.push([y2, -1, x1, x2]);
+    events.push([y1, 1, x1, x2], [y2, -1, x1, x2]);
   }
-  
+
   events.sort((a, b) => a[0] - b[0]);
-  
+
   for (const [y, sig, x1, x2] of events) {
     let [maxWidth, currX] = [0, -1];
-    for (let [cx1, cx2] of active) {
+
+    for (const [cx1, cx2] of active) {
       currX = Math.max(currX, cx1);
       maxWidth += Math.max(0, cx2 - currX);
       currX = Math.max(currX, cx2);
     }
-    
-    area += (ll(maxWidth) * ll(y - currY)) % MOD;
-    
+
+    res += (ll(maxWidth) * ll(y - currY)) % mod;
+
     if (sig === 1) {
       active.push([x1, x2]);
       active.sort((a, b) => a[0] - b[0]);
-    }
-    else {
+    } else {
       for (let i = 0; i < active.length; i++) {
         const [cx1, cx2] = active[i];
         if (cx1 === x1 && cx2 === x2) {
@@ -39,9 +38,9 @@ var rectangleArea = function(rectangles) {
         }
       }
     }
-    
+
     currY = y;
   }
-  
-  return area % MOD;
+
+  return res % mod;
 };
