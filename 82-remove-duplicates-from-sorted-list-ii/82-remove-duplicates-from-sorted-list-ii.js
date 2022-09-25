ac08ea0bdd214515a20ebf71fcb01f064c1ab308
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-25 17:42:57 +0800

    Time: 114 ms (36.86%), Space: 44.4 MB (53.83%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -9,19 +9,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-    const dummy = new ListNode(0, head);
-    let curr = dummy;
+var deleteDuplicates = function (head) {
+  const dummy = new ListNode(-1, head);
+  let curr = dummy;
+
+  while (curr.next?.next) {
+    if (curr.next.val === curr.next.next.val) {
+      const { val } = curr.next;
+      while (curr.next?.val === val) curr.next = curr.next.next;
+    } 
     
-    while (curr.next?.next) {
-        if (curr.next.val === curr.next.next.val) {
-            const { val } = curr.next;
-            while (curr.next?.val === val) 
-                curr.next = curr.next.next;
-        }       
-        
-        else curr = curr.next;
-    }
-    
-    return dummy.next;
+    else curr = curr.next;
+  }
+
+  return dummy.next;
 };
