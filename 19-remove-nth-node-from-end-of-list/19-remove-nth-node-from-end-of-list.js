de8d8b5bf06fc87e804b6c962becd6189bba2e5e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-09-28 21:47:36 +0800

    Time: 117 ms (17.77%), Space: 43.3 MB (33.57%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -10,18 +10,18 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
   const dummy = new ListNode(-1, head);
   let [slow, fast] = [dummy, dummy];
-  
-  for (let i = 0; i <= n; i++) fast = fast.next;
-  
-  while (fast) {
+
+  while (n--) fast = fast.next;
+
+  while (fast?.next) {
     slow = slow.next;
     fast = fast.next;
   }
-  
+
   slow.next = slow.next.next;
-  
+
   return dummy.next;
 };
