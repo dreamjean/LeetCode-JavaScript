329a4ad4a3bc5563d37c71c3c07383fdbc2c76d8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-01 20:01:39 +0800

    Time: 204 ms (7.06%), Space: 50.3 MB (99.74%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -3,7 +3,15 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+var rotate = function(nums, k) {
   const n = nums.length;
-  nums.splice(0, 0, ...nums.splice(n - (k % n)));
+  k %= n;
+  
+  reverse(0, n - 1, nums)
+  reverse(0, k - 1, nums)
+  reverse(k, n - 1, nums)
 };
+
+const reverse = (l, r, nums) => {
+  while (l < r) [nums[l++], nums[r--]] = [nums[r], nums[l]]
+}
