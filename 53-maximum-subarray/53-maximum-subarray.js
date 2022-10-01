4b3eb0aa3bcfa91dcaf1dc3c8e4832da8f08597c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-01 18:37:43 +0800

    Time: 154 ms (17.05%), Space: 50.8 MB (21.38%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let [sum, max] = [0, nums[0]];
+  let [max, sum] = [nums[0], 0];
 
   for (let num of nums) {
     sum = Math.max(num, sum + num);
