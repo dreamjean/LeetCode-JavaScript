8f80400384bd941fc7d50c4c94001c0c5f813db1
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-01 19:33:29 +0800

    Time: 107 ms (83.07%), Space: 50.7 MB (68.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
+var containsDuplicate = function (nums) {
   const set = new Set();
-  
+
   for (let num of nums) {
     if (set.has(num)) return true;
-    
+
     set.add(num);
   }
-  
+
   return false;
 };
