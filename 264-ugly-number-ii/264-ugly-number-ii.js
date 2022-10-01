53fa9add551ba6ae65fe35c23eb6fe9cad0fcce7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-02 02:31:01 +0800

    Time: 86 ms (87.98%), Space: 48 MB (34.43%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.js
+++ 264-ugly-number-ii/264-ugly-number-ii.js
@@ -7,13 +7,13 @@ var nthUglyNumber = function (n) {
   let [i2, i3, i5] = [0, 0, 0];
 
   while (!ans[n - 1]) {
-    const [c1, c2, c3] = [ans[i2] * 2, ans[i3] * 3, ans[i5] * 5];
-    const ugly = Math.min(c1, c2, c3);
+    const [t2, t3, t5] = [ans[i2] * 2, ans[i3] * 3, ans[i5] * 5];
+    const ugly = Math.min(t2, t3, t5);
     ans.push(ugly);
 
-    if (ugly === c1) i2++;
-    if (ugly === c2) i3++;
-    if (ugly === c3) i5++;
+    if (t2 === ugly) i2++;
+    if (t3 === ugly) i3++;
+    if (t5 === ugly) i5++;
   }
 
   return ans[n - 1];
