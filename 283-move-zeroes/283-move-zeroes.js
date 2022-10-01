6dd1b21fb953f8e239585361df632bbcf847c015
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-01 19:01:11 +0800

    Time: 163 ms (34.62%), Space: 47 MB (27.80%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,14 +2,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-  let [l, r] = [0, 1];
-  
+var moveZeroes = function (nums) {
+  let [l, r] = [0, 0];
+
   while (r < nums.length) {
     if (nums[l] && !nums[r]) l = r;
-    else if (!nums[l] && nums[r]) 
-      [nums[l++], nums[r]] = [nums[r], nums[l]]
-    
+    if (!nums[l] && nums[r]) [nums[l++], nums[r]] = [nums[r], nums[l]];
     r++;
   }
 };
