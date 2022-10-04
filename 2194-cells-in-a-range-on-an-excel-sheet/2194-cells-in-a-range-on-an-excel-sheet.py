46822651b25342d2e096538d90571f2d6b8dc28e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-04 21:15:40 +0800

    Time: 99 ms (5.31%), Space: 13.9 MB (68.17%) - LeetHub

---
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.py
@@ -0,0 +1,4 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        c1, r1, _, c2, r2 = map(ord, s)
+        return [chr(c) + chr(r) for c in range(c1, c2 + 1) for r in range(r1, r2 + 1)]
