53f9e94d9bacafa5cfaf7a84ebe80dd3b934c3b7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-04 20:58:08 +0800

    Time: 216 ms (87.76%), Space: 55.1 MB (53.06%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.js
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.js
@@ -3,19 +3,19 @@
  * @param {number} totalTrips
  * @return {number}
  */
-var minimumTime = function (time, totalTrips) {
-  let [low, high] = [1, 1e14];
-
+var minimumTime = function(time, totalTrips) {
+  let [low, high] = [1, totalTrips * time[0]];
+  
   while (low < high) {
     const mid = low + Math.floor((high - low) / 2);
-    if (graterThanTotal(time, mid, totalTrips)) high = mid;
+    if (biggerThanTotal(time, mid, totalTrips)) high = mid;
     else low = mid + 1;
   }
-
+  
   return low;
 };
 
-const graterThanTotal = (time, mid, totalTrips) => {
+const biggerThanTotal = (time, mid, totalTrips) => {
   let trips = 0;
   
   for (let t of time) trips += Math.floor(mid / t);
