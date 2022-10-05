ef5ce375b4cdf26cbaa0267de2cba264fd259be8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-06 01:52:19 +0800

    Time: 160 ms (39.66%), Space: 48 MB (68.97%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.js
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.js
@@ -12,31 +12,30 @@
  * @param {number} depth
  * @return {TreeNode}
  */
-var addOneRow = function (root, val, depth) {
+var addOneRow = function(root, val, depth) {
   if (depth === 1) return new TreeNode(val, root, null);
-
+  
   const queue = [root];
   let leave = 1;
-
+  
   while (queue.length) {
     const size = queue.length;
     if (leave + 1 === depth) {
-      for (let i = 0; i < size; ++i) {
+      for (let i = 0; i < size; i++) {
         const node = queue.shift();
         node.left = new TreeNode(val, node.left, null);
         node.right = new TreeNode(val, null, node.right);
       }
       break;
+    } else if (leave + 1 < depth) {
+      for (let i = 0; i < size; i++) {
+        const { left, right } = queue.shift();
+        if (left) queue.push(left);
+        if (right) queue.push(right);
+      }
     }
-
-    for (let i = 0; i < size; ++i) {
-      const { left, right } = queue.shift();
-      if (left) queue.push(left);
-      if (right) queue.push(right);
-    }
-
     ++leave;
   }
-
+  
   return root;
 };
