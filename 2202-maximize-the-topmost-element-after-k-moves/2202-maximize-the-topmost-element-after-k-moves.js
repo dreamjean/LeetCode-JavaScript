07d8081bd302359e3a3206627be30fc4306b5b2f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-05 22:03:39 +0800

    Time: 148 ms (29.17%), Space: 56.4 MB (6.25%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.js
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.js
@@ -6,7 +6,7 @@
 var maximumTop = function(nums, k) {
   if (!k) return nums[0];
   if (k == 1) return nums[1] ?? -1;
-  if (nums.length === 1 && (k & 1) > 0) return -1;
+  if (nums.length === 1 && (k & 1)) return -1;
   
-  return Math.max(...nums.slice(0, k - 1), nums[k] || 0);
+  return Math.max(...nums.slice(0, k - 1), (nums[k] || 0));
 };
