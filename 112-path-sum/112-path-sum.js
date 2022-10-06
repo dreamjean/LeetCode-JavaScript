380fdc3b170e3d0e6cf077a76c5e994e49277189
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-06 22:28:30 +0800

    Time: 106 ms (65.80%), Space: 45.9 MB (30.04%) - LeetHub

--- 112-path-sum/112-path-sum.js
+++ 112-path-sum/112-path-sum.js
@@ -11,12 +11,9 @@
  * @param {number} targetSum
  * @return {boolean}
  */
-var hasPathSum = function (root, targetSum) {
+var hasPathSum = function(root, targetSum) {
   if (!root) return false;
   if (!root.left && !root.right) return targetSum === root.val;
-
-  return (
-    hasPathSum(root.left, targetSum - root.val) ||
-    hasPathSum(root.right, targetSum - root.val)
-  );
+  
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 };
