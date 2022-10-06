3d2bbcbd7325f7059dca3383ea727fcc6dd681c3
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-06 23:43:06 +0800

    Time: 95 ms (67.87%), Space: 44.7 MB (29.59%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.js
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.js
@@ -3,24 +3,19 @@
  * @param {number} k
  * @return {number[]}
  */
-var numsSameConsecDiff = function (n, k) {
-  const ans = [];
-
-  const dfs = (path = []) => {
-    if (path.length === n) return ans.push(+path.join(""));
-
-    for (let i = 0; i < 10; i++) {
-      if (
-        (!path.length && !i) ||
-        (path.length && Math.abs(path.at(-1) - i) !== k)
-      )
-        continue;
-
-      dfs([...path, i]);
+var numsSameConsecDiff = function(n, k) {
+  let list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  
+  while (--n) {
+    const curr = [];
+    for (let x of list) {
+      y = x % 10;
+      if (y + k < 10) curr.push(x * 10 + y + k);
+      if (k && y - k >= 0) curr.push(x * 10 + y - k);
     }
-  };
-
-  dfs();
-
-  return ans;
+    
+    list = [...curr];
+  }
+  
+  return list;
 };
