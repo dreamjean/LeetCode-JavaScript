786e9224351565bbd9192e829ea7d7304e604d66
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-08 01:29:45 +0800

    Time: 554 ms (9.13%), Space: 80.8 MB (7.34%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.js
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.js
@@ -5,21 +5,23 @@
  * @param {number} k
  * @return {number}
  */
-var maxPerformance = function(n, speed, efficiency, k) {
-  const persons = efficiency.map((e, i) => [e, speed[i]]).sort((a, b) => b[0] - a[0]);
+var maxPerformance = function (n, speed, efficiency, k) {
+  const persons = efficiency
+    .map((e, i) => [e, speed[i]])
+    .sort((a, b) => b[0] - a[0]);
   const pq = new MinPriorityQueue();
   const ll = BigInt;
-  const mod = ll(10 ** 9 + 7);
-  let [res, sum] = [0n, 0];
-  
-  for (const [e, s] of persons) {
+  const mod = ll(1e9 + 7);
+  let [ans, sum] = [0n, 0];
+
+  for (let [e, s] of persons) {
     sum += s;
     pq.enqueue(s);
     if (pq.size() > k) sum -= pq.dequeue().element;
-    
-    const curr = ll(sum) * ll(e);
-    if (curr > res) res = curr;
+
+    const curr = ll(e) * ll(sum);
+    if (curr > ans) ans = curr;
   }
-  
-  return res % mod;
+
+  return ans % mod;
 };
