d85734214a96d92b154c484f83e5d2f73fa219bf
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-10 02:39:57 +0800

    Time: 365 ms (84.85%), Space: 63.8 MB (87.88%) - LeetHub

--- 870-advantage-shuffle/870-advantage-shuffle.js
+++ 870-advantage-shuffle/870-advantage-shuffle.js
@@ -3,18 +3,15 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var advantageCount = function(nums1, nums2) {
-  const indexs = nums1.map((_, i) => i);
-  const n = nums1.length;
-  const ans = new Array(n);
-  let [l, r] = [0, n - 1];
-  
-  indexs.sort((a, b) => nums2[b] - nums2[a]);
+var advantageCount = function (nums1, nums2) {
   nums1.sort((a, b) => b - a);
-  
+  const indexs = nums2.map((_, i) => i).sort((a, b) => nums2[b] - nums2[a]);
+  const ans = [];
+  let [l, r] = [0, nums1.length - 1];
+
   for (let i of indexs) {
     ans[i] = nums1[l] > nums2[i] ? nums1[l++] : nums1[r--];
   }
-  
+
   return ans;
 };
