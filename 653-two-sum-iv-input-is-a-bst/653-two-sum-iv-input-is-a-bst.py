af8f94e0d978724b15d56d98221698888a80bafd
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-09 15:49:48 +0800

    Time: 75 ms (98.57%), Space: 16.2 MB (95.44%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -1,28 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} k
- * @return {boolean}
- */
-var findTarget = function(root, k) {
-  const set = new Set();
-  const queue = [root];
-  
-  while (queue.length) {
-    const { val, left, right } = queue.shift();
-    if (set.has(k - val)) return true;
-    
-    set.add(val);
-    if (left) queue.push(left);
-    if (right) queue.push(right);
-  }
-  
-  return false;
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        seen = set()
+        q = [root]
+        for node in q:
+            if k - node.val in seen:
+                return True
+            seen.add(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return False
