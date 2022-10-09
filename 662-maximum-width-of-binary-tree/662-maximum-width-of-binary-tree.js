91f41f41e0342eaf06738a931cb217f3a2c58178
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-09 16:42:48 +0800

    Time: 130 ms (33.68%), Space: 48.3 MB (19.65%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.js
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.js
@@ -21,9 +21,10 @@ var widthOfBinaryTree = function (root) {
 
     for (let i = 0; i < size; i++) {
       let [node, index] = queue.shift();
-      
-      if (node.left) queue.push([node.left, (index - start) * 2]);
-      if (node.right) queue.push([node.right, (index - start) * 2 + 1]);
+      index -= start;
+
+      if (node.left) queue.push([node.left, index * 2]);
+      if (node.right) queue.push([node.right, index * 2 + 1]);
     }
   }
 
