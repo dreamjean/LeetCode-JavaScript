5f6159240e63f75a6cd96ef7b1a53498d605c95c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-11 01:17:38 +0800

    Time: 63 ms (95.48%), Space: 41.9 MB (78.10%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -6,14 +6,15 @@
 var areAlmostEqual = function (s1, s2) {
   if (s1 === s2) return true;
 
-  let [first, second] = [-1, -1];
+  let [l, r] = [-1, -1];
 
   for (let i = 0; i < s1.length; i++) {
     if (s1[i] === s2[i]) continue;
-    if (first === -1) first = i;
-    else if (second === -1) second = i;
+
+    if (l === -1) l = i;
+    else if (r === -1) r = i;
     else return false;
   }
 
-  return s1[first] === s2[second] && s1[second] === s2[first];
+  return s1[l] === s2[r] && s1[r] === s2[l];
 };
