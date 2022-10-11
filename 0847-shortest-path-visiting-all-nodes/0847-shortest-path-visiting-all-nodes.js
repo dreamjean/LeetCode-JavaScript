96477e3309a99a8299516b44a2367d565760f906
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-11 23:04:50 +0800

    Time: 126 ms (94.52%), Space: 48.6 MB (89.04%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} graph
+ * @return {number}
+ */
+var shortestPathLength = function (graph) {
+  const n = graph.length;
+  const seen = Array.from({ length: n }, () => new Array(1 << n).fill(false));
+  const queue = [];
+
+  for (let i = 0; i < graph.length; i++) {
+    seen[i][1 << i] = true;
+    queue.push([i, 1 << i, 0]);
+  }
+
+  while (queue.length) {
+    const [u, mask, cost] = queue.shift();
+    if (mask === (1 << n) - 1) return cost;
+
+    for (let v of graph[u]) {
+      const maskV = mask | (1 << v);
+      if (seen[v][maskV]) continue;
+
+      seen[v][maskV] = true;
+      queue.push([v, maskV, cost + 1]);
+    }
+  }
+};
