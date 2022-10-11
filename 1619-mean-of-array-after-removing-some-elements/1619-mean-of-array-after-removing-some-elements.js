d2b10f3391bf9bc5de256035a5b76bdfcc9c89e5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-11 23:18:05 +0800

    Time: 122 ms (18.88%), Space: 43.9 MB (48.95%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.js
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.js
@@ -2,12 +2,15 @@
  * @param {number[]} arr
  * @return {number}
  */
-var trimMean = function(arr) {
-  const size = arr.length;
-  const skip = size / 20;
-  
-  return arr
-    .sort((a, b) => a - b)
-    .slice(skip, size - skip)
-    .reduce((a, b) => a + b) / (size - 2 * skip);
+var trimMean = function (arr) {
+  const n = arr.length;
+  const skip = ~~(n / 20);
+
+  return (
+    arr
+      .sort((a, b) => a - b)
+      .slice(skip, -skip)
+      .reduce((a, b) => a + b) /
+    (n - 2 * skip)
+  );
 };
