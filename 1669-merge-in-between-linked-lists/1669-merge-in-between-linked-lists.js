204dbdf9ecdd23b274a18fa7380a56eccb3c8a75
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-13 23:29:05 +0800

    Time: 426 ms (15.34%), Space: 62.9 MB (30.67%) - LeetHub

---
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+  const dummy = new ListNode(-1, list1);
+  let [first, second] = [dummy, dummy];
+  
+  for (let i = 0; i <= b - a; i++) second = second.next;
+  
+  for (let i = 0; i < a; i++) {
+    first = first.next;
+    second = second.next;
+  }
+  
+  first.next = list2;
+  while (first.next) first = first.next;
+  
+  first.next = second.next;
+  
+  return dummy.next;
+};
