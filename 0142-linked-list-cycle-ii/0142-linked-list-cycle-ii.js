d6d250da8a01de123be7522368feae5ef8d618bf
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-13 12:22:15 +0800

    Time: 121 ms (55.11%), Space: 44.7 MB (79.09%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  let [slow, fast] = [head, head];
+  
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    
+    if (slow === fast) {
+      let p = head
+      while (p != slow) {
+        p = p.next
+        slow = slow.next
+      }
+      
+      return p
+    }    
+  }
+  
+  return null;
+};
