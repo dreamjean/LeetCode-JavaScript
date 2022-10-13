019273e1e9f602d29b225f5789addacee2ee129e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-13 20:48:51 +0800

    Time: 1329 ms (22.67%), Space: 126 MB (75.24%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+  if (!head.next) {
+    head = null;
+    return head;
+  }
+  
+  let [prev, slow, fast] = [null, head, head];
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  
+  prev.next = prev.next.next;
+  
+  return head;
+};
