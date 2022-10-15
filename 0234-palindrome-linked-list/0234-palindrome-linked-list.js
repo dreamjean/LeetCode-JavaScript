29eb1d8631fffed615ce08bee7c0081142e23d5e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-15 18:49:14 +0800

    Time: 180 ms (88.09%), Space: 79.9 MB (39.98%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  if (!head.next) return true;
+  
+  const middle = slice(head);
+  const second = reverse(middle);
+  
+  return isSameList(head, second);  
+};
+
+const slice = (node) => {
+  let [prev, slow, fast] = [null, node, node];
+  
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  
+  prev.next = null;
+  
+  return slow;
+}
+
+const reverse = (node) => {
+  let prev = null;
+  
+  while (node) [node.next, prev, node] = [prev, node, node.next];
+  
+  return prev;
+}
+
+const isSameList = (a, b) => {
+  let [aNext, bNext] = [null, null]
+  
+  while (a && b) {
+    if (a.val !== b.val) return false;
+    
+    a = a.next
+    b = b.next
+  }
+  
+  return true;
+}
