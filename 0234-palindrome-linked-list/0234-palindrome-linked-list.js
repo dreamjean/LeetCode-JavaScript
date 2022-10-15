67475aed11f89ee61987fd50e2d36607fea8c16d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-15 18:51:49 +0800

    Time: 243 ms (49.65%), Space: 80 MB (39.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -9,44 +9,44 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
+var isPalindrome = function (head) {
   if (!head.next) return true;
-  
+
   const middle = slice(head);
   const second = reverse(middle);
-  
-  return isSameList(head, second);  
+
+  return isSameList(head, second);
 };
 
 const slice = (node) => {
   let [prev, slow, fast] = [null, node, node];
-  
+
   while (fast && fast.next) {
     prev = slow;
     slow = slow.next;
     fast = fast.next.next;
   }
-  
+
   prev.next = null;
-  
+
   return slow;
-}
+};
 
 const reverse = (node) => {
   let prev = null;
-  
+
   while (node) [node.next, prev, node] = [prev, node, node.next];
-  
+
   return prev;
-}
+};
 
 const isSameList = (a, b) => {
   while (a && b) {
     if (a.val !== b.val) return false;
-    
-    a = a.next
-    b = b.next
+
+    a = a.next;
+    b = b.next;
   }
-  
+
   return true;
-}
+};
