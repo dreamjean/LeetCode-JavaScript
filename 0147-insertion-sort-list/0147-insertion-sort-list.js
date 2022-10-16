76cfa482caa783d5f7960f7ecd90010f168fa03f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-16 23:44:57 +0800

    Time: 117 ms (82.73%), Space: 44.8 MB (40.16%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+  const dummy = new ListNode();
+  let [prev, curr, next] = [dummy, head, null];
+  
+  while (curr) {
+    next = curr.next;
+    while (prev.next?.val < curr.val) prev = prev.next;
+           
+    curr.next = prev.next;
+    prev.next = curr;
+    prev = dummy;
+    curr = next;
+  }
+    
+  return dummy.next;
+};
