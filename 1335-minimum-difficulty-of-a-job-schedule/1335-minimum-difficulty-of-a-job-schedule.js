a338004aef623cd8bb62195b54c35c2061e0de2f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-17 02:40:12 +0800

    Time: 1149 ms (5.81%), Space: 49.2 MB (31.39%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} jobDifficulty
+ * @param {number} d
+ * @return {number}
+ */
+var minDifficulty = function(jobDifficulty, d) {
+  const n = jobDifficulty.length;
+  const map = new Map();
+  
+  const dfs = (i, d) => {
+    const key = `${i}-${d}`;
+    if (d === 1) return Math.max(...jobDifficulty.slice(i));
+    if (key in map) return map[key];
+    
+    let [res, max] = [Infinity, 0];
+    
+    for (let j = i; j <= n - d; ++j) {
+      max = Math.max(max, jobDifficulty[j]);
+      res = Math.min(res, max + dfs(j + 1, d - 1));
+    }
+    
+    return map[key] = res;
+  }
+  
+  return n < d ? -1 : dfs(0, d)
+};
