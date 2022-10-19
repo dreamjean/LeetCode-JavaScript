3627f1b928827e0f70bbeef3377af30d15b4ec3b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-19 22:39:35 +0800

    Added solution - LeetHub

---
+++ Sum of Array Elements - GFG/sum-of-array-elements.js
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let N,X;
+        N = parseInt(readLine());
+        let arr = readLine().split(' ').map(x=>parseInt(x));
+        let obj = new Solution();
+        console.log(obj.sumElement(arr, N));
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+class Solution {
+  sumElement(arr,n) {
+      // code here
+      
+      return arr.reduce((a, b) => a + b);
+  }
+}
