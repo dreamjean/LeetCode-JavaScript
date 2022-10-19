e607eedb56eadebcccd94803b5a264f1b165017c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-20 07:55:53 +0800

    Time: 129 ms (50.88%), Space: 47.2 MB (37.17%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -10,6 +10,6 @@ var topKFrequent = function (words, k) {
 
   return Object.entries(map)
     .sort((a, b) => (a[1] === b[1] ? a[0].localeCompare(b[0]) : b[1] - a[1]))
-    .slice(0, k)
-    .map(([word, _]) => word);
+    .map(([word, _]) => word)
+    .slice(0, k);
 };
