15dcbaad29ad2a17d54bb1e4df6f1692dbdb1f32
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-21 00:16:13 +0800

    Time: 245 ms (28.03%), Space: 48.5 MB (43.24%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const seen = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+  ]
+  
+  let res = ''
+  
+  while (num) {
+    for (let [x, key] of seen) {
+      while (num >= x) {
+        res += key;
+        num -= x;
+      }
+    }
+  }
+  
+  return res;
+};
