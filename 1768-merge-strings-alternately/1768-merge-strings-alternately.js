4755c1a8bfe44e99e54a713e05b593c33d94434c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-23 01:03:13 +0800

    Time: 91 ms (61.45%), Space: 43.3 MB (12.73%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -3,12 +3,12 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function (word1, word2) {
-  let ans = "";
-
+var mergeAlternately = function(word1, word2) {
+  let res = '';
+  
   for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-    ans += (word1[i] || "") + (word2[i] || "");
+    res += (word1[i] || '') + (word2[i] || '');
   }
-
-  return ans;
+  
+  return res;
 };
