58df1b6215f05f16b8958fb75d0e62fb68001bb0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-22 22:55:02 +0800

    Time: 273 ms (17.14%), Space: 60.1 MB (39.43%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number[]} profit
+ * @return {number}
+ */
+var jobScheduling = function (startTime, endTime, profit) {
+  const n = startTime.length;
+  const dp = new Array(n + 1).fill(0);
+  const jobs = startTime
+    .map((time, i) => [time, endTime[i], profit[i]])
+    .sort((a, b) => (a[1] - b[1]));
+
+  for (let i = 1; i <= n; i++) {
+    const k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
+    dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+  }
+
+  return dp[n];
+};
+
+const binarySearch = (arr, right, target) => {
+  let left = 0;
+
+  while (left < right) {
+    const mid = (left + right) >>> 1;
+    if (arr[mid][1] <= target) left = mid + 1;
+    else right = mid;
+  }
+
+  return left;
+};
