14192954f54be50b14b7e0563d466b2ec184b26e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-26 00:31:14 +0800

    Time: 137 ms (12.76%), Space: 44.6 MB (32.36%) - LeetHub

---
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[]}
+ */
+var numsSameConsecDiff = function (n, k) {
+  let list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  while (--n) {
+    const curr = [];
+    for (let x of list) {
+      const y = x % 10;
+      if (y + k < 10) curr.push(x * 10 + y + k);
+      if (k && y - k >= 0) curr.push(x * 10 + y - k);
+    }
+
+    list = curr;
+  }
+
+  return list;
+};
