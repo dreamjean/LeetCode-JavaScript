617902b19547ce2e6effadfb613d19df3673cff9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-27 19:01:48 +0800

    Time: 164 ms (44.69%), Space: 44.7 MB (53.10%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.js
+++ 0147-insertion-sort-list/0147-insertion-sort-list.js
@@ -9,19 +9,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var insertionSortList = function(head) {
+var insertionSortList = function (head) {
   const dummy = new ListNode();
   let [prev, curr, next] = [dummy, head, null];
-  
+
   while (curr) {
     next = curr.next;
     while (prev.next?.val < curr.val) prev = prev.next;
-           
+
     curr.next = prev.next;
     prev.next = curr;
     prev = dummy;
     curr = next;
   }
-    
+
   return dummy.next;
 };
