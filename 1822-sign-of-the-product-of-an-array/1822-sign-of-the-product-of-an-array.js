c7e1c3fbe5983e09a4cea494469a4774b27841ba
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-27 09:25:45 +0800

    Time: 79 ms (81.53%), Space: 43.7 MB (50.25%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arraySign = function (nums) {
-  return nums.reduce((acc, curr) => acc * (!curr ? 0 : curr < 0 ? -1 : 1), 1);
+var arraySign = function(nums) {
+  let sign = 0;
+  
+  for (let num of nums) {
+    if (!num) return 0;
+    sign += num < 0;
+  }
+  
+  return sign % 2 ? -1 : 1;
 };
