5d8577269162a6fa444045b325f932ebcb670713
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-28 21:36:52 +0800

    Time: 140 ms (45.22%), Space: 42.5 MB (76.40%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.js
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var subArrayRanges = function(nums) {
+var subArrayRanges = function (nums) {
   const n = nums.length;
   let sum = 0;
-  
+
   for (let i = 0; i < n; i++) {
-    let [min, max] = [Infinity, -Infinity];
+    let [min, max] = [nums[i], nums[i]];
     for (let j = i; j < n; j++) {
       min = Math.min(min, nums[j]);
       max = Math.max(max, nums[j]);
       sum += max - min;
     }
   }
-  
+
   return sum;
 };
