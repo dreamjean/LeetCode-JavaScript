108590c096f79d52aadd422f84023b0d0a3d6dd6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-10-29 18:24:52 +0800

    Time: 120 ms (64.29%), Space: 45.1 MB (76.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findErrorNums = function(nums) {
+var findErrorNums = function (nums) {
   const n = nums.length;
   const cnt = new Array(n + 1).fill(0);
   let [a, b] = [0, 0];
-  
+
   nums.forEach((num) => cnt[num]++);
-  
+
   for (let i = 1; i <= n; i++) {
-    if (cnt[i] === 2) a = i;
     if (!cnt[i]) b = i;
+    if (cnt[i] === 2) a = i;
   }
-  
+
   return [a, b];
 };
