b3904597b084992ede771f7f3d69e623476f9c8e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-17 18:57:58 +0800

    Time: 239 ms (62.42%), Space: 51.9 MB (50.91%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+var numMatchingSubseq = function(s, words) {
+  return words.reduce((acc, curr) => acc + isSubsequences(curr, s), 0);
+};
+
+const isSubsequences = (word, s, map = new Map()) => {
+  if (map.has(word)) return map.get(word);
+  
+  let index = -1;
+  for (let ch of word) {
+    index = s.indexOf(ch, index + 1);
+    if (index === -1) {
+      map.set(word, false);
+      return false;
+    }
+  }
+  
+  map.set(word, true);
+  
+  return true;
+}
