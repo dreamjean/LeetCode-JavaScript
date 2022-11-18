e24cf22ec649ea712244e8f4a38f28f347d0b8f4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-18 18:31:30 +0800

    Time: 250 ms (66.67%), Space: 42.3 MB (100.00%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.js
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.js
@@ -7,19 +7,21 @@ var minimumTimeRequired = function(jobs, k) {
   const workers = new Array(k).fill(0);
   let ans = Number.MAX_VALUE;
   
-  const dfs = (i = 0) => {
+  const backtrack = (i = 0) => {
     if (i === jobs.length) return ans = Math.min(ans, Math.max(...workers));
     
     for (let j = 0; j < k; j++) {
-      workers[j] += jobs[i];
-      dfs(i + 1);
-      workers[j] -= jobs[i];
+      if (workers[j] + jobs[i] < ans) {
+        workers[j] += jobs[i];
+        backtrack(i + 1);
+        workers[j] -= jobs[i];
+      }
       
       if (!workers[j]) break;
     }
   }
   
-  dfs();
+  backtrack();
   
   return ans;
 };
