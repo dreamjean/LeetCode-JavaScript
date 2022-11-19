60e6dcc4713bbe6d1f51bd3080679f3544b57026
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-20 00:01:33 +0800

    Time: 327 ms (90.00%), Space: 77.4 MB (13.33%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -5,19 +5,20 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
-    potions.sort((a, b) => a - b);
-    
-    return spells.map(spell => spell = numberOfSuccess(spell, success, potions))
+  potions.sort((a, b) => a - b);
+  
+  return spells.map((spell) => getSuccessfulIndex(spell, success, potions));
 };
 
-const numberOfSuccess = (num, target, arr = []) => {
-    let [left, right] = [0, arr.length - 1];
-    
-    while (left <= right) {
-        const mid = (left + right) >>> 1;
-        if (arr[mid] * num >= target) right = mid - 1;
-        else left = mid + 1;
-    }
-    
-    return arr.length - left;
+const getSuccessfulIndex = (num, target, potions) => {
+  const n = potions.length;
+  let [lo, hi] = [0, n - 1];
+  
+  while (lo <= hi) {
+    const mid = (lo + hi) >>> 1;
+    if (num * potions[mid] < target) lo = mid + 1;
+    else hi = mid - 1;
+  }
+  
+  return n - lo;
 }
