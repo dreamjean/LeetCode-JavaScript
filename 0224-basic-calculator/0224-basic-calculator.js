53cadc3d6d8ae86da4d3faa34c1d655934a278da
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-20 22:39:10 +0800

    Time: 164 ms (32.01%), Space: 49.6 MB (25.50%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  const stack = [1];
+  let [ans, num, sign] = [0, 0, 1];
+  
+  for (let c of s) {
+    if (/[0-9]/.test(c)) num = num * 10 + +c;
+    if (/[+-]/.test(c)) {
+      ans += num * sign;
+      sign = stack.at(-1) * (c === '+' ? 1 : -1);
+      num = 0;
+    }
+    
+    if (c === '(') stack.push(sign);
+    if (c === ')') stack.pop();
+  }
+  
+  return ans + num * sign;
+};
