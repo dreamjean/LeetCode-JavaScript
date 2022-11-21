8cab6b68c811cc9772f947de4334017193eff931
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-21 23:10:47 +0800

    Time: 251 ms (34.11%), Space: 51.9 MB (38.76%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -3,38 +3,24 @@
  * @param {number[]} entrance
  * @return {number}
  */
-var nearestExit = function (maze, entrance) {
+var nearestExit = function(maze, entrance) {
   const [m, n] = [maze.length, maze[0].length];
-  const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
-  const queue = [entrance];
-  let steps = 0;
-
+  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+  const queue = [[...entrance, 0]];
+  
   while (queue.length) {
-    const size = queue.length;
-    for (let i = 0; i < size; i++) {
-      const [r, c] = queue.shift();
-      if (
-        (r !== entrance[0] || c !== entrance[1]) &&
-        (!r || r === m - 1 || !c || c === n - 1)
-      )
-        return steps;
-
-      for (const [dx, dy] of dirs) {
-        const [x, y] = [dx + r, dy + c];
-        if (x < 0 || x >= m || y < 0 || y >= n || maze[x][y] === "+") continue;
-
-        maze[x][y] = "+";
-        queue.push([x, y]);
-      }
+    const [r, c, cost] = queue.shift();
+    if ((r !== entrance[0] || c !== entrance[1]) && 
+       (!r || r === m - 1 || !c || c === n - 1)) return cost;
+    
+    for (const [dx, dy] of dirs) {
+      const [x, y] = [dx + r, dy + c];
+      if (x < 0 || x >= m || y < 0 || y >= n || maze[x][y] !== '.') continue;
+      
+      maze[x][y] = '+';
+      queue.push([x, y, cost + 1]);
     }
-
-    ++steps;
   }
-
+  
   return -1;
 };
