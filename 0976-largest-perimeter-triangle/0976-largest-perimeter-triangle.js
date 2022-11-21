1ea2e900656719d0f71a9dd4b2c42b1714cd5ee2
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-21 23:58:02 +0800

    Time: 184 ms (29.20%), Space: 46.1 MB (14.82%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -6,9 +6,7 @@ var largestPerimeter = function(nums) {
   nums.sort((a, b) => b - a);
   
   for (let i = 2; i < nums.length; i++) {
-    if (nums[i - 2] < nums[i - 1] + nums[i]) {
-      return nums[i - 2] + nums[i - 1] + nums[i];
-    }
+    if (nums[i - 2] < nums[i - 1] + nums[i]) return nums[i - 2] + nums[i - 1] + nums[i];
   }
   
   return 0;
