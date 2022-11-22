ff08e516f332e169241cb2830ee6073a049642da
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-22 23:44:04 +0800

    Time: 3479 ms (16.67%), Space: 90.7 MB (16.67%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeviation = function(nums) {
+  const pq = new MaxPriorityQueue();
+  
+  for (let num of nums) {
+    if (num & 1) num *= 2;
+    
+    pq.enqueue(num);
+  }
+  
+  let ans = pq.front().element - pq.back().element;
+  
+  while (!(pq.front().element & 1)) {
+    pq.enqueue(pq.dequeue().element / 2);
+    
+    ans = Math.min(ans, pq.front().element - pq.back().element);
+  }
+  
+  return ans;
+};
+
