781d2ef12a8d4046bdb214c4ea90d78a63c729d8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-23 22:31:54 +0800

    Time: 88 ms (54.55%), Space: 41.4 MB (96.97%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.js
+++ 0458-poor-pigs/0458-poor-pigs.js
@@ -7,7 +7,7 @@
 var poorPigs = function(buckets, minutesToDie, minutesToTest) {
   let pigs = 0;
   
-  while ((Math.floor(minutesToTest / minutesToDie) + 1) ** pigs < buckets) pigs++;
+  while ((minutesToTest / minutesToDie + 1) ** pigs < buckets) pigs++;
   
   return pigs;
 };
