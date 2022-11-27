e9f93fdb8cd7a9365b9578b52fae10a525023ba6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-28 01:24:48 +0800

    Time: 106 ms (90.90%), Space: 44.7 MB (16.78%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  return nums.reduce(
+    (acc, curr) => {
+      if (acc.at(-1) < curr) acc.push(curr);
+      else acc[acc.findIndex((num) => num >= curr)] = curr;
+      
+      return acc;
+    },
+    [nums[0]]
+  ).length;
+};
