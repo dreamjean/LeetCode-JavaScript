11f05be85ad9094b60694e43e59c557df1e80370
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-29 01:49:01 +0800

    Time: 73 ms (97.55%), Space: 46.6 MB (83.33%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function(root) {
+  const queue = [[root, 0]];
+  let ans = 1;
+  
+  while (queue.length) {
+    const size = queue.length;
+    const [start, end] = [queue[0][1], queue[size - 1][1]];
+    ans = Math.max(ans, end - start + 1);
+    
+    for (let i = 0; i < size; i++) {
+      let [node, index] = queue.shift();
+      index -= start;
+
+      if (node.left) queue.push([node.left, index * 2]);
+      if (node.right) queue.push([node.right, index * 2 + 1]);
+    }
+  }
+  
+  return ans;
+};
