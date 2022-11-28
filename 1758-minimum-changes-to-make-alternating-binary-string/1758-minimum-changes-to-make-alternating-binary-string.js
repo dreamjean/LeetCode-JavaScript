4dd17101fc376c317b89d61c2c38a575d0cd15b0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-29 01:57:57 +0800

    Time: 101 ms (70.83%), Space: 42.9 MB (54.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var minOperations = function(s) {
-  let [cnt1, cnt2] = [0, 0];
+  let cnt = 0;
   
   for (let i = 0; i < s.length; i++) {
-    if (!(i & 1)) s[i] !== '0' ? cnt1++ : cnt2++;
-    if (i & 1) s[i] !== '1' ? cnt1++ : cnt2++;
+    if (+s[i] !== i % 2) cnt++;
   }
   
-  return Math.min(cnt1, cnt2);
+  return Math.min(cnt, s.length - cnt);
 };
