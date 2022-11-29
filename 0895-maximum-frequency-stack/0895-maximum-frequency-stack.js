84a43e893ac60883f45f17aebb41cd8a3ebbc83e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-30 02:17:30 +0800

    Time: 691 ms (30.30%), Space: 78.9 MB (62.42%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.js
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.js
@@ -1,34 +1,36 @@
-var FreqStack = function () {
-  this.freq = {};
+
+var FreqStack = function() {
   this.stack = [];
+  this.freq = {};
 };
 
-/**
+/** 
  * @param {number} val
  * @return {void}
  */
-FreqStack.prototype.push = function (val) {
-  const { freq, stack } = this;
-  freq[val] ? freq[val]++ : (freq[val] = 1);
-
-  const num = freq[val];
-  if (!stack[num]) stack[num] = [];
-  stack[num].push(val);
+FreqStack.prototype.push = function(val) {
+  this.freq[val] ? this.freq[val]++ : this.freq[val] = 1;
+  
+  const cnt = this.freq[val];
+  if (!this.stack[cnt]) this.stack[cnt] = [];
+  
+  this.stack[cnt].push(val);
 };
 
 /**
  * @return {number}
  */
-FreqStack.prototype.pop = function () {
-  const top = this.stack.at(-1);
-  const x = top.pop();
+FreqStack.prototype.pop = function() {
+  const top = this.stack[this.stack.length - 1];
+  const pop = top.pop();
   if (!top.length) this.stack.pop();
-  this.freq[x]--;
-
-  return x;
+  
+  this.freq[pop]--;
+  
+  return pop;
 };
 
-/**
+/** 
  * Your FreqStack object will be instantiated and called as such:
  * var obj = new FreqStack()
  * obj.push(val)
