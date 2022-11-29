294265c968230b6e7caf7c1692c099d49cfd196f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-11-30 01:19:22 +0800

    Time: 925 ms (13.63%), Space: 123.7 MB (24.47%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -3,20 +3,15 @@
  * @return {number[][]}
  */
 var findWinners = function(matches) {
-  const map = {};
-  const set = new Set();
-  const losers = [];
+  const players = {};
+  const ans = Array.from({ length: 2 }, () => []);
   
   for (let [winner, loser] of matches) {
-    set.add(winner);
-    map[loser] ? map[loser]++ : map[loser] = 1;
+    if (!(winner in players)) players[winner] = 0;
+    players[loser] ? players[loser]++ : players[loser] = 1;
   }
   
-  for (let [key, cnt] of Object.entries(map)) {
-    const loser = +key;
-    if (set.has(loser)) set.delete(loser);
-    if (cnt === 1) losers.push(loser);
-  } 
+  Object.entries(players).forEach(([player, cnt]) => cnt < 2 && ans[cnt].push(+player));
   
-  return [[...set].sort((a, b) => a - b), losers];
+  return ans;
 };
