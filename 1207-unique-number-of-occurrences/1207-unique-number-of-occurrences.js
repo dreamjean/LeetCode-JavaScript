8d5d8c7d28e0f0887f7a6d7baf90cb821eaee9c9
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-01 02:12:53 +0800

    Time: 73 ms (83.47%), Space: 42.5 MB (69.07%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -6,11 +6,7 @@ var uniqueOccurrences = function(arr) {
   const map = new Map();
   
   arr.forEach((x) => map[x] ? map[x]++ : map[x] = 1);
-  const cnt = Object.values(map).sort((a, b) => a - b);
+  const cnt = Object.values(map);
   
-  for (let i = 1; i < cnt.length; i++) {
-    if (cnt[i] === cnt[i - 1]) return false;
-  }
-  
-  return true;
+  return cnt.length === new Set(cnt).size;
 };
