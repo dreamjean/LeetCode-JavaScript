c44cd82c46744e64f6b5ab77f9351266f63eceaa
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-04 00:52:53 +0800

    Time: 401 ms (39.39%), Space: 53.3 MB (39.39%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -7,11 +7,11 @@ var closeStrings = function(word1, word2) {
   const cnt1 = countChar(word1);
   const cnt2 = countChar(word2);
   
-  if (Object.keys(cnt1).some((char) => !cnt2[char])) return false;
-  
-  return sortedToString(cnt1) === sortedToString(cnt2);
+  return !checkKey(cnt1, cnt2) && sortedToString(cnt1) === sortedToString(cnt2);
 };
   
 const countChar = (word) => word.split('').reduce((acc, curr) => (acc[curr] ? acc[curr]++ : acc[curr] = 1, acc), {});
+
+const checkKey = (cnt1, cnt2) => Object.keys(cnt1).some((char) => !cnt2[char]);
 
 const sortedToString = (map) => Object.values(map).sort((a, b) => b - a).join('');
