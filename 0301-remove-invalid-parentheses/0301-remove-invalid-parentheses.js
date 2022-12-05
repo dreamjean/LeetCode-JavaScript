d7e3513bfc0e63cd43a3f90f75ccd8bbcd67a7c7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-06 01:01:14 +0800

    Time: 340 ms (10.74%), Space: 49.9 MB (13.52%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var removeInvalidParentheses = function(s) {
+  const set = new Set();
+  const queue = [s];
+  const ans = [];
+  let found = false;
+  
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const str = queue.shift();
+      if (isValid(str)) {
+        ans.push(str);
+        found = true;
+      }
+      
+      if (found) continue;
+      
+      for (let j = 0; j < str.length; j++) {
+        if (/[(/)]/.test(str[j])) {
+          const newStr = str.slice(0, j) + str.slice(j + 1);
+          if (set.has(newStr)) continue;
+          
+          set.add(newStr);
+          queue.push(newStr);
+        }
+      }
+    }
+  }
+  
+  return ans;
+};
+
+const isValid = (s) => {
+  let open = 0;
+  
+  for (let c of s) {
+    if (c === '(') open++;
+    if (c === ')') open--;
+    
+    if (open < 0) return false;
+  }
+  
+  return open === 0;
+}
