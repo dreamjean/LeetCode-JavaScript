39f4e659216b64199c55ac461c4db50c422e243b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-07 02:15:27 +0800

    Time: 120 ms (48.79%), Space: 44.1 MB (90.74%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (!head) return head;
+  
+  let [odd, even] = [head, head.next];
+  
+  while (odd.next && odd.next.next) {
+    let temp = odd.next;
+    odd.next = odd.next.next;
+    odd = odd.next;
+    temp.next = odd.next;
+  }
+  
+  odd.next = even;
+  
+  return head;
+};
