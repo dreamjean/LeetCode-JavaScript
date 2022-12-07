6dafedd5a0a6362850f2ca33f820157b2a0e134b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-08 00:29:36 +0800

    Time: 109 ms (77.78%), Space: 50.8 MB (44.44%) - LeetHub

--- 0481-magical-string/0481-magical-string.js
+++ 0481-magical-string/0481-magical-string.js
@@ -2,15 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-var magicalString = function (n) {
+var magicalString = function(n) {
   const s = [1, 2, 2];
   let i = 2;
-
+  
   while (s.length < n) {
     const even = !(i % 2) ? 1 : 2;
-    s[i] === 1 ? s.push(even) : s.push(even, even);
-    i++;
+    s[i++] === 1 ? s.push(even) : s.push(even, even);
   }
-
+  
   return s.slice(0, n).reduce((a, b) => a + (b === 1), 0);
 };
