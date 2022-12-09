3a915d0936520d0c6d08c04ba36a09b1d245d8fe
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-09 23:08:28 +0800

    Time: 119 ms (44.44%), Space: 44 MB (100.00%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.js
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.js
@@ -4,28 +4,28 @@
  */
 var findRedundantDirectedConnection = function(edges) {
   const n = edges.length;
-  const parents = Array.from({ length: n + 1 }, (_, i) => i);
+  const parent = Array.from({ length: n + 1 }, (_, i) => i);
   let ans1 = [];
   let ans2 = [];
   
-  for (let [x, y] of edges) {
-    const rootX = find(x, parents);
-    const rootY = find(y, parents);
-    if (rootY !== y) ans1 = [x, y];
-    else if (rootX === rootY) ans2 = [x, y];
-    else parents[rootY] = rootX;
+  for (let [u, v] of edges) {
+    const uRoot = find(u, parent);
+    const vRoot = find(v, parent);
+    if (v !== vRoot) ans1 = [u, v];
+    else if (uRoot === vRoot) ans2 = [u, v];
+    else parent[vRoot] = uRoot;
   }
   
   if (!ans1.length) return ans2;
   if (!ans2.length) return ans1;
   
-  return edges.find(([_, y]) => y === ans1[1]);
+  return edges.find(([_, v]) => v === ans1[1]);
 };
 
-const find = (x, parents) => {
+const find = (x, parent) => {
   let root = x;
   
-  while (root !== parents[root]) root = parents[root];
+  while (root !== parent[root]) root = parent[root];
   
   return root;
 }
