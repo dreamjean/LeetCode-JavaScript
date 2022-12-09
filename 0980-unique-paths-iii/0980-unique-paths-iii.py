ec0bc8ba3449fcd3d2cb3c3b6b53c3b9cfac3646
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-10 01:30:11 +0800

    Time: 107 ms (66.40%), Space: 14 MB (55.10%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,37 +1,36 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-var uniquePathsIII = function(grid) {
-  const [m, n] = [grid.length, grid[0].length];
-  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-  let [ans, empty, x, y] = [0, 1, -1, -1];
-  
-  const dfs = (x, y) => {
-    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] < 0) return;    
-    if (grid[x][y] === 2) {
-      if (!empty) ans++;
-      return;
-    } 
-    
-    grid[x][y] = -2;
-    empty--;
-    
-    dirs.forEach(([dx, dy]) => dfs(dx + x, dy + y));
-    
-    grid[x][y] = 0;
-    empty++;
-  }
-  
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      const num = grid[i][j];
-      if (!num) empty++;
-      if (num === 1) [x, y] = [i, j];
-    }
-  }
-  
-  dfs(x, y);
-  
-  return ans;
-};
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:    
+        self.ans = 0
+        m, n, empty = len(grid), len(grid[0]), 1
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
+        for i in range(m):
+            for j in range(n):
+                if not grid[i][j]:
+                    empty += 1
+                elif grid[i][j] == 1:
+                    x, y = i, j
+                    
+        def dfs(x, y, empty):
+            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] < 0:
+                return
+            
+            if (grid[x][y] == 2):
+                self.ans += empty == 0
+                return
+              
+            grid[x][y] = -2
+            empty -= 1
+            
+            for [dx, dy] in dirs:
+                dfs(dx + x, dy + y, empty)
+            
+            grid[x][y] = 0
+            empty += 1
+            
+                    
+        dfs(x, y, empty)
+        
+        return self.ans
+        
+        
