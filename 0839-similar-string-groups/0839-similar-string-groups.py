98189787c4498a441739df44c784ba4e08015067
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-13 16:34:25 +0800

    Time: 7273 ms (7.67%), Space: 14.1 MB (90.98%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -19,13 +19,7 @@ class UnionFind:
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
         def isSimilar(str1, str2):
-            count = 0
-            for i in range(len(str1)):
-               if str1[i] != str2[i]:
-                    count += 1
-                    if count > 2:
-                        return False
-            return True
+            return sum(str1[i] != str2[i] for i in range(len(str1))) <= 2
           
           
         n = len(strs)
