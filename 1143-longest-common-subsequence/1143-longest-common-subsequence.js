1feddf3dd96f58fcd9396f969dcdbaa593fd1d5a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-16 02:23:50 +0800

    Time: 100 ms (91.73%), Space: 56.3 MB (41.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -7,9 +7,9 @@ var longestCommonSubsequence = function(text1, text2) {
   const [m, n] = [text1.length, text2.length];
   const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
   
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1], dp[i][j] + (text1[i] === text2[j]));
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + (text1[i - 1] === text2[j - 1]));
     }
   }
   
