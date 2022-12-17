b572e358f45d57206bab3fec2b76776ca1982d10
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-18 00:10:13 +0800

    Time: 92 ms (61.11%), Space: 41.7 MB (94.44%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.js
+++ 0765-couples-holding-hands/0765-couples-holding-hands.js
@@ -4,29 +4,46 @@
  */
 var minSwapsCouples = function(row) {
   const n = row.length / 2;
-  const parent = Array.from({ length: n }, (_, i) => i);
-  let cnt = 0;
+  const uf = new UnionFind(2 * n);
   
   for (let i = 0; i < 2 * n; i += 2) {
-    if (union(~~(row[i] / 2), ~~(row[i + 1] / 2), parent)) cnt++; 
+    uf.union(~~(row[i] / 2), ~~(row[i + 1] / 2));
   }
-  
-  return n - cnt;
+
+  return 2 * n - uf.count;
 };
 
-const find = (x, parent) => {
-  let root = x;
+class UnionFind {
+  constructor(n) {
+    this.parent = Array.from({ length: n }, (_, i) => i);
+    this.size = new Array(n).fill(1);
+    this.count = n;
+  }
   
-  while (root !== parent[root]) root = parent[root];
+  find(x) {
+    const parent = this.parent;
+    while (x !== parent[x]) {
+      parent[x] = parent[parent[x]];
+      x = parent[x];
+    }
+    
+    return x;
+  }
   
-  return root;
-}
-
-const union = (x, y, parent) => {
-  const xRoot = find(x, parent);
-  const yRoot = find(y, parent);
-  if (xRoot === yRoot) return true;
-  
-  parent[xRoot] = yRoot;
-  return false;
+  union(x, y) {
+    let rootX = this.find(x);
+    let rootY = this.find(y);
+    if (rootX === rootY) return;
+    
+    const { parent, size } = this;
+    if (size[rootX] < size[rootY]) {
+      parent[rootX] = rootY;
+      size[rootY] += size[rootX]
+    }
+    else {
+      parent[rootY] = rootX;
+      size[rootX] += size[rootY]
+    }
+    this.count--;
+  }
 }
