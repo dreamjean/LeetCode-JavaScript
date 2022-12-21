7a7cdf45243a0b45426712ead2fa4f80cbcab99e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-22 01:34:06 +0800

    Time: 659 ms (66.67%), Space: 44.9 MB (66.67%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.js
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.js
@@ -6,27 +6,26 @@ var maxScore = function(nums) {
   const [m, n] = [nums.length, nums.length / 2];
   const memo = new Array(1 << m).fill(0);
   
-  const dfs = (i, mask) => {
+  const dfs = (i = 1, mask = 0) => {
     if (i > n) return 0;
     if (memo[mask]) return memo[mask];
     
     let ans = 0;
-    for (let j = 0; j < m; j++){
+    
+    for (let j = 0; j < m; j++) {
       if ((mask >> j) & 1) continue;
-      
       for (let k = j + 1; k < m; k++) {
         if ((mask >> k) & 1) continue;
-        
-        const source = i * gcd(nums[j], nums[k]);
         const newMask = mask | (1 << j) | (1 << k);
-        ans = Math.max(ans, dfs(i + 1, newMask) + source);
+        const source = i * gcd(nums[j], nums[k]);
+        ans = Math.max(ans, source, source + dfs(i + 1, newMask));
       }
     }
     
     return memo[mask] = ans;
   }
   
-  return dfs(1, 0);
+  return dfs();
 };
 
-const gcd = (a, b) => !b ? a : gcd(b, a % b);
+const gcd = (a, b) => (!b ? a : gcd(b, a % b));
