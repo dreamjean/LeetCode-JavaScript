e248309a1eb2986dfe53ac34a9d5bee377b98f24
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-24 23:37:58 +0800

    Time: 80 ms (70.83%), Space: 43.6 MB (72.92%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTilings = function(n) {
+  const mod = 1e9 + 7;
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 5;
+  
+  for (let i = 4; i <= n; i++)
+    dp[i] = (2 * dp[i - 1] + dp[i - 3]) % mod;
+  
+  return dp[n];
+};
