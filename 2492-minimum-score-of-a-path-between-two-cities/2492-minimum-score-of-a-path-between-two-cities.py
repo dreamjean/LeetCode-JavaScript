fa154b80ff1aab19950aa3dd6d70801bdc454dcf
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2022-12-28 02:25:08 +0800

    Time: 1988 ms (83.09%), Space: 138.2 MB (5.03%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,38 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+      
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+      
+    def union(self, x, y):
+        rootX, rootY = self.find(x), self.find(y)
+        if rootX == rootY:
+            return
+        if self.rank[rootX] < self.rank[rootY]:
+            self.parent[rootX] = rootY
+            self.rank[rootY] += self.rank[rootX]
+        else:
+            self.parent[rootY] = rootX
+            self.rank[rootX] += self.rank[rootY]
+        
+        
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        uf = UnionFind(n + 1)
+        root = uf.find(1)
+        ans = float('inf')
+        
+        for a, b, _ in roads:
+            uf.union(a, b)
+            
+        for a, b, dist in roads:
+            if uf.connected(root, a) or uf.connected(root, b):
+                ans = min(ans, dist)
+        return ans
