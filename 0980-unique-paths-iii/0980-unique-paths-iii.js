6f8a7ca473168f83580df326b8e34f43b42f686a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-01 01:43:19 +0800

    Time: 81 ms (77.92%), Space: 44.8 MB (41.56%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.js
+++ 0980-unique-paths-iii/0980-unique-paths-iii.js
@@ -5,29 +5,30 @@
 var uniquePathsIII = function(grid) {
   const [m, n] = [grid.length, grid[0].length];
   const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-  let [ans, empty, x, y] = [0, 1, -1, -1];
+  let [empty, ans, x, y] = [1, 0, -1, -1];
   
-  const dfs = (x, y) => {
-    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] < 0) return;    
-    if (grid[x][y] === 2) {
+  const dfs = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] < 0) return;
+    
+    if (grid[r][c] === 2) {
       if (!empty) ans++;
       return;
-    } 
+    }
     
-    grid[x][y] = -2;
+    grid[r][c] = -2;
     empty--;
     
-    dirs.forEach(([dx, dy]) => dfs(dx + x, dy + y));
+    dirs.forEach(([dx, dy]) => dfs(r + dx, c + dy));
     
-    grid[x][y] = 0;
+    grid[r][c] = 0;
     empty++;
   }
   
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      const num = grid[i][j];
-      if (!num) empty++;
-      if (num === 1) [x, y] = [i, j];
+      const curr = grid[i][j];
+      if (grid[i][j] === 1) [x, y] = [i, j];
+      if (!grid[i][j]) empty++;
     }
   }
   
