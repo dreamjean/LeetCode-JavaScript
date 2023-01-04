c542e915ca4c656262335ce851fcd7d34457a765
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-05 04:19:13 +0800

    Time: 1745 ms (33.33%), Space: 127.7 MB (66.67%) - LeetHub

---
+++ 1632-rank-transform-of-a-matrix/1632-rank-transform-of-a-matrix.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var matrixRankTransform = function(matrix) {
+  const [m, n] = [matrix.length, matrix[0].length];
+  const rank = new Array(m + n).fill(0);
+  const map = {};
+  
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      const curr = matrix[i][j];
+      map[curr] ? map[curr].push([i, j]) : map[curr] = [[i, j]];
+    }
+  }
+  
+  for (let key of Object.keys(map).sort((a, b) => +a - +b)) {
+    const uf = new UnionFind();
+    const indexs = map[key];
+    
+    indexs.forEach(([x, y]) => uf.union(x, y + m));
+    
+    for (let group of Object.values(uf.getGroup())) {
+      let maxRank = 0;
+      group.forEach((i) => maxRank = Math.max(maxRank, rank[i]));
+      group.forEach((i) => rank[i] = maxRank + 1);
+    }
+    
+    indexs.forEach(([x, y]) => matrix[x][y] = rank[x]);
+  }
+  
+  return matrix;
+};
+
+class UnionFind {
+  constructor() {
+    this.parent = {};
+  }
+  
+  find(x) {
+    const parent = this.parent;
+    if (!parent[x]) parent[x] = x;
+    return x === parent[x] ? x : this.find(parent[x]);
+  }
+  
+  union(x, y) {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
+    if (rootX < rootY) this.parent[rootX] = rootY;
+    else this.parent[rootY] = rootX;
+  }
+  
+  getGroup() {
+    const group = {};
+    for (let i of Object.keys(this.parent)) {
+      const root = this.find(i);
+      group[root] ? group[root].push(+i) : group[root] = [+i];
+    }
+    
+    return group;
+  }
+}
