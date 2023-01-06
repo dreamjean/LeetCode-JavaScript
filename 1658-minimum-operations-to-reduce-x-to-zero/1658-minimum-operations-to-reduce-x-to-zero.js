4cd89def97bc93069ef0c53638a6b273fa8a7673
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-07 01:15:12 +0800

    Time: 127 ms (74.42%), Space: 53.1 MB (95.35%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -4,19 +4,18 @@
  * @return {number}
  */
 var minOperations = function(nums, x) {
-    const sum = nums.reduce((a, b) => a + b) - x;
-    const n = nums.length;
-    let [start, end, currSum, diff] = [0, 0, 0, Number.MIN_VALUE];
+  const sum = nums.reduce((a, b) => a + b) - x;
+  const n = nums.length;
+  let [start, end, currSum, diff] = [0, 0, 0, -1];
+  
+  if (!sum) return n;
+  
+  while (end < n) {
+    currSum += nums[end++];
+    while (start < end && currSum > sum) currSum -= nums[start++];
     
-    if (!sum) return n;
-    
-    while (end < n) {
-        currSum += nums[end++];
-        
-        while (start < end && currSum > sum) currSum -= nums[start++];
-        
-        if (currSum === sum) diff = Math.max(diff, end - start);
-    }
-    
-    return diff === Number.MIN_VALUE ? -1 : n - diff;
+    if (currSum === sum) diff = Math.max(diff, end - start);
+  }
+  
+  return diff > -1 ? n - diff : diff;;
 };
