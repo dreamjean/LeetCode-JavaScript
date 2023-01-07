c045b4b0e4f54a936f73c50184ced520fa27af7e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-08 01:49:08 +0800

    Time: 695 ms (84.22%), Space: 19 MB (74.85%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,18 +1,11 @@
-/**
- * @param {number[]} gas
- * @param {number[]} cost
- * @return {number}
- */
-var canCompleteCircuit = function(gas, cost) {
-  const n = gas.length;
-  let [start, tank, total] = [0, 0, 0];
-  
-  for (let i = 0; i < n; i++) {
-    const curr = gas[i] - cost[i];
-    tank += curr;
-    total += curr;
-    if (tank < 0) [start, tank] = [i + 1, 0];
-  }
-  
-  return total >= 0 ? start : -1;
-};
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = tank = total = 0
+        for i in range(len(gas)):
+            curr = gas[i] - cost[i]
+            tank += curr
+            total += curr
+            if tank < 0:
+                start = i + 1
+                tank = 0
+        return -1 if total < 0 else start
