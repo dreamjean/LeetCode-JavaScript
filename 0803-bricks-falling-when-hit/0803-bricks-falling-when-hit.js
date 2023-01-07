daf95f8c09ee95f9b0f629b9d051ab084a93b67a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-08 03:04:34 +0800

    Time: 298 ms (66.67%), Space: 68.1 MB (83.33%) - LeetHub

--- 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.js
+++ 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.js
@@ -8,44 +8,41 @@ var hitBricks = function(grid, hits) {
   const uf = new UnionFind(m * n + 1);
   const ans = new Array(k).fill(0);
   
-  hits.forEach(([x, y]) => {
-    if (grid[x][y] === 1) grid[x][y] = 2;
+  hits.forEach(([r, c]) => {
+    if (grid[r][c] === 1) grid[r][c] = 2;
   })
   
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
+  for (let i = 0; i < m; i++) 
+    for (let j = 0; j < n; j++) 
       if (grid[i][j] === 1) unionAround(i, j, grid, uf);
-    }
-  }
   
-  for (let i = k - 1; i >= 0; i--) {
-    const [x, y] = hits[i];
+  for (let t = k - 1; t >= 0; t--) {
+    const [x, y] = hits[t];
     if (grid[x][y] === 2) {
       grid[x][y] = 1;
-      
-      let numBricks = uf.componentSize(0);
+      let originBricks = uf.getSize(0);
       unionAround(x, y, grid, uf);
-      let currNumBricks = uf.componentSize(0);
-      ans[i] = Math.max(0, currNumBricks - numBricks - 1);
-      numBricks = currNumBricks;
-    }
+      const currBricks = uf.getSize(0);
+    
+      ans[t] = Math.max(0, currBricks - originBricks - 1);
+    }    
   }
-  
+    
   return ans;
 };
 
 const mark = (x, y, n) => x * n + y + 1;
 
 const unionAround = (x, y, grid, uf) => {
-  const [m, n] = [grid.length, grid[0].length];
   const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-  const currMark = mark(x, y, n);
-  
-  if (!x) uf.union(0, currMark);
+  const [m, n] = [grid.length, grid[0].length];
+  const pos = mark(x, y, n);
+  if (!x) uf.union(0, pos);
   
   for (let [dx, dy] of dirs) {
     const [nx, ny] = [dx + x, dy + y];
-    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] === 1) uf.union(currMark, mark(nx, ny, n));
+    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] === 1)
+      uf.union(pos, mark(nx, ny, n));
   }
 }
 
@@ -54,26 +51,26 @@ class UnionFind {
     this.parent = Array.from({ length: n }, (_, i) => i);
     this.size = new Array(n).fill(1);
   }
+
   find(x) {
     const parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-    
-    return parent[x];
+
+    return x;
+  } 
+
+  getSize(x) {
+    return this.size[this.find(x)];
   }
-  
-  componentSize(x) {
-    let root = this.find(x);
-    return this.size[root];
-  }
-  
+
   union(x, y) {
-    let rootX = this.find(x);
-    let rootY = this.find(y);
+    const rootX = this.find(x);
+    const rootY = this.find(y);
     if (rootX === rootY) return;
-    
+
     const { parent, size } = this;
     if (size[rootX] < size[rootY]) {
       parent[rootX] = rootY;
