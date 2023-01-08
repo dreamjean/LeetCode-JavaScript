8387fa495bd86008855524e1766aa8ac0b1e2221
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-08 19:00:20 +0800

    Time: 151 ms (68.69%), Space: 50.6 MB (57.39%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.js
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -2,24 +2,26 @@
  * @param {number[][]} points
  * @return {number}
  */
-var maxPoints = function (points) {
+var maxPoints = function(points) {
+  const n = points.length;
   let ans = 1;
-
-  for (const [x1, y1] of points) {
+  
+  for (let i = 0; i < n; i++) {
     const map = {};
     let max = 0;
-    for (let i = 1; i < points.length; i++) {
-      const [x2, y2] = points[i];
-      const [a, b] = [x1 - x2, y1 - y2];
-      const k = gcd(a, b);
-      const key = `${a / k}-${b / k}`;
-      map[key] ? map[key]++ : (map[key] = 1);
+    for (let j = 1; j < n; j++) {
+      const [x1, y1] = points[i];
+      const [x2, y2] = points[j];
+      const [diffX, diffY] = [x2 - x1, y2 - y1];
+      const k = gcd(diffX, diffY);
+      const key = `${diffX / k}-${diffY / k}`;
+      map[key] ? map[key]++ : map[key] = 1;
       max = Math.max(max, map[key]);
     }
-
+    
     ans = Math.max(ans, max + 1);
   }
-
+  
   return ans;
 };
 
