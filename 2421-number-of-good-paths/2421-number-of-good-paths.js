fefcec131f1d2d5e098841ed389c590533fe8a91
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-08 23:40:04 +0800

    Time: 735 ms (76.19%), Space: 91 MB (52.38%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.js
+++ 2421-number-of-good-paths/2421-number-of-good-paths.js
@@ -6,26 +6,29 @@
 var numberOfGoodPaths = function(vals, edges) {
   const n = vals.length;
   const uf = new UnionFind(n);
-  const max = Math.max(...vals);  
+  const max = Math.max(...vals);
   const adj = Array.from({ length: max + 1 }, () => []);
-  const map = vals.reduce((acc, curr, i) => (acc[curr] ? acc[curr].push(i) : acc[curr] = [i], acc), {});
+  const map = {};
   let ans = n;
   
-  for (let [u, v] of edges) {
-    const max = Math.max(vals[u], vals[v]);
-    adj[max].push([u, v]);
+  vals.forEach((val, i) => map[val] ? map[val].push(i) : map[val] = [i]);
+  
+  for (let [a, b] of edges) {
+    const max = Math.max(vals[a], vals[b]);
+    adj[max].push([a, b]);
   }
-
-  for (let [val, indexs] of Object.entries(map)) {
-    adj[val].forEach(([u, v]) => uf.union(u, v));
+  
+  for (let [num, indexs] of Object.entries(map)) {
+    adj[num].forEach(([a, b]) => uf.union(a, b));
     
-    const count = {};
-    indexs.forEach((i) => {
+    const group = {};
+    for (let i of indexs) {
       const root = uf.find(i);
-      count[root] ? count[root]++ : count[root] = 1;
-    });
+      group[root] ? group[root]++ : group[root] = 1;
+      
+    }
     
-    for (let num of Object.values(count)) ans += num * (num - 1) / 2;
+    ans += Object.values(group).reduce((acc, curr) => acc + (curr - 1) * curr / 2, 0);
   }
   
   return ans;
@@ -36,22 +39,26 @@ class UnionFind {
     this.parent = Array.from({ length: n }, (_, i) => i);
     this.size = new Array(n).fill(1);
   }
-  
+
   find(x) {
     const parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-    
+
     return x;
   }
   
+  getSize(x) {
+    return this.size[this.find(x)]
+  }
+
   union(x, y) {
     const rootX = this.find(x);
     const rootY = this.find(y);
     if (rootX === rootY) return;
-    
+
     const { parent, size } = this;
     if (size[rootX] < size[rootY]) {
       parent[rootX] = rootY;
