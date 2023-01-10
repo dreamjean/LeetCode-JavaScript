368c0df27a3ce4d73ac244fd0a668c69f470c56d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-10 23:58:06 +0800

    Time: 164 ms (100.00%), Space: 66.1 MB (100.00%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.js
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.js
@@ -9,7 +9,7 @@ var smallestStringWithSwaps = function(s, pairs) {
   const map = {};
   let ans = '';
   
-  pairs.forEach(([a, b]) => uf.union(a, b)); 
+  pairs.forEach(([a, b]) => uf.union(a, b));
   
   for (let i = 0; i < n; i++) {
     const root = uf.find(i);
@@ -17,11 +17,11 @@ var smallestStringWithSwaps = function(s, pairs) {
   }
   
   Object.values(map).forEach((chars) => chars.sort((a, b) => b.charCodeAt() - a.charCodeAt()));
-  
+
   for (let i = 0; i < n; i++) {
     const root = uf.find(i);
-    if (root in map) ans += map[root].pop();
-  }
+    ans += map[root].pop();
+  }  
   
   return ans;
 };
@@ -30,20 +30,24 @@ class UnionFind {
   constructor(n) {
     this.parent = Array.from({ length: n }, (_, i) => i);
   }
-  
+
   find(x) {
     const parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-    
+
     return x;
   }
-  
+
   union(x, y) {
     const rootX = this.find(x);
     const rootY = this.find(y);
-    if (rootX !== rootY) this.parent[rootX] = rootY;
+    if (rootX === rootY) return;
+      
+    rootX < rootY 
+      ? this.parent[rootX] = rootY
+      : this.parent[rootY] = rootX;
   }
 }
