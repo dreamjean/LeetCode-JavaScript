e9f09f7e43be2052c150d05c625e73fbd754ada7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-11 06:05:40 +0800

    Time: 116 ms (91.40%), Space: 54.9 MB (90.32%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
@@ -3,26 +3,51 @@
  * @param {number[][]} connections
  * @return {number}
  */
-var makeConnected = function (n, connections) {
-  if (connections.length < n - 1) return -1;
-
-  const graph = Array.from({ length: n }, () => new Set());
-  const seen = new Array(n).fill(0);
-  let count = 0;
-
-  const dfs = (curr) => {
-    for (let next of graph[curr]) {
-      if (!seen[next]++) dfs(next);
-    }
-  };
-
+var makeConnected = function(n, connections) {
+  const uf = new UnionFind(n);
+  let extract = 0;
+  
   for (let [a, b] of connections) {
-    graph[a].add(b);
-    graph[b].add(a);
+    if (uf.connected(a, b)) extract++;
+    else uf.union(a, b);
+  }
+  
+  return extract >= uf.getCount() - 1 ? uf.getCount() - 1 : -1;
+};
+
+class UnionFind {
+  constructor(n) {
+    this.parent = Array.from({ length: n }, (_, i) => i);
+    this.count = n;
   }
 
-  for (let i = 0; i < n; i++) 
-    if (!seen[i]++ && ++count) dfs(i);
+  find(x) {
+    const parent = this.parent;
+    while (x !== parent[x]) {
+      parent[x] = parent[parent[x]];
+      x = parent[x];
+    }
 
-  return count - 1;
-};
+    return x;
+  }
+  
+  connected(x, y) {
+    return this.find(x) === this.find(y);
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  union(x, y) {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
+    if (rootX === rootY) return;
+
+    rootX < rootY
+      ? this.parent[rootX] = rootY
+      : this.parent[rootY] = rootX;
+    
+    this.count--;
+  }
+}
