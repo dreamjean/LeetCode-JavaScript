1683a2352c0d037dbc7b90ef81ec90938631a580
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-11 17:08:38 +0800

    Time: 201 ms (100.00%), Space: 61.4 MB (100.00%) - LeetHub

--- 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.js
+++ 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.js
@@ -6,11 +6,12 @@
  */
 var areConnected = function(n, threshold, queries) {
   const uf = new UnionFind(n + 1);
+  const map = {};
   
   for (let i = threshold + 1; i <= n; i++) {
-    if (uf.parentIsChanged(i)) continue;
+    if (uf.parentChanged(i)) continue;
     for (let j = i * 2; j <= n; j++) {
-      if (!(j % i)) uf.union(j, i);
+      if (!(j % i)) uf.union(i, j);
     }
   }
   
@@ -22,30 +23,30 @@ class UnionFind {
     this.parent = Array.from({ length: n }, (_, i) => i);
     this.size = new Array(n).fill(1);
   }
-
+  
   find(x) {
     const parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-
+    
     return x;
+  }
+  
+  parentChanged(x) {
+    return this.parent[x] !== x;
   }
   
   connected(x, y) {
     return this.find(x) === this.find(y);
   }
-
-  parentIsChanged(x) {
-    return this.parent[x] !== x;
-  }
-
+  
   union(x, y) {
     const rootX = this.find(x);
     const rootY = this.find(y);
     if (rootX === rootY) return;
-
+    
     const { parent, size } = this;
     if (size[rootX] < size[rootY]) {
       parent[rootX] = rootY;
