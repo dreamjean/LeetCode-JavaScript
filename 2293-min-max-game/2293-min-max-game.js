42377e580c1a0ff2de1faa1cc3c0ad38d14fd8a4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-15 02:11:52 +0800

    Time: 61 ms (91.55%), Space: 44.6 MB (12.68%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.js
+++ 2293-min-max-game/2293-min-max-game.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var minMaxGame = function(nums) {
-    const n = nums.length;
-    if (n === 1) return nums[0];
+  const n = nums.length;
+  
+  if (n === 1) return nums[0];
+  
+  const newNums = nums.reduce((acc, _, i) => {
+    const [even, odd] = [nums[i * 2], nums[i * 2 + 1]];
+    if (i < n / 2) acc.push(i & 1 ? Math.max(even, odd) : Math.min(even, odd));
     
-    const newNums = nums.reduce((arr, _, i) => {
-        const isEven = i % 2 === 0;
-        if (i < n / 2) {
-            const [curr, next] = [nums[i * 2], nums[i * 2 + 1]]
-            const num = isEven ? Math.min(curr, next) : Math.max(curr, next);
-            arr[i] = num;
-        }
-        
-        return arr;
-    }, []);
-    
-    return minMaxGame(newNums);
+    return acc;
+  }, []);
+  
+  return minMaxGame(newNums);
 };
