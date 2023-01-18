a6c11c76cd45c90ed046321e93d24fdb0810ca39
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-19 04:59:24 +0800

    Time: 92 ms (96.00%), Space: 48.6 MB (92.00%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.js
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.js
@@ -2,26 +2,31 @@
  * @param {number[][]} graph
  * @return {number}
  */
-var shortestPathLength = function (graph) {
+var shortestPathLength = function(graph) {
   const n = graph.length;
-  const seen = Array.from({ length: n }, () => new Array(1 << n).fill(false));
+  const visited = Array.from({ length: n }, () => new Array(1 << n).fill(false));
   const queue = [];
-
-  for (let i = 0; i < graph.length; i++) {
-    seen[i][1 << i] = true;
+  
+  for (let i = 0; i < n; i++) {
     queue.push([i, 1 << i, 0]);
+    visited[i][1 << i] = true;
   }
-
+  
   while (queue.length) {
-    const [u, mask, cost] = queue.shift();
-    if (mask === (1 << n) - 1) return cost;
-
-    for (let v of graph[u]) {
-      const maskV = mask | (1 << v);
-      if (seen[v][maskV]) continue;
-
-      seen[v][maskV] = true;
-      queue.push([v, maskV, cost + 1]);
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const [u, mask, cost] = queue.shift();
+      if (mask === (1 << n) - 1) return cost;
+      
+      for (let v of graph[u]) {
+        const maskV = mask | (1 << v);
+        if (visited[v][maskV]) continue;
+        
+        visited[v][maskV] = true;
+        queue.push([v, maskV, cost + 1]);
+      }
     }
   }
+  
+  return -1;
 };
