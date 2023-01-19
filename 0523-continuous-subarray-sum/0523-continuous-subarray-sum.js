1c4da8ddb1210a6bc8b06f2852c2d1089502117b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-20 07:20:35 +0800

    Time: 115 ms (90.00%), Space: 60.8 MB (87.20%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -3,16 +3,17 @@
  * @param {number} k
  * @return {boolean}
  */
-var checkSubarraySum = function (nums, k) {
+var checkSubarraySum = function(nums, k) {
   const set = new Set();
   let [sum, preSum] = [0, 0];
-
-  return nums.some((num) => {
-    sum += num;
-    if (k) sum %= k;
+  
+  for (let num of nums) {
+    sum = (sum + num % k) % k;
     if (set.has(sum)) return true;
-
+    
     set.add(preSum);
     preSum = sum;
-  });
+  }
+  
+  return false;
 };
