12760195486eefa4d301645617ca8057af5137ec
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-23 03:48:10 +0800

    Time: 79 ms (52.27%), Space: 44.1 MB (82.95%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.js
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var calculateTax = function(brackets, income) {
-    let sum = 0;
+  let sum = 0;
+  
+  for (let i = 0; i < brackets.length; i++) {
+    const preUpper = brackets[i - 1]?.[0];
+    let [upper, percent] = brackets[i];
+    upper = upper > income ? income : upper;
+    if (upper <= preUpper) break;
     
-    for (let i = 0; i < brackets.length; i++) {
-        const prevUpper = brackets[i - 1]?.[0];
-        let [upper, percent] = brackets[i];
-        upper = upper > income ? income : upper;
-        if (upper < prevUpper) break;
-        
-        sum += (upper - (prevUpper || 0)) * percent / 100;
-    }
-    
-    return !income ? 0 : sum;
+    sum += (upper - (preUpper || 0)) * percent / 100;
+  }
+  
+  return !income ? 0 : sum;
 };
