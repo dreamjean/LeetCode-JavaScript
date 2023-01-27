536bc4e0d79abaa334738782ce0a1089adf1d6f4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-28 00:16:01 +0800

    Time: 35 ms (86.59%), Space: 14 MB (21.26%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,15 +1,8 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var greatestLetter = function(s) {
-  const set = new Set(s);
-  
-  for (let i = 90; i >= 65; i--) {
-    const upper = String.fromCharCode(i);
-    const lower = String.fromCharCode(i + 32);
-    if (set.has(upper) && set.has(lower)) return upper;
-  }
-  
-  return '';
-};
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        seen = set(s)
+        upper, lower = ord('Z'), ord('z')
+        for i in range(26):
+            if chr(upper - i) in seen and chr(lower - i) in seen:
+                return chr(upper - i)
+        return ''
