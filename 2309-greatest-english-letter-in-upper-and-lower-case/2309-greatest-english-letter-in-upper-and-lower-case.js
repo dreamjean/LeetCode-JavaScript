d2f7fe40798097cf0cd50a98ff3cc4926d0a6a66
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-01-28 00:09:15 +0800

    Time: 55 ms (98.74%), Space: 44.2 MB (81.13%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.js
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.js
@@ -3,13 +3,13 @@
  * @return {string}
  */
 var greatestLetter = function(s) {
-    const set = new Set(s);
-    
-    for (let i = 90; i >= 65; --i) {
-        const upper = String.fromCharCode(i);
-        const lower = String.fromCharCode(i + 32);
-        if (set.has(upper) && set.has(lower)) return upper;
-    }
-    
-    return '';
+  const set = new Set(s);
+  
+  for (let i = 90; i >= 65; i--) {
+    const upper = String.fromCharCode(i);
+    const lower = String.fromCharCode(i + 32);
+    if (set.has(upper) && set.has(lower)) return upper;
+  }
+  
+  return '';
 };
