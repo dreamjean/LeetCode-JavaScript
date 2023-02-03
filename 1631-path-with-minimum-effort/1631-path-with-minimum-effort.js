148a163cc6fdb131754e1d199637715c478f1c1a
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-03 14:08:20 +0800

    Time: 280 ms (84.31%), Space: 52.1 MB (74.51%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
@@ -4,26 +4,22 @@
  */
 var minimumEffortPath = function(heights) {
   const [m, n] = [heights.length, heights[0].length];
-  const uf = new UnionFind(m * n);
   const pq = new MinPriorityQueue({ priority: x => x[0] });
+  const uf = new UnionFind(m * n);
   
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       const curr = heights[i][j];
       const currPos = mark(i, j, n);
-      if (i > 0) pq.enqueue(
-        [Math.abs(curr - heights[i - 1][j]), currPos, mark(i - 1, j, n)]
-      );
-      if (j > 0) pq.enqueue(
-        [Math.abs(curr - heights[i][j - 1]), currPos, mark(i, j - 1, n)]
-      );
+      if (i > 0) pq.enqueue([Math.abs(curr - heights[i - 1][j]), currPos, mark(i - 1, j, n)]);
+      if (j > 0) pq.enqueue([Math.abs(curr - heights[i][j - 1]), currPos, mark(i, j - 1, n)]);
     }
   }
   
   while (!pq.isEmpty()) {
     const [diff, pos1, pos2] = pq.dequeue().element;
     uf.union(pos1, pos2);
-    if (uf.connected(0, mark(m - 1, n -1, n))) return diff;
+    if (uf.connected(0, m * n - 1)) return diff;
   }
   
   return 0;
@@ -34,11 +30,10 @@ const mark = (x, y, n) => x * n + y;
 class UnionFind {
   constructor(n) {
     this.parent = Array.from({ length: n }, (_, i) => i);
-    this.size = new Array(n).fill(1);
   }
 
   find(x) {
-    const parent = this.parent;
+    const { parent } = this;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
@@ -50,19 +45,14 @@ class UnionFind {
   connected(x, y) {
     return this.find(x) === this.find(y);
   }
-  
+
   union(x, y) {
     const rootX = this.find(x);
     const rootY = this.find(y);
     if (rootX === rootY) return;
 
-    const { parent, size } = this;
-    if (size[rootX] < size[rootY]) {
-      parent[rootX] = rootY;
-      size[rootY] += size[rootX];
-    } else {
-      parent[rootY] = rootX;
-      size[rootX] += size[rootY];
-    }
+    rootX < rootY 
+      ? this.parent[rootX] = rootY
+      : this.parent[rootY] = rootX;
   }
 }
