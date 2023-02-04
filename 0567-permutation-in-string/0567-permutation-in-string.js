3f126a66bd725ea0358fce39994fa981333f2e8b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-05 00:01:29 +0800

    Time: 99 ms (65.92%), Space: 43.7 MB (75.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -3,23 +3,23 @@
  * @param {string} s2
  * @return {boolean}
  */
-var checkInclusion = function (s1, s2) {
-  const cnt = new Array(26).fill(0);
-  const n = s1.length;
-  const a = "a".charCodeAt();
-
-  for (let i = 0; i < n; i++) {
-    cnt[s1.charCodeAt(i) - a]++;
-    cnt[s2.charCodeAt(i) - a]--;
+var checkInclusion = function(s1, s2) {
+  const cnt = new Array(126).fill(0);
+  const [n1, n2] = [s1.length, s2.length];
+  
+  
+  for (let i = 0; i < n1; i++) {
+    cnt[s1.charCodeAt(i)]++;
+    cnt[s2.charCodeAt(i)]--;
   }
-
+  
   if (cnt.every((x) => !x)) return true;
-
-  for (let i = n; i < s2.length; i++) {
-    cnt[s2.charCodeAt(i) - a]--;
-    cnt[s2.charCodeAt(i - n) - a]++;
+  
+  for (let i = n1; i < n2; i++) {
+    cnt[s2.charCodeAt(i)]--;
+    cnt[s2.charCodeAt(i - n1)]++;
     if (cnt.every((x) => !x)) return true;
   }
-
+  
   return false;
 };
