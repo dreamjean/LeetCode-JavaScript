85934dbfcfdaf0c594866bc511e40ce33330524d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-05 22:59:24 +0800

    Time: 220 ms (11.11%), Space: 54.8 MB (22.22%) - LeetHub

--- 1210-minimum-moves-to-reach-target-with-rotations/1210-minimum-moves-to-reach-target-with-rotations.js
+++ 1210-minimum-moves-to-reach-target-with-rotations/1210-minimum-moves-to-reach-target-with-rotations.js
@@ -6,30 +6,25 @@ var minimumMoves = function(grid) {
   const n = grid.length;
   const dirs = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
   const visited = new Set('0-0-0');
-  const queue = [[0, 0, 0]];
-  let step = 0;
+  const queue = [[0, 0, 0, 0]];
   
   while (queue.length) {
-    const size = queue.length;
-    for (let i = 0; i < size; i++) {
-      const [x, y, r] = queue.shift();
-      if (x === n - 1 && y === n - 2) return step;
+    const [x, y, r, cost] = queue.shift();
+    if (x === n - 1 && y === n - 2) return cost;
 
-      for (let [dx, dy, dr] of dirs) {
-        const [nx, ny, nr] = [dx + x, dy + y, dr ^ r];
-        const [x2, y2] = [nx + nr, ny + (nr ^ 1)];
-        if (
-          visited.has(`${nx}-${ny}-${nr}`) || x2 >= n || y2 >= n ||
-          grid[nx][ny] || grid[x2][y2] || (nr !== r && grid[nx + 1][ny + 1])  
-        )
-          continue;
+    for (let [dx, dy, dr] of dirs) {
+      const [nx, ny, nr] = [dx + x, dy + y, dr ^ r];
+      const [x2, y2] = [nx + nr, ny + (nr ^ 1)];
+      if (
+        visited.has(`${nx}-${ny}-${nr}`) || x2 >= n || y2 >= n ||
+        grid[nx][ny] || grid[x2][y2] || (nr !== r && grid[nx + 1][ny + 1])  
+      )
+        continue;
 
-        visited.add(`${nx}-${ny}-${nr}`);
-        queue.push([nx, ny, nr]);
-      }
+      visited.add(`${nx}-${ny}-${nr}`);
+      queue.push([nx, ny, nr, cost + 1]);
     }
-    
-    step++;
+
   }
   
   return -1;
