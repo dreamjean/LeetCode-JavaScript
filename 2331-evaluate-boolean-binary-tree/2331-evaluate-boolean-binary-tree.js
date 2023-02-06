b190deea663af317da18fbb9c012d621d24358e4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-06 23:26:36 +0800

    Time: 76 ms (69.64%), Space: 47.6 MB (98.81%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -11,7 +11,6 @@
  * @return {boolean}
  */
 var evaluateTree = function(root) {
-  if (!root) return;
   if (!root.left && !root.right) return root.val;
   
   const left = evaluateTree(root.left);
