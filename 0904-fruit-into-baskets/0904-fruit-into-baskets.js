55597214bea3bd5c4fdbcfaa4816e8f248cddcdc
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-08 00:58:12 +0800

    Time: 88 ms (98.05%), Space: 50.3 MB (92.62%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -2,16 +2,16 @@
  * @param {number[]} fruits
  * @return {number}
  */
-var totalFruit = function (fruits) {
-  let [fruitA, fruitB, cntB, currMax, max] = new Array(5).fill(0);
-
+var totalFruit = function(fruits) {
+  let [fruitA, fruitB, currA, currB, currMax, max] = [-1, -1, 0, 0, 0, 0];
+  
   for (let fruit of fruits) {
-    currMax = fruit === fruitA || fruit === fruitB ? currMax + 1 : cntB + 1;
-    cntB = fruit === fruitB ? cntB + 1 : 1;
-
+    currMax = fruit === fruitA || fruit === fruitB ? currMax + 1 : currB + 1;
+    currB = fruit === fruitB ? currB + 1 : 1;
     if (fruit !== fruitB) [fruitA, fruitB] = [fruitB, fruit];
+    
     max = Math.max(max, currMax);
   }
-
+  
   return max;
 };
