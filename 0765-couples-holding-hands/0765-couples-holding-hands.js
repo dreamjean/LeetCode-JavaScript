547084f4f45506190c074da415eb1b8812c8e813
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-08 00:29:25 +0800

    Time: 67 ms (64.71%), Space: 42.4 MB (23.53%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.js
+++ 0765-couples-holding-hands/0765-couples-holding-hands.js
@@ -6,35 +6,39 @@ var minSwapsCouples = function(row) {
   const n = row.length / 2;
   const uf = new UnionFind(n);
   
-  for (let i = 0; i < 2 * n; i += 2) {
+  for (let i = 0; i < n * 2; i += 2) {
     uf.union(~~(row[i] / 2), ~~(row[i + 1] / 2));
   }
-
-  return n - uf.count;
+  
+  return uf.getCount();
 };
 
 class UnionFind {
   constructor(n) {
     this.parent = Array.from({ length: n }, (_, i) => i);
-    this.count = n;
+    this.count = 0;
   }
-  
+
   find(x) {
-    const parent = this.parent;
+    const { parent } = this;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-    
+
     return x;
   }
-  
+
+  getCount() {
+    return this.count;
+  }
+
   union(x, y) {
-    let rootX = this.find(x);
-    let rootY = this.find(y);
+    const rootX = this.find(x);
+    const rootY = this.find(y);
     if (rootX === rootY) return;
-    
-    this.parent[rootX] = rootY;
-    this.count--;
+
+    this.parent[rootX] = rootY; 
+    this.count++;
   }
 }
