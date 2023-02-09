a33a12e3f7a57591301bc8bc642342e067ed2181
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-10 00:49:31 +0800

    Time: 409 ms (83.33%), Space: 54.6 MB (83.33%) - LeetHub

--- 2493-divide-nodes-into-the-maximum-number-of-groups/2493-divide-nodes-into-the-maximum-number-of-groups.js
+++ 2493-divide-nodes-into-the-maximum-number-of-groups/2493-divide-nodes-into-the-maximum-number-of-groups.js
@@ -7,33 +7,34 @@ var magnificentSets = function(n, edges) {
   const graph = Array.from({ length: n + 1 }, () => []);
   const map = {};
   
-  for (let [a, b] of edges) {
+  edges.forEach(([a, b]) => {
     graph[a].push(b);
     graph[b].push(a);
-  }
+  })
   
   for (let i = 1; i <= n; i++) {
     const leave = new Array(n + 1).fill(0);
     const queue = [[i, 1]];
-    let [maxDepth, smallestIdx] = [0, Infinity];
+    let [smallIndex, maxDepth] = [Infinity, 0];
     leave[i] = 1;
     
     while (queue.length) {
       const [curr, depth] = queue.shift();
+      smallIndex = Math.min(smallIndex, curr);
       maxDepth = Math.max(maxDepth, depth);
-      smallestIdx = Math.min(smallestIdx, curr);
       
       for (let next of graph[curr]) {
         if (leave[next] === depth) return -1;
+        
         if (!leave[next]) {
           leave[next] = depth + 1;
-          queue.push([next, depth + 1])
+          queue.push([next, depth + 1]);
         }
       }
     }
     
-    map[smallestIdx] = Math.max(map[smallestIdx] || 0, maxDepth);
+    map[smallIndex] = Math.max(map[smallIndex] || 0, maxDepth);
   }
   
-  return Object.values(map).reduce((a, b) => a + b);
+  return [...Object.values(map)].reduce((a, b) => a + b);
 };
