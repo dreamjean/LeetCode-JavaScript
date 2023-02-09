2777730c00a51f1c3e941df2a1223ed2dd551a3b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-09 23:58:26 +0800

    Time: 580 ms (84.78%), Space: 28.6 MB (43.48%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -1,33 +1,14 @@
-/**
- * @param {string[]} ideas
- * @return {number}
- */
-var distinctNames = function(ideas) {
-  const suffices = Array.from({ length: 26 }, () => new Set());
-  let ans = 0;
-  
-  ideas.forEach((idea) => {
-    const [first, second] = [idea[0], idea.slice(1)];
-    suffices[first.charCodeAt() - 'a'.charCodeAt()].add(second);
-  });
-  
-  for (let i = 0; i < 25; i++) {
-    for (let j = i + 1; j < 26; j++) {
-      const [setA, setB] = [suffices[i], suffices[j]];
-      const m = getSameCount(setA, setB);
-      ans += 2 * (setA.size - m) * (setB.size - m);
-    }
-  }
-  
-  return ans;
-};
-
-const getSameCount = (setA, setB) => {
-  let count = 0;
-  
-  for (let word of setA) {
-    if (setB.has(word)) count++;
-  }
-  
-  return count;
-}
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+        count = [set() for _ in range(26)]
+        ans = 0
+        
+        for idea in ideas:
+            count[ord(idea[0]) - ord('a')].add(idea[1:])
+            
+        for i in range(25):
+            for j in range(i + 1, 26):
+                a, b = count[i], count[j]
+                m = len(a & b)
+                ans += 2 * (len(a) - m) * (len(b) - m)
+        return ans
