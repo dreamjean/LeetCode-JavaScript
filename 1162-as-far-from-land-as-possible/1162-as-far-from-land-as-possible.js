c1daddc5232c5e60e2b04f8458f641e4554fb799
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-10 23:48:07 +0800

    Time: 139 ms (57.55%), Space: 49.7 MB (49.06%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.js
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.js
@@ -2,36 +2,35 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var maxDistance = function (grid) {
-  const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
+var maxDistance = function(grid) {
+  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
   const n = grid.length;
   const queue = [];
-  let steps = -1;
-
-  for (let i = 0; i < n; i++)
-    for (let j = 0; j < n; j++) if (grid[i][j] === 1) queue.push([i, j]);
-
-  if (!queue.length || queue.length === n * n) return steps;
-
+  let step = -1;
+  
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j]) queue.push([i, j]);
+    }
+  }
+  
+  if (!queue.length || queue.length === n * n) return step;
+  
   while (queue.length) {
-    ++steps;
-    let size = queue.length;
-    while (size--) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
       const [r, c] = queue.shift();
-      for (const [dx, dy] of dirs) {
+      for (let [dx, dy] of dirs) {
         const [x, y] = [dx + r, dy + c];
         if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y]) continue;
-
+        
         grid[x][y] = 1;
         queue.push([x, y]);
       }
     }
+    
+    step++;
   }
-
-  return steps;
+  
+  return step;
 };
