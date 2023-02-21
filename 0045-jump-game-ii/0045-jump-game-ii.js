77c3501b41ed36079ea4af6fd7daa1c32b7d4d59
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-21 19:25:34 +0800

    Time: 75 ms (54.66%), Space: 44 MB (61.74%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var jump = function(nums) {
-  let [jumps, end, curFarthest] = [0, 0, 0];
+  let [jumps, end, farCanReach] = [0, 0, 0];
   
   for (let i = 0; i < nums.length - 1; i++) {
-    curFarthest = Math.max(curFarthest, nums[i] + i);
-    if (end === i) {
-      end = curFarthest;
+    farCanReach = Math.max(farCanReach, nums[i] + i);
+    if (i === end) {
+      end = farCanReach;
       jumps++;
     }
   }
