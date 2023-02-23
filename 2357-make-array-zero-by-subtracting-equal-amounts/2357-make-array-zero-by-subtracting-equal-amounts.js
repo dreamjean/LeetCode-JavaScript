607265ebf115efa769861ecb841c1d7bb756c7b1
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-24 01:38:12 +0800

    Time: 48 ms (99.27%), Space: 41.9 MB (73.54%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    const set = new Set(nums.filter(num => num));
-    
-    return set.size;
+  const set = new Set(nums.filter(num => num));
+  
+  return set.size;
 };
