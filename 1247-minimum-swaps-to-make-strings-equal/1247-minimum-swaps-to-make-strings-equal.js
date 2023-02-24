291c50491c5815d330d07801141f214e44e26c4b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-25 00:45:20 +0800

    Time: 47 ms (100.00%), Space: 42.3 MB (86.67%) - LeetHub

--- 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.js
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.js
@@ -7,10 +7,9 @@ var minimumSwap = function(s1, s2) {
   let [x, y] = [0, 0];
   
   for (let i = 0; i < s1.length; i++) {
-    if (s1[i] !== s2[i]) {
-      s1[i] === 'x' ? x++ : y++;
-    }
+    if (s1[i] === s2[i]) continue;
+    s1[i] === 'x' ? x++ : y++;
   }
   
-  return (x + y) % 2 ? -1 : ~~((x + 1) / 2) + ~~((y + 1) / 2);
+  return (x + y) & 1 ? -1 : ~~((x + 1) / 2) + ~~((y + 1) / 2);
 };
