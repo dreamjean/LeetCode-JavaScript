b803bb62234240e23b7e6226a65a86db08ad6ed5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-25 23:03:54 +0800

    Time: 67 ms (85.58%), Space: 44.2 MB (26.64%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -7,17 +7,15 @@ var wordBreak = function(s, wordDict) {
   const set = new Set(wordDict);
   const ok = [true];
   
-  for (let i = 1; i <= s.length; i++) {
+  for (let i = 1; i <= s.length; i++) 
     ok.push(check(s, i, set, ok));
-  }
   
   return ok.at(-1);
 };
 
 const check = (s, i, set, ok) => {
-  for (let j = 0; j < i; j++) {
+  for (let j = 0; j < i; j++) 
     if (set.has(s.slice(j, i)) && ok[j]) return true;
-  }
   
   return false;
 }
