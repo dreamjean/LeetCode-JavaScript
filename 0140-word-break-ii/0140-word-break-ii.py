59e12c49b20387ee28d66fe656d3ed175626a8b2
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-28 02:54:31 +0800

    Time: 32 ms (66.68%), Space: 13.8 MB (73.99%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,25 +1,16 @@
-/**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {string[]}
- */
-var wordBreak = function(s, wordDict) {
-  const set = new Set(wordDict);
-  const n = s.length;
-  const ans = [];
-  
-  const dfs = (start, path) => {
-    if (start === n) return ans.push([...path].join(' '));
-    
-    for (let i = start; i < n; i++) {
-      const sub = s.substring(start, i + 1);
-      if (!set.has(sub)) continue;
-      
-      dfs(i + 1, [...path, sub]);
-    }
-  }
-  
-  dfs(0, []);
-  
-  return ans;
-};
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def dfs(start, path):
+            if start == len(s):
+                ans.append(' '.join(path))
+                return
+            for i in range(start, len(s)):
+                sub = s[start:i+1]
+                if sub in seen:
+                    dfs(i + 1, path + [sub])
+          
+          
+        seen = set(wordDict)
+        ans = []
+        dfs(0, [])
+        return ans
