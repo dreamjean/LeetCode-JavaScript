4e8e1c4b0d55c8711602238215bd7ebfcf575f28
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-28 01:51:11 +0800

    Time: 82 ms (68.18%), Space: 45.2 MB (100.00%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.js
+++ 0427-construct-quad-tree/0427-construct-quad-tree.js
@@ -15,12 +15,12 @@
  * @return {Node}
  */
 var construct = function(grid) {
-  return solve(0, 0, grid, grid.length);
+  return solve(0, 0, grid.length, grid);
 };
 
-const sameValue = (x1, y1, grid, length) => {
-  for (let i = x1; i < x1 + length; i++) {
-    for (let j = y1; j < y1 + length; j++) {
+const sameValue = (x1, y1, n, grid) => {
+  for (let i = x1; i < x1 + n; i++) {
+    for (let j = y1; j < y1 + n; j++) {
       if (grid[i][j] !== grid[x1][y1]) return false;
     }
   }
@@ -28,17 +28,17 @@ const sameValue = (x1, y1, grid, length) => {
   return true;
 }
 
-const solve = (x1, y1, grid, length) => {
-  if (sameValue(x1, y1, grid, length)) return new Node(grid[x1][y1] === 1, true);
+const solve = (x1, y1, n, grid) => {
+  if (sameValue(x1, y1, n, grid)) return new Node(grid[x1][y1] === 1, true);
   
-  length /= 2;
+  n /= 2;
   
   return new Node(
+    false, 
     false,
-    false,
-    solve(x1, y1, grid, length),
-    solve(x1, y1 + length, grid, length),
-    solve(x1 + length, y1, grid, length),
-    solve(x1 + length, y1 + length, grid, length)
+    solve(x1, y1, n, grid),
+    solve(x1, y1 + n, n, grid),
+    solve(x1 + n, y1, n, grid),
+    solve(x1 + n, y1 + n, n, grid)
   )
 }
