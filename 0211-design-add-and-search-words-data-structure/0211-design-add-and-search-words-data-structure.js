84d2c365671b0093330a8ea87135380d5879a674
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-02-28 19:57:46 +0800

    Time: 2270 ms (54.00%), Space: 112.1 MB (16.96%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -3,6 +3,19 @@ class TrieNode {
     this.children = new Array(26).fill(null);
     this.isWord = false;
   }
+  
+  insert(word) {
+    let node = this;
+  
+    for (let ch of word) {
+      const index = ch.charCodeAt() - 'a'.charCodeAt();
+      if (!node.children[index]) node.children[index] = new TrieNode();
+
+      node = node.children[index];
+    }
+
+    node.isWord = true;
+  }
 }
 
 var WordDictionary = function() {
@@ -14,16 +27,7 @@ var WordDictionary = function() {
  * @return {void}
  */
 WordDictionary.prototype.addWord = function(word) {
-  let node = this.root;
-  
-  for (let ch of word) {
-    const index = ch.charCodeAt() - 'a'.charCodeAt();
-    if (!node.children[index]) node.children[index] = new TrieNode();
-    
-    node = node.children[index];
-  }
-  
-  node.isWord = true;
+  return this.root.insert(word);
 };
 
 /** 
@@ -31,16 +35,16 @@ WordDictionary.prototype.addWord = function(word) {
  * @return {boolean}
  */
 WordDictionary.prototype.search = function(word) {
-  const dfs = (index, node, word) => {
+  const dfs = (index, node) => {
     if (index === word.length) return node.isWord;
     
     const ch = word[index++];
     if (ch !== '.') {
       const child = node.children[ch.charCodeAt() - 'a'.charCodeAt()];
-      if (child && dfs(index, child, word)) return true;
+      if (child && dfs(index, child)) return true;
     } else {
       for (let child of node.children) 
-        if (child && dfs(index, child, word)) return true;
+        if (child && dfs(index, child)) return true;
     }
     
     return false;
