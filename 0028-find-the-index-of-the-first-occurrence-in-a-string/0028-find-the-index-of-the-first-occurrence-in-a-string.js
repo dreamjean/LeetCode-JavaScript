50e9f1eacf6b4a043df685d441d2f1e4206fea96
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-03 16:56:52 +0800

    Time: 59 ms (84.25%), Space: 42.3 MB (34.81%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  const n = needle.length;
+  
+  for (let i = 0; i < haystack.length; i++) {
+    if (!needle.startsWith(haystack[i])) continue;
+      
+    if (haystack.substring(i, i + n) === needle) return i;
+  }
+  
+  return -1;
+};
