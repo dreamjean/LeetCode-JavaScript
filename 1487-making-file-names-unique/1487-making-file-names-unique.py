796c39e5520a3247edc283656161548f59d2b82f
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-03 16:50:57 +0800

    Time: 9020 ms (5.14%), Space: 29.9 MB (11.43%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.py
+++ 1487-making-file-names-unique/1487-making-file-names-unique.py
@@ -1,15 +1,16 @@
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
         d = {}
-        for name in names:
+        for i, name in enumerate(names):
             if name in d:
                 k = d[name]
                 while f'{name}({k})' in d:
                     k += 1
                 d[f'{name}({k})'] = 1
                 d[name] = k + 1
+                names[i] = f'{name}({k})'
             d[name] = 1
-        return d.keys()
+        return names
         
         
         
