92526f7767c58eb64c9cf7411c40e8b4187a23b7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-04 01:55:54 +0800

    Time: 564 ms (77.78%), Space: 86.4 MB (55.56%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.js
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.js
@@ -3,49 +3,21 @@
  * @return {number}
  */
 var findMaximumXOR = function(nums) {
-  const root = new TrieNode();
-  let max = 0;
+  let [max, mask] = [0, 0];
   
-  nums.forEach((num) => {
-    insert(root, num);
-    max = Math.max(max, maxXOR(root, num));
-  })
+  for (let i = 31; i >= 0; i--) {
+    mask = mask | (1 << i);
+    const set = new Set();
+    nums.forEach((num) => set.add(num & mask));
+    
+    let temp = max | (1 << i);
+    for (let prefix of set) {
+      if (set.has(temp ^ prefix)) {
+        max = temp;
+        break;
+      }
+    }
+  }
   
   return max;
 };
-
-const maxXOR = (root, num) => {
-  let node = root;
-  let ans = 0;
-  
-  for (let i = 31; i >= 0; i--) {
-    const bit = (num >> i) & 1;
-    if (node.next[1 - bit]) {
-      ans |= (1 << i);
-      node = node.next[1 - bit];
-    }
-    
-    else node = node.next[bit];
-  }
-  
-  return ans;
-}
-
-const insert = (root, num) => {
-  let node = root;
-  
-  for (let i = 31; i >= 0; i--) {
-    const bit = (num >> i) & 1;
-    if (!node.next[bit]) node.next[bit] = new TrieNode();
-    
-    node = node.next[bit];
-  }
-  
-  return node;
-}
-
-class TrieNode {
-  constructor() {
-    this.next = [null, null];
-  }
-}
