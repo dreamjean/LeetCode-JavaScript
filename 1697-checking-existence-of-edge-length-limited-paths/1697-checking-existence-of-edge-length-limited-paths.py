120a7a5e3f5de676fe828bf99d3a7ea14ac61d32
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-04 22:38:41 +0800

    Time: 2040 ms (59.36%), Space: 60.4 MB (56.16%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
@@ -1,33 +1,36 @@
 class UnionFind:
     def __init__(self, n: int):
-        self.parent = [i for i in range(n)]
+        self.parent = list(range(n))
         
     def find(self, x):
         if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
+            x = self.find(self.parent[x])
         return self.parent[x]
       
-    def isConnect(self, x, y):
+    def isConnected(self, x, y):
         return self.find(x) == self.find(y)
       
     def union(self, x, y):
-        rootX, rootY = self.find(x), self.find(y)
-        if rootX != rootY:
-            self.parent[rootX] = rootY
-        
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
+            return
+        if root_x < root_y:
+            self.parent[root_x] = root_y
+        else:
+            self.parent[root_y] = root_x
+            
 
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
         edgeList.sort(key=lambda x: x[2])
         queries = sorted([q + [i] for i, q in enumerate(queries)], key=lambda x: x[2])
         uf = UnionFind(n)
-        ans = [None] * len(queries)
+        ans = [False] * len(queries)
         j = 0
-        
         for p, q, limit, i in queries:
             while j < len(edgeList) and edgeList[j][2] < limit:
                 u, v, _ = edgeList[j]
                 uf.union(u, v)
                 j += 1
-            ans[i] = uf.isConnect(p, q)
+            ans[i] = uf.isConnected(p, q)
         return ans
