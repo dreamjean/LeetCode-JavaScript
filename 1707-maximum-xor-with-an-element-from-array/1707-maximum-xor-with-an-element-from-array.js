f3ff6c8cd63cf26073c7664bf2b2995300cce3c6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-07 00:31:55 +0800

    Time: 1620 ms (50.00%), Space: 161.9 MB (50.00%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.js
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.js
@@ -4,60 +4,50 @@
  * @return {number[]}
  */
 var maximizeXor = function(nums, queries) {
-
-    // 字典树
-    const trie = [null, null];
-    const HIGH_BIT = 30;
-
-    function insert(val) {
-        // use node to keep track of the trie
-        let node = trie;
-        for (let i = HIGH_BIT - 1; i >= 0; --i) {
-            const bit = (val >> i) & 1;
-            if (node[bit] === null) {
-                node[bit] = [null, null];
-            }
-            node = node[bit];
-        }
+  nums.sort((a, b) => a - b);
+  queries = queries.map((q, i) => [...q, i]).sort((a, b) => a[1] - b[1]);
+  const trie = new Trie();
+  const ans = new Array(queries.length);
+  let j = 0;
+  
+  for (let [x, m, i] of queries) {
+    while (j < nums.length && nums[j] <= m) {
+      trie.insert(nums[j++]);
     }
-
-    function getMaxXor(val) {
-        let ans = 0;
-        let node = trie;
-        for (let i = HIGH_BIT - 1; i >= 0; --i) {
-            let bit = (val >> i) & 1;
-            if (node[bit ^ 1] !== null) {
-                ans |= 1 << i;
-                bit ^= 1;
-            }
-            node = node[bit];
-        }
-        return ans;
-    }
-
-    queries.forEach((v, i) => v.push(i));
-    queries.sort((a, b) => a[1] - b[1]);
-    const qlen = queries.length;
-    const ans = new Array(qlen);
-    let idx = 0;
-
-    nums.sort((a, b) => a - b);
-    const n = nums.length;
-    for (const q of queries) {
-        const x = q[0];
-        const m = q[1];
-        const qid = q[2];
-
-        while (idx < n && nums[idx] <= m) {
-            insert(nums[idx]);
-            ++idx;
-        }
-        if (idx === 0) {
-            ans[qid] = -1;
-        } else {
-            ans[qid] = getMaxXor(x);
-        }
-    }
-
-    return ans;
+    
+    ans[i] = !j ? -1 : trie.maxXOR(x);
+  }
+  
+  return ans;
 };
+
+class Trie {
+  constructor() {
+    this.root = [null, null];
+  }
+  
+  insert(num) {
+    let node = this.root;
+    for (let i = 30; i >= 0; --i) {
+      const bit = (num >> i) & 1;
+      if (node[bit] === null) node[bit] = [null, null];
+      
+      node = node[bit];
+    }
+  }
+  
+  maxXOR(num) {
+    let node = this.root;
+    let ans = 0;
+    for (let i = 30; i >= 0; --i) {
+      let bit = (num >> i) & 1;
+      if (node[1 - bit] !== null) {
+        ans |= (1 << i);
+        bit ^= 1;
+      }
+      node = node[bit];
+    }
+    
+    return ans;
+  }
+}
