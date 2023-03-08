dc9ec92f74e9905bd11f4b426ec5100904e6bc09
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-09 02:14:53 +0800

    Time: 59 ms (87.36%), Space: 43.3 MB (18.39%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.js
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.js
@@ -7,8 +7,8 @@ var minimumRecolors = function(blocks, k) {
   let [cntW, ans] = [0, 100];
   
   for (let i = 0; i < blocks.length; i++) {
-    cntW += (blocks[i] === 'W');
-    if (i >= k) cntW -= (blocks[i - k] === 'W');
+    if (blocks[i] === 'W') ++cntW;
+    if (i >= k && blocks[i - k] === 'W') --cntW;
     if (i >= k - 1) ans = Math.min(ans, cntW);
   }
   
