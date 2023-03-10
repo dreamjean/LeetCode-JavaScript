7005bcfc117f8eaa352ce2a9701dd7f7254931a1
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-11 02:46:26 +0800

    Time: 115 ms (20.73%), Space: 50.2 MB (8.54%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ */
+var Solution = function(head) {
+  this.head = head;
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.getRandom = function() {
+  let [node, i, ans] = [this.head, 1, 0];
+  
+  while (node) {
+    if (~~(Math.random() * i++) === 0) ans = node.val;
+    
+    node = node.next;
+  }
+  
+  return ans;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
