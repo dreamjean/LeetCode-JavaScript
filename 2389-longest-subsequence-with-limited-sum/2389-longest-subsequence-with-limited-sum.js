205ffbfd3d25ebe29848c10a2d42d25687211e2d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-17 04:28:21 +0800

    Time: 88 ms (66.81%), Space: 45.2 MB (40.09%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -8,15 +8,15 @@ var answerQueries = function(nums, queries) {
     .sort((a, b) => a - b)
     .reduce((acc, curr, i) => (acc.push(!i ? curr : curr + acc.at(-1)), acc), []);
   
-  return queries.map((q) => search(sums, q));
+  return queries.map((num) => search(num, sums));
 };
 
-const search = (sums, query) => {
+const search = (target, sums) => {
   let [l, r] = [0, sums.length - 1];
   
   while (l <= r) {
     const mid = (l + r) >>> 1;
-    if (sums[mid] > query) r = mid - 1;
+    if (sums[mid] > target) r = mid - 1;
     else l = mid + 1;
   }
   
