5a5e421b66b7ad0d84a09037ae73e3daaaef737e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-23 00:27:39 +0800

    Time: 1731 ms (76.51%), Space: 58.6 MB (96.31%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,35 +1,29 @@
 class UnionFind:
-    def __init__(self, n: int):
+    def __init__(self, n):
         self.parent = list(range(n))
-        self.rank = [0] * n
         
     def find(self, x):
         if x != self.parent[x]:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
       
-    def connected(self, x, y):
+    def isConnected(self, x, y):
         return self.find(x) == self.find(y)
       
     def union(self, x, y):
-        rootX, rootY = self.find(x), self.find(y)
-        if rootX == rootY:
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
             return
-        if self.rank[rootX] > self.rank[rootY]:
-            rootX, rootY = rootY, rootX
-        self.parent[rootX] = rootY
+        self.parent[root_x] = root_y
         
-        
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         uf = UnionFind(n + 1)
-        root = uf.find(1)
         ans = float('inf')
-        
         for a, b, _ in roads:
             uf.union(a, b)
-            
-        for a, b, dist in roads:
-            if uf.connected(root, a) or uf.connected(root, b):
-                ans = min(ans, dist)
+        for a, _, distance in roads:
+            if uf.isConnected(1, a):
+                ans = min(ans, distance)
         return ans
