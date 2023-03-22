afd5c4c5f041f449171a6fec4b01bb8da23f8d3b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-22 23:21:15 +0800

    Time: 177 ms (64.62%), Space: 46.1 MB (83.96%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.js
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.js
@@ -4,27 +4,20 @@
  * @return {number}
  */
 var bestTeamScore = function(scores, ages) {
-    let arr = []
-    for(let i =0; i<scores.length;i++){
-        arr.push([ages[i],scores[i]])
+  const person = ages
+    .map((age, i) => [age, scores[i]])
+    .sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0])
+  const n = ages.length;
+  const dp = new Array(n).fill(0);
+  dp[0] = person[0][1];
+  
+  for (let i = 1; i < n; i++) {
+    dp[i] = person[i][1];
+    for (let j = 0; j < i; j++) {
+      if (person[i][1] >= person[j][1])
+        dp[i] = Math.max(dp[i], dp[j] + person[i][1]);
     }
-    arr.sort((a,b)=>a[0]==b[0] ? a[1] - b[1] : a[0] - b[0])
-    
-    let res = new Array(scores.length).fill(0);
-    res[0] = arr[0][1];
-
-    for(let i = 1; i<arr.length; i++){
-        let [fa , fs] = arr[i]
-        res[i] = fs;
-        for(let j =i-1; j>=0; j--){
-            let [sa, ss] = arr[j]
-            if(sa == fa) res[i] = Math.max(res[i], res[j] + fs);
-            else{
-                if(fs >= ss ){
-                    res[i] = Math.max(res[i], res[j] + fs);
-                }
-            }
-        }
-    }
-    return Math.max(...res)
+  }
+  
+  return Math.max(...dp);
 };
