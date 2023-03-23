b91df2d30f5773efad773e130d09200e97ce4856
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-24 02:47:15 +0800

    Time: 90 ms (98.95%), Space: 53.9 MB (98.95%) - LeetHub

---
+++ font-stylevertical-align-inherit-font-stylevertical-align-inherit-1319-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-font/font-stylevertical-align-inherit-font-stylevertical-align-inherit-1319-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-font.py
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var makeConnected = function(n, connections) {
+  if (connections.length < n - 1) return -1;
+  
+  const uf = new UnionFind(n);
+  
+  connections.forEach(([a, b]) => uf.union(a, b));
+  
+  return uf.componentSize() - 1;
+};
+
+class UnionFind {
+  constructor(n) {
+    this.parent = Array.from({ length: n }, (_, i) => i);
+    this.count = n;
+  }
+  
+  find(x) {
+    const parent = this.parent;
+    while (x !== parent[x]) {
+      parent[x] = parent[parent[x]];
+      x = parent[x];
+    }
+    
+    return x;
+  }
+  
+  componentSize() {
+    return this.count;
+  }
+  
+  union(x, y) {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
+    if (rootX === rootY) return;
+    
+    this.parent[rootX] = rootY;
+    this.count--;
+  }
+}
