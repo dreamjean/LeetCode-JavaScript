2251ed6cedfe451a9a45bc5ecdf2a8ab3c87a2c7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-24 02:33:46 +0800

    Time: 90 ms (98.95%), Space: 53.9 MB (98.95%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
@@ -18,30 +18,27 @@ class UnionFind {
     this.parent = Array.from({ length: n }, (_, i) => i);
     this.count = n;
   }
-
+  
   find(x) {
     const parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-
+    
     return x;
   }
-
+  
   componentSize() {
     return this.count;
   }
-
+  
   union(x, y) {
     const rootX = this.find(x);
     const rootY = this.find(y);
     if (rootX === rootY) return;
-
-    rootX < rootY
-      ? this.parent[rootX] = rootY
-      : this.parent[rootY] = rootX;
     
+    this.parent[rootX] = rootY;
     this.count--;
   }
 }
