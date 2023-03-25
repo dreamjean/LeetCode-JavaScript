a2b409a8c9877bab6f0d3ab990d723a93700e219
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-03-26 02:20:06 +0800

    Time: 394 ms (90.00%), Space: 87.7 MB (86.67%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.js
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.js
@@ -4,32 +4,32 @@
  * @return {number}
  */
 var countPairs = function(n, edges) {
-    const graph = Array.from({ length: n }, () => []);
-    const visite = new Array(n).fill(false);
-    let ans = n * (n - 1) / 2;
-    
-    for (const [u, v] of edges) {
-        graph[u].push(v);
-        graph[v].push(u);
+  const graph = Array.from({ length: n }, () => []);
+  const visited = new Array(n).fill(false);
+  let ans = n * (n - 1) / 2;
+  
+  edges.forEach(([a, b]) => {
+    graph[a].push(b);
+    graph[b].push(a);
+  })
+  
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      let count = dfs(i, visited, graph);
+      ans -= count * (count - 1) / 2;
     }
-    
-    for (let i = 0; i < n; i++) {
-        if (!visite[i]) {
-            const cnt = dfs(i, visite, graph);
-            ans -= cnt * (cnt - 1) / 2;
-        }
-    }
-    
-    return ans;
+  }
+  
+  return ans;
 };
 
-const dfs = (start, visite, graph) => {
-    visite[start] = true;
-    let res = 1;
-    
-    for (const v of graph[start]) {
-        if (!visite[v]) res += dfs(v, visite, graph);
-    }
-    
-    return res;
+const dfs = (curr, visited, graph) => {
+  visited[curr] = true;
+  let res = 1;
+  
+  graph[curr].forEach((next) => {
+    if (!visited[next]) res += dfs(next, visited, graph);
+  });
+  
+  return res;
 }
