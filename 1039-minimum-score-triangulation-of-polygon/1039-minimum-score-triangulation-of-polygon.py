e084a5ba831b059f6f60b0affdfecde2bc59e033
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-03 01:06:18 +0800

    Time: 93 ms (86.38%), Space: 13.9 MB (63.85%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.py
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.py
@@ -1,19 +1,10 @@
-/**
- * @param {number[]} values
- * @return {number}
- */
-var minScoreTriangulation = function(values) {
-  const n = values.length;
-  const dp = Array.from({ length: n }, () => new Array(n).fill(0));
-  
-  for (let d = 2; d < n; ++d) {
-    for (let i = 0; i + d < n; ++i) {
-      let j = i + d;
-      dp[i][j] = Infinity;
-      for (let k = i + 1; k < j; k++) 
-        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
-    }
-  }
-  
-  return dp[0][n - 1];
-};
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+        for d in range(2, n):
+            for i in range(n - d):
+                j = i + d
+                dp[i][j] = min([dp[i][k] + dp[k][j] + values[i] * values[k] * values[j]
+                               for k in range(i + 1, j)])
+        return dp[0][-1]
