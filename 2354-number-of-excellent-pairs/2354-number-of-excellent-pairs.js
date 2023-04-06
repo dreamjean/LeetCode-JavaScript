b8b1c539dab0b3cd3b5f8a3819308747c99d74b6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-07 01:54:18 +0800

    Time: 173 ms (66.67%), Space: 76.2 MB (33.33%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.js
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.js
@@ -4,28 +4,30 @@
  * @return {number}
  */
 var countExcellentPairs = function(nums, k) {
-    const set = new Set(nums);
-    const cnt = new Array(30).fill(0);
-    let ans = 0;
-    
-    for (let num of set) {
-        const count = bitCount(num);
-        ++cnt[count]
+  const set = new Set(nums);
+  const count = new Array(30).fill(0);
+  let ans = 0;
+  
+  for (let num of set) {
+    const cnt = bitCount(num);
+    ++count[cnt];
+  }
+  
+  for (let i = 1; i < 30; ++i) {
+    for (let j = 1; j < 30; ++j) {
+      if (i + j >= k) ans += count[i] * count[j];
     }
-    
-    for (let i = 1; i < 30 ; i++) 
-        for (let j = 1; j < 30; j++) 
-            if (i + j >= k) ans += cnt[i] * cnt[j];
-    
-    return ans;
+  }
+  
+  return ans;
 };
 
-function bitCount(num) {
-    let cnt = 0
-    while (num) {
-        let tmp = num & (~num + 1)
-        cnt++
-        num = num ^ tmp
-    }
-    return cnt
+const bitCount = (num) => {
+  let count = 0;
+  while (num) {
+    count += (num & 1);
+    num >>= 1;
+  }
+  
+  return count;
 }
