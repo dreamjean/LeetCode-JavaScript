0201c75b0aa9f1a96b924f3579ab9eefaddf595b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-06 18:00:30 +0800

    Time: 78 ms (51.03%), Space: 45.5 MB (36.55%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.js
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.js
@@ -3,26 +3,29 @@
  * @return {number}
  */
 var closedIsland = function(grid) {
-    const [m, n] = [grid.length, grid[0].length];
-    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    let count = 0;
+  const [m, n] = [grid.length, grid[0].length];
+  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+  let count = 0;
+  
+  const dfs = (r, c) => {
+    if (grid[r][c]) return true;
+    if (!r || !c || r >= m - 1 || c >= n - 1) return false;
     
-    const dfs = (r, c) => {
-        if (grid[r][c]) return true;
-        if (!r || r === m - 1 || !c || c === n - 1) return false;
-        
-        grid[r][c] = 1;
-        let res = true;
-        
-        for (const [dx, dy] of dirs)
-            if (!dfs(dx + r, dy + c)) res = false;
-        
-        return res;
+    grid[r][c] = 1;
+    let res = true;
+    
+    for (let [dx, dy] of dirs) {
+      if (!dfs(dx + r, dy + c)) res = false;
     }
     
-    for (let i = 0; i < m; i++) 
-        for (let j = 0; j < n; j++)
-            if (!grid[i][j]) count += +dfs(i, j);
-    
-    return count;
+    return res;
+  }
+  
+  for (let i = 1; i < m - 1; i++) {
+    for (let j = 1; j < n- 1; j++) {
+      if (!grid[i][j] && dfs(i, j)) count++;
+    } 
+  }
+  
+  return count;
 };
