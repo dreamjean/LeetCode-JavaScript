59a4324b4e5495219b9bc62d53eb7643897ab540
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-07 15:29:07 +0800

    Time: 148 ms (23.81%), Space: 61.3 MB (19.84%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.js
+++ 1020-number-of-enclaves/1020-number-of-enclaves.js
@@ -2,36 +2,36 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var numEnclaves = function (grid) {
+var numEnclaves = function(grid) {
   const [m, n] = [grid.length, grid[0].length];
-  const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
+  const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
   let count = 0;
-
-  const dfs = (r, c) => {
-    if (r < 0 || r >= m || c < 0 || c >= n || !grid[r][c]) return;
-
+  
+  const fill = (r, c) => {
+    if (r < 0 || r >= m || c < 0 || c >= n || !grid[r][c]) return 0;
+    
+    let res = 1;
     grid[r][c] = 0;
-
-    for (const [dx, dy] of dirs) dfs(dx + r, dy + c);
-  };
-
+    dirs.forEach(([dx, dy]) => res += fill(dx + r, dy + c));
+    
+    return res;
+  }
+  
   for (let i = 0; i < m; i++) {
-    if (grid[i][0]) dfs(i, 0);
-    if (grid[i][n - 1]) dfs(i, n - 1);
+    fill(i, 0);
+    fill(i, n - 1);
   }
-
+  
   for (let j = 0; j < n; j++) {
-    if (grid[0][j]) dfs(0, j);
-    if (grid[m - 1][j]) dfs(m - 1, j);
+    fill(0, j);
+    fill(m - 1, j);
   }
-
-  for (let i = 0; i < m; i++)
-    for (let j = 0; j < n; j++) if (grid[i][j]) count++;
-
+  
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j]) count += fill(i, j);
+    }
+  }
+  
   return count;
 };
