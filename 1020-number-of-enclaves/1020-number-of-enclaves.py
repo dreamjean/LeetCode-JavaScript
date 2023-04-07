8afb2c4a0775584d5befc6c0b93dba42f5014e2c
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-07 15:35:29 +0800

    Time: 643 ms (80.28%), Space: 100.5 MB (11.58%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def fill(r, c):
+            if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] == 0:
+                return
+            grid[r][c] = 0
+            for (x, y) in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                fill(x, y)
+                
+                
+        m, n, count = len(grid), len(grid[0]), 0
+        for i in range(m):
+            fill(i, 0)
+            fill(i, n - 1)
+        for j in range(n):
+            fill(0, j)
+            fill(m - 1, j)
+        return sum(grid[i][j] for i in range(m) for j in range(n))
