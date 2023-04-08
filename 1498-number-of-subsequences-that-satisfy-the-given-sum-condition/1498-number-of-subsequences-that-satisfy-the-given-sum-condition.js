0ed568eeb0ecf97af949797cc90af7900cf5fef0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-09 04:44:39 +0800

    Time: 200 ms (88.89%), Space: 54.2 MB (82.22%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var numSubseq = function(nums, target) {
+  nums.sort((a, b) => a - b);
+  const [n, mod] = [nums.length, 1e9 + 7];
+  const pows = new Array(n).fill(0);
+  let [l, r, ans] = [0, n - 1, 0];
+  pows[0] = 1;
+  
+  for (let i = 1; i < n; i++) 
+    pows[i] = (pows[i - 1] * 2) % mod;
+  
+  while (l <= r) {
+    if (nums[l] + nums[r] > target) r--;
+    else ans = (ans + pows[r - l++]) % mod;
+  }
+  
+  return ans;
+};
