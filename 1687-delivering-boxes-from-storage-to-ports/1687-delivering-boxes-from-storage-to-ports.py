95f07418c1edc7ca425c548d6880549fc5ca74f5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-09 17:14:02 +0800

    Time: 2258 ms (60.66%), Space: 63.4 MB (86.89%) - LeetHub

--- 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
+++ 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
@@ -2,23 +2,20 @@ class Solution:
     def boxDelivering(self, boxes: List[List[int]], portsCount: int, maxBoxes: int, maxWeight: int) -> int:
         n = len(boxes)
         dp = [0] + [float('inf')] * n
-        need = lastJ = j = 0
+        need = last_j = j = 0
         for i in range(n):
             while j < n and maxBoxes > 0 and maxWeight >= boxes[j][1]:
                 maxBoxes -= 1
                 maxWeight -= boxes[j][1]
-                if j == 0 or boxes[j][0] != boxes[j - 1][0]:
-                    lastJ = j
+                if j == 0 or boxes[j - 1][0] != boxes[j][0]:
+                    last_j = j
                     need += 1
                 j += 1
-            
             dp[j] = min(dp[j], dp[i] + need + 1)
-            dp[lastJ] = min(dp[lastJ], dp[i] + need)
+            dp[last_j] = min(dp[last_j], dp[i] + need)
+            
             maxBoxes += 1
             maxWeight += boxes[i][1]
-            
             if i == n - 1 or boxes[i][0] != boxes[i + 1][0]:
                 need -= 1
-        
         return dp[-1]
-            
