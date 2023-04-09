3b0899875929aa899162f02a06607036324ea6fb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-10 07:24:50 +0800

    Time: 95 ms (73.33%), Space: 45.2 MB (46.67%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.js
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.js
@@ -3,22 +3,19 @@
  * @return {number}
  */
 var balancedString = function(s) {
-  const map = { 'Q': 0, 'W': 0, 'E': 0, 'R': 0 };
+  const count = { 'Q': 0, 'E': 0, 'W': 0, 'R': 0 };
   const [n, k] = [s.length, s.length / 4];
-  let [l, r, ans] = [0, 0, n];
+  let [ans, j] = [n, 0];
   
-  for (let ch of s)
-    ++map[ch];
+  for (let ch of s) count[ch]++;
   
-  while (r < n) {
-    --map[s[r]];
-    while (l < n && map['Q'] <= k && map['W'] <= k && map['E'] <= k && map['R'] <= k) {
-      ans = Math.min(ans, r - l + 1);
-      ++map[s[l++]];
+  for (let i = 0; i < n; i++) {
+    count[s[i]]--;
+    while (j < n && count['Q'] <= k && count['E'] <= k && count['W'] <= k && count['R'] <= k) {
+      ans = Math.min(ans, i - j + 1);
+      count[s[j++]]++;
     }
-    
-    r++;
-  } 
+  }
   
   return ans;
 };
