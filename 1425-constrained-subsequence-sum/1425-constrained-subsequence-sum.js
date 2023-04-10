ae3101d3b735078e67a5b9a236029ea7e9add068
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-11 06:06:12 +0800

    Time: 121 ms (77.78%), Space: 55.6 MB (77.78%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.js
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.js
@@ -10,10 +10,7 @@ var constrainedSubsetSum = function(nums, k) {
   for (let i = 0; i < nums.length; i++) {
     nums[i] += head <= tail ? nums[stack[head]] : 0;
     ans = Math.max(ans, nums[i]);
-    while (head <= tail && nums[stack[tail]] < nums[i]) {
-      stack.pop();
-      tail--;
-    }
+    while (head <= tail && nums[stack[tail]] < nums[i]) tail--;
     
     if (nums[i] > 0) stack[++tail] = i;
     if (head <= tail && i - stack[head] >= k) head++;
