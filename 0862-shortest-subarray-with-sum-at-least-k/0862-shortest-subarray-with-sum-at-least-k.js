16a97f7ba7ff2b07eafe6762d0fc8afcbc1d02ac
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-11 05:38:20 +0800

    Time: 128 ms (96.15%), Space: 58 MB (46.15%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.js
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.js
@@ -3,27 +3,26 @@
  * @param {number} k
  * @return {number}
  */
-var shortestSubarray = function (nums, k) {
+var shortestSubarray = function(nums, k) {
   const n = nums.length;
-  const preSum = new Array(n + 1).fill(0);
   const stack = [];
   let [head, tail, ans] = [0, 0, n + 1];
-
-  nums.forEach((num, i) => (preSum[i + 1] += preSum[i] + num));
-
-  preSum.forEach((sum, i) => {
-    const size = stack.length;
-    while (head < size && sum - preSum[stack[head]] >= k)
+  
+  for (let i = 0; i < n; i++) {
+    if (i) nums[i] += nums[i - 1];
+    if (nums[i] >= k) ans = Math.min(ans, i + 1);
+    
+    while (head < tail && nums[i] - nums[stack[head]] >= k)
       ans = Math.min(ans, i - stack[head++]);
-
-    while (size && sum <= preSum[stack[tail - 1]]) {
+    
+    while (tail && nums[i] <= nums[stack[tail - 1]]) {
       stack.pop();
       tail--;
     }
-
+    
     stack.push(i);
     tail++;
-  });
-
+  }
+  
   return ans <= n ? ans : -1;
 };
