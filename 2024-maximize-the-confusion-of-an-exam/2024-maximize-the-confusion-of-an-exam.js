d7c297af029596c7721e15fbe2b1e5a3add75bf7
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-12 06:36:03 +0800

    Time: 77 ms (92.68%), Space: 45.2 MB (63.41%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.js
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var maxConsecutiveAnswers = function(answerKey, k) {
-  let [l, r, cntT, cntF, max] = new Array(5).fill(0);
+  let [l, r, ans, cntF, cntT] = [0, 0, 0, 0, 0];
   
   while (r < answerKey.length) {
-    if (answerKey[r] === 'T') cntT++;
-    if (answerKey[r] === 'F') cntF++;
-    while (cntT > k && cntF > k) {
-      if (answerKey[l] === 'T') cntT--;
-      if (answerKey[l] === 'F') cntF--;
-      l++;
-    }
+    answerKey[r] === 'T' ? cntT++ : cntF++;
+    while (cntF > k && cntT > k) 
+      answerKey[l++] === 'T' ? cntT-- : cntF--;
     
-    max = Math.max(max, r - l + 1);
+    ans = Math.max(ans, r - l + 1);
     r++;
   }
   
-  return max;
+  return ans;
 };
+  
