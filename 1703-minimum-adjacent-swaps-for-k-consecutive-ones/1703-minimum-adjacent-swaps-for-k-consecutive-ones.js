bda41f151cd6fb771ac3d4cff5c7dd2672cd70f8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-13 04:26:35 +0800

    Time: 103 ms (75.00%), Space: 58.3 MB (75.00%) - LeetHub

--- 1703-minimum-adjacent-swaps-for-k-consecutive-ones/1703-minimum-adjacent-swaps-for-k-consecutive-ones.js
+++ 1703-minimum-adjacent-swaps-for-k-consecutive-ones/1703-minimum-adjacent-swaps-for-k-consecutive-ones.js
@@ -4,15 +4,18 @@
  * @return {number}
  */
 var minMoves = function(nums, k) {
-  const preSum = [0];
-  const [mid1, mid2] = [Math.ceil(k / 2), Math.floor(k / 2)];
-  let ans = Infinity;
+  const pos = nums.reduce((acc, num, i) => (num && acc.push(i), acc), []);
+  let [mid, count] = [Math.floor(k / 2), 0];
   
-  for (let i = 0; i < nums.length; i++) 
-    if (nums[i]) preSum.push(preSum.at(-1) + i);  
-
-  for (let i = 0; i < preSum.length - k; i++) 
-    ans = Math.min(ans, preSum[i + k] - preSum[i + mid1] - (preSum[i + mid2] - preSum[i]));
+  for (let i = 1; i < k; i++)
+    count += (pos[i] - pos[i - 1] - 1) * Math.min(i, k - i)
   
-  return ans - mid1 * mid2;
+  let ans = count;
+  
+  for (let i = k; i < pos.length; i++) {
+    count += pos[i] - pos[i - mid] - (pos[i - k + mid] - pos[i - k])
+    ans = Math.min(ans, count);
+  }
+  
+  return ans;
 };
