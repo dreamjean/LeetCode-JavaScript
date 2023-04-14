740ede9b197be78f02b988e779d3b57e70e6d13d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-15 07:07:31 +0800

    Time: 1196 ms (96.91%), Space: 60.4 MB (73.20%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
+        dq = deque()
+        ans = -inf
+        for x, y in points:
+            while dq and dq[0][1] < x - k:
+                dq.popleft()
+            if dq:
+                ans = max(ans, dq[0][0] + x + y)
+            while dq and dq[-1][0] <= y - x:
+                dq.pop()
+            dq.append([y - x, x])
+        return ans
