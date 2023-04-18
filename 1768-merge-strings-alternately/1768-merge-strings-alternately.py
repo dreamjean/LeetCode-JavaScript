a3bb3977e44513079191b8fc665a223d67087357
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-18 17:21:36 +0800

    Time: 47 ms (97.21%), Space: 42.6 MB (33.53%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,3 +1,14 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+  const [n1, n2] = [word1.length, word2.length];
+  let ans = '';
+  
+  for (let i = 0; i < Math.max(n1, n2); i++) 
+    ans += (word1[i] ?? '') + (word2[i] ?? '');
+  
+  return ans;
+};
