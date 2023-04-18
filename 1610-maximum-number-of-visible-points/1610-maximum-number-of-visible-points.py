6b77a63addd4936a3ed096c8fb78ca3ed0ddfa92
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-19 04:46:37 +0800

    Time: 2612 ms (17.49%), Space: 47.5 MB (72.62%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -2,20 +2,17 @@ class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         angle = angle * math.pi / 180
         x0, y0 = location
-        arr = []
-        j, extra, ans = 0, 0, 0
-        
+        angles = []
+        j = extra = count = 0
         for x, y in points:
             if x == x0 and y == y0:
                 extra += 1
-                continue
-            arr.append(math.atan2(y - y0, x - x0))
-        
-        arr.sort()
-        arr += [x + math.pi * 2 for x in arr]
-        
-        for i, x in enumerate(arr):
-            while x - arr[j] > angle:
+            else:
+                angles.append(math.atan2(y - y0, x - x0))
+        angles.sort()
+        angles += [x + math.pi * 2 for x in angles]
+        for i, x in enumerate(angles):
+            while x - angles[j] > angle:
                 j += 1
-            ans = max(ans, i - j + 1)
-        return ans + extra
+            count = max(count, i - j + 1)
+        return count + extra
