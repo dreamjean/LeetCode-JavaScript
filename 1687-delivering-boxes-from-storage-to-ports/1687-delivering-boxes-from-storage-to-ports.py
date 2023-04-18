049ca81db5c8544326b4b27d711c2a10d5b3c361
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-18 17:52:54 +0800

    Time: 2277 ms (48.15%), Space: 63.6 MB (53.70%) - LeetHub

--- 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
+++ 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
@@ -1,21 +1,24 @@
 class Solution:
     def boxDelivering(self, boxes: List[List[int]], portsCount: int, maxBoxes: int, maxWeight: int) -> int:
         n = len(boxes)
-        dp = [0] + [float('inf')] * n
-        need = last_j = j = 0
-        for i in range(n):
-            while j < n and maxBoxes > 0 and maxWeight >= boxes[j][1]:
+        memo = [0] + [inf] * n
+        j = lastJ = need = 0
+        for i, [ports, weight] in enumerate(boxes):
+            while j < n and maxBoxes > 0 and boxes[j][1] <= maxWeight:                
                 maxBoxes -= 1
                 maxWeight -= boxes[j][1]
-                if j == 0 or boxes[j - 1][0] != boxes[j][0]:
-                    last_j = j
+                if j == 0 or boxes[j][0] != boxes[j - 1][0]:
+                    lastJ = j
                     need += 1
                 j += 1
-            dp[j] = min(dp[j], dp[i] + need + 1)
-            dp[last_j] = min(dp[last_j], dp[i] + need)
+            
+            memo[j] = min(memo[j], memo[i] + need + 1)
+            memo[lastJ] = min(memo[lastJ], memo[i] + need)
             
             maxBoxes += 1
-            maxWeight += boxes[i][1]
-            if i == n - 1 or boxes[i][0] != boxes[i + 1][0]:
+            maxWeight += weight
+            
+            if i == n - 1 or ports != boxes[i + 1][0]:
                 need -= 1
-        return dp[-1]
+        
+        return memo[-1]
