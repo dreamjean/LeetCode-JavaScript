b4762e10b1a8c8b68469268449ec15efcd8382b5
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-19 05:49:14 +0800

    Time: 1257 ms (46.74%), Space: 60.3 MB (70.65%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -3,11 +3,11 @@ class Solution:
         dq = deque()
         ans = -inf
         for x, y in points:
-            while dq and dq[0][1] < x - k:
+            while dq and x - dq[0][1] > k:
                 dq.popleft()
             if dq:
                 ans = max(ans, dq[0][0] + x + y)
             while dq and dq[-1][0] <= y - x:
                 dq.pop()
-            dq.append([y - x, x])
+            dq.append((y - x, x))
         return ans
