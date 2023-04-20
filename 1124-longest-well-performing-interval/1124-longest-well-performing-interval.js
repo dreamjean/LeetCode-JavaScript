cd6b523130f778ddd85fae6ce85fb7f68393a0f0
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-20 08:51:45 +0800

    Time: 77 ms (70.00%), Space: 45.6 MB (25.00%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.js
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.js
@@ -6,12 +6,12 @@ var longestWPI = function(hours) {
   const map = {};
   let [ans, source] = [0, 0];
   
-  hours.forEach((hour, i) => {
-    source += hour > 8 ? 1 : -1;
-    if (!(source in map)) map[source] = i;
+  for (let i = 0; i < hours.length; i++) {
+    source += hours[i] > 8 ? 1 : -1;
     if (source > 0) ans = i + 1;
-    if ((source - 1) in map) ans = Math.max(ans, i - map[source - 1]);
-  })
+    if (!(source in map)) map[source] = i;
+    if (source - 1 in map) ans = Math.max(ans, i - map[source - 1]);
+  }
   
   return ans;
 };
