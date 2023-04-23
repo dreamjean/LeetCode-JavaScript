feb655dcecb27c6e19c708c8b737243189c9da04
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-23 11:07:00 +0800

    Time: 705 ms (94.40%), Space: 28 MB (91.40%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -1,25 +1,15 @@
-/**
- * @param {number[]} nums
- * @param {number} indexDiff
- * @param {number} valueDiff
- * @return {boolean}
- */
-var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
-  const bucket = {};
-  
-  for (let [i, num] of nums.entries()) {
-    const idx = Math.floor(num / (valueDiff + 1));
-    if (idx in bucket) return true;
-    if ((idx - 1 in bucket) && Math.abs(num - bucket[idx - 1]) <= valueDiff) return true;
-    if ((idx + 1 in bucket) && Math.abs(num - bucket[idx + 1]) <= valueDiff) return true;
-    
-    bucket[idx] = num;
-    if (i >= indexDiff) {
-      const oldNum = nums[i - indexDiff];
-      const oldIdx = Math.floor(oldNum / (valueDiff + 1));
-      delete bucket[oldIdx];
-    }
-  }
-  
-  return false;
-};
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        bucket = {}
+        for i, num in enumerate(nums):
+            idx = num // (valueDiff + 1)
+            if idx in bucket:
+                return True
+            if idx - 1 in bucket and abs(num - bucket[idx - 1]) <= valueDiff:
+                return True
+            if idx + 1 in bucket and abs(num - bucket[idx + 1]) <= valueDiff:
+                return True
+            bucket[idx] = num
+            if i >= indexDiff:
+                del bucket[nums[i - indexDiff] // (valueDiff + 1)]
+        return False
