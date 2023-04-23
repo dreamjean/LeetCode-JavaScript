b74c0298e4c9453ff4e681f22ebd9f05bb7fff3d
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-24 03:56:58 +0800

    Time: 110 ms (75.00%), Space: 47.5 MB (50.00%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.js
+++ 1416-restore-the-array/1416-restore-the-array.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 var numberOfArrays = function(s, k) {
-  const dp = new Array(s.length).fill(-1);
+  const n = s.length;
+  const mod = 1e9 + 7;
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
   
-  return dfs(0, s, k, dp);
-};
-
-const dfs = (i, s, k, dp) => {
-  if (i === s.length) return 1;
-  if (s[i] === '0') return 0;
-  if (dp[i] !== -1) return dp[i];
-  
-  let [ans, num] = [0, 0];
-  for (let j = i; j < s.length; j++) {
-    num = num * 10 + +s[j];
-    if (num > k) break;
-    ans = (ans + dfs(j + 1, s, k, dp)) % (10 ** 9 + 7);
+  for (let i = 0; i < n; i++) {
+    if (s[i] === '0') continue;
+    
+    let num = 0;
+    for (let j = i; j < n; j++) {
+      num = num * 10 + +s[j];
+      if (num > k) break;
+      
+      dp[j + 1] = (dp[j + 1] + dp[i]) % mod;
+    }
   }
   
-  return dp[i] = ans;
-}
+  return dp[n];
+};
