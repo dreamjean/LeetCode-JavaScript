0f054952928b2d57f2c7e7acdc0bd0287c50b618
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-24 10:55:46 +0800

    Time: 402 ms (19.56%), Space: 76.4 MB (11.41%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+  const n = nums.length;
+  if (n < 2) return 0;
+  
+  const [min, max] = [Math.min(...nums), Math.max(...nums)];
+  const d = Math.max(1, Math.floor((max - min) / (n - 1)));
+  const bucketSize = Math.floor((max - min) / d) + 1;
+  const bucket = Array.from({ length: bucketSize }, () => [-1, -1]);
+  let [ans, pos] = [0, -1];
+  
+  for (let i = 0; i < n; i++) {
+    const idx = Math.floor((nums[i] - min) / d);
+    if (bucket[idx][0] === -1) bucket[idx] = [nums[i], nums[i]];
+    else {
+      bucket[idx][0] = Math.min(bucket[idx][0], nums[i]);
+      bucket[idx][1] = Math.max(bucket[idx][1], nums[i]);
+    }
+  }
+  
+  for (let i = 0; i < bucketSize; i++) {
+    if (bucket[i][0] === -1) continue;
+    
+    if (pos !== -1) ans = Math.max(ans, bucket[i][0] - bucket[pos][1]);
+    pos = i;
+  }
+  
+  return ans;
+};
