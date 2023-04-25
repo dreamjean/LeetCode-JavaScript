401dd9907446636a9ec4b50dad34e3d786216c5b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-26 03:31:28 +0800

    Time: 51 ms (78.05%), Space: 14.1 MB (48.43%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
         def maxSum(nums, L, M):
-            sumL = sumM = 0
+            sum1, sum2 = 0, 0
             for i in range(L + M):
                 if i < L:
-                    sumL += nums[i]
+                    sum1 += nums[i]
                 else:
-                    sumM += nums[i]
-                    
-            maxL, ans = sumL, sumL + sumM
-            for i in range(L + M, len(nums)):
-                sumL += nums[i - M] - nums[i - L - M]
-                maxL = max(maxL, sumL)
-                sumM += nums[i] - nums[i - M]
-                ans = max(ans, maxL + sumM)
+                    sum2 += nums[i]
+            max_sum1, ans = sum1, sum1 + sum2
+            for i in range(L+M, len(nums)):
+                sum1 += nums[i - M] - nums[i - L - M]
+                sum2 += nums[i] - nums[i - M]
+                max_sum1 = max(max_sum1, sum1)
+                ans = max(ans, max_sum1 + sum2)
             return ans
-                    
-          
+        
+        
         return max(maxSum(nums, firstLen, secondLen), maxSum(nums, secondLen, firstLen))
