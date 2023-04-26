82e295aafcca789d3f6823557849cbad7396f435
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-27 05:50:24 +0800

    Time: 54 ms (6.23%), Space: 16.6 MB (5.02%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        
+        def dfs(l, r, path = ''):
+            if l == 0 and r == 0:
+                ans.append(path)
+                return
+            if l > 0:
+                dfs(l - 1, r, path + '(')
+            if r > l:
+                dfs(l, r - 1, path + ')')
+                
+          
+        dfs(n, n)
+        return ans
