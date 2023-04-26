bf9eb202cec81fd7d71030153d1007f761cfbb20
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-27 05:32:06 +0800

    Time: 81 ms (77.10%), Space: 45.4 MB (50.16%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -8,18 +8,18 @@ var isMatch = function(s, p) {
   let dp = new Array(n + 1).fill(false);
   dp[0] = true;
   
-  for (let j = 2; j <= n; j++) 
-    dp[j] = dp[j - 2] && (p[j - 1] === '*');
+  for (let j = 2; j <= n; j++)
+    dp[j] = (p[j - 1] === '*') && dp[j - 2];
   
   for (let i = 1; i <= m; i++) {
     const tmp = new Array(n + 1).fill(false);
     
     for (let j = 1; j <= n; j++) {
-      if (s[i - 1] === p[j - 1] || p[j - 1] === '.') 
+      if (s[i - 1] === p[j - 1] || p[j - 1] === '.')
         tmp[j] = dp[j - 1];
       
       else if (p[j - 1] === '*') {
-        if (s[i - 1] !== p[j - 2] && p[j - 2] !== '.') 
+        if (s[i - 1] !== p[j - 2] && p[j - 2] !== '.')
           tmp[j] = tmp[j - 2];
         
         else tmp[j] = tmp[j - 2] || dp[j];
