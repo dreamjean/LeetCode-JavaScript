857fa875b44ae7249ee866c2dc8ffa7e4433542b
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-28 06:05:12 +0800

    Time: 212 ms (88.05%), Space: 62.3 MB (8.18%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.js
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.js
@@ -6,18 +6,20 @@
  */
 var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
   const bucket = {};
+  const w = valueDiff + 1;
   
   for (let [i, num] of nums.entries()) {
-    const idx = Math.floor(num / (valueDiff + 1));
+    const idx = Math.floor(num / w);
     if (idx in bucket) return true;
-    if ((idx - 1 in bucket) && Math.abs(num - bucket[idx - 1]) <= valueDiff) return true;
-    if ((idx + 1 in bucket) && Math.abs(num - bucket[idx + 1]) <= valueDiff) return true;
+    if (idx - 1 in bucket && Math.abs(num - bucket[idx - 1]) < w)
+        return true
+    if (idx + 1 in bucket && Math.abs(num - bucket[idx + 1]) < w)
+        return true
     
     bucket[idx] = num;
     if (i >= indexDiff) {
       const oldNum = nums[i - indexDiff];
-      const oldIdx = Math.floor(oldNum / (valueDiff + 1));
-      delete bucket[oldIdx];
+      delete bucket[Math.floor(oldNum / w)];
     }
   }
   
