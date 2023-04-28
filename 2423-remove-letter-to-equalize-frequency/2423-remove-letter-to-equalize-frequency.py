b7dec3a78071ee87b2f6b0a53e0d49c1ef7e2b8e
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-29 07:38:59 +0800

    Time: 38 ms (20.17%), Space: 16.2 MB (9.49%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -1,23 +1,11 @@
-/**
- * @param {string} word
- * @return {boolean}
- */
-var equalFrequency = function(word) {
-  let count = new Array(26).fill(0);
-  
-  for (let ch of word) count[ch.charCodeAt() - 'a'.charCodeAt()]++;
-  
-  for (let i = 0; i < count.length; i++) {
-    if (!count[i]) continue;
-    
-    count[i]--;
-    
-    const set = new Set(count);
-    if (set.size === 1 || (set.size === 2 && set.has(0))) return true;
-    
-    count[i]++;
-  }
-  
-  return false;
-};
-
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        count = Counter(word)
+        for ch in word:
+            count[ch] -= 1
+            if count[ch] == 0:
+                count.pop(ch)
+            if len(set(count.values())) == 1:
+                return True
+            count[ch] += 1
+        return False
