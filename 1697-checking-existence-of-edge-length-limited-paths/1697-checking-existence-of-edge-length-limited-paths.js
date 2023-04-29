37b668584839d32b27af08913492072af338ebc4
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-29 20:29:16 +0800

    Time: 404 ms (100.00%), Space: 93.4 MB (50.00%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.js
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.js
@@ -6,18 +6,18 @@
  */
 var distanceLimitedPathsExist = function(n, edgeList, queries) {
   edgeList.sort((a, b) => a[2] - b[2]);
-  queries = queries.map((query, i) => [...query, i]).sort((a, b) => a[2] - b[2]);
+  queries = queries.map((query, i) => [i, ...query]).sort((a, b) => a[3] - b[3]);
   const uf = new UnionFind(n);
   const ans = new Array(queries.length);
   let j = 0;
   
-  for (let [p, q, limit, i] of queries) {
+  for (let [i, p, q, limit] of queries) {
     while (j < edgeList.length && edgeList[j][2] < limit) {
       const [u, v] = edgeList[j++];
       uf.union(u, v);
     }
     
-    ans[i] = uf.connected(p, q);
+    ans[i] = uf.isConnected(p, q);
   }
   
   return ans;
@@ -27,26 +27,26 @@ class UnionFind {
   constructor(n) {
     this.parent = Array.from({ length: n }, (_, i) => i);
   }
-
+  
   find(x) {
-    const parent = this.parent;
+    let parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-
+    
     return x;
   }
-
-  connected(x, y) {
+  
+  isConnected(x, y) {
     return this.find(x) === this.find(y);
   }
   
   union(x, y) {
-    const rootX = this.find(x);
-    const rootY = this.find(y);
+    let rootX = this.find(x);
+    let rootY = this.find(y);
     if (rootX === rootY) return;
-   
-    rootX < rootY ? this.parent[rootX] = rootY : this.parent[rootY] = rootX;
+    
+    this.parent[rootX] = rootY;
   }
 }
