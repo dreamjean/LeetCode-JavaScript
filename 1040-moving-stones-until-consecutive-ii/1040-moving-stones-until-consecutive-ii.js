1c4970320d201a4cb05f5768690e52ac172b3853
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-04-30 05:55:16 +0800

    Time: 82 ms (33.33%), Space: 47.4 MB (33.33%) - LeetHub

--- 1040-moving-stones-until-consecutive-ii/1040-moving-stones-until-consecutive-ii.js
+++ 1040-moving-stones-until-consecutive-ii/1040-moving-stones-until-consecutive-ii.js
@@ -5,15 +5,16 @@
 var numMovesStonesII = function(stones) {
   stones.sort((a, b) => a - b);
   const n = stones.length;
-  const high = Math.max(stones[n - 1] - stones[1] - n + 2, stones[n - 2] - stones[0] - n + 2);
-  let [i, low] = [0, n];
+  let [j, low] = [0, n];
+  let high = Math.max(stones[n - 1] - stones[1], stones[n - 2] - stones[0]) - n + 2;
   
-  for (let j = 0; j < n; ++j) {
-    while (stones[j] - stones[i] >= n) ++i;
+  for (let [i, stone] of stones.entries()) {
+    while (stone - stones[j] >= n) j++;
     
-    if (j - i + 1 === n - 1 && stones[j] - stones[i] === n - 2)
+    if (i - j === n - 2 && stone - stones[j] === n - 2)
       low = Math.min(low, 2);
-    else low = Math.min(low, n - (j - i + 1));
+    
+    else low = Math.min(low, n - (i - j + 1));
   }
   
   return [low, high];
