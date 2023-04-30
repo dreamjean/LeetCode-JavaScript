579bafbc46caa134436f3395b6ed6e40f18591a2
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-01 04:52:13 +0800

    Time: 258 ms (88.89%), Space: 81.9 MB (44.44%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.js
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.js
@@ -5,17 +5,17 @@
  */
 var maxNumEdgesToRemove = function(n, edges) {
   edges.sort((a, b) => b[0] - a[0]);
-  const ufa = new UnionFind(n + 1);
-  const ufb = new UnionFind(n + 1);
+  const ufAlice = new UnionFind(n + 1);
+  const ufBob = new UnionFind(n + 1);
   let count = 0;
   
   for (let [type, u, v] of edges) {
-    if (type === 3) count += (ufa.union(u, v) | ufb.union(u, v));
-    else if (type === 2) count += ufb.union(u, v);
-    else count += ufa.union(u, v);
+    if (type === 3) count += ufAlice.union(u, v) | ufBob.union(u, v);
+    else if (type === 2) count += ufBob.union(u, v);
+    else count += ufAlice.union(u, v);
   }
   
-  return ufa.allUnited() && ufb.allUnited() ? edges.length - count : -1;
+  return ufAlice.united() && ufBob.united() ? edges.length - count : -1;
 };
 
 class UnionFind {
@@ -23,29 +23,29 @@ class UnionFind {
     this.parent = Array.from({ length: n }, (_, i) => i);
     this.count = n;
   }
-
+  
   find(x) {
-    const parent = this.parent;
+    let parent = this.parent;
     while (x !== parent[x]) {
       parent[x] = parent[parent[x]];
       x = parent[x];
     }
-
+    
     return x;
   }
-
-  allUnited() {
+  
+  united() {
     return this.count === 2;
   }
-
+  
   union(x, y) {
-    const rootX = this.find(x);
-    const rootY = this.find(y);
+    let rootX = this.find(x);
+    let rootY = this.find(y);
     if (rootX === rootY) return false;
-
+    
     this.parent[rootX] = rootY;
     this.count--;
-
+    
     return true;
   }
 }
