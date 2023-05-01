9b52ef0555c9d689401d70ed784a2362ae5ecf75
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-01 09:13:55 +0800

    Time: 63 ms (25.17%), Space: 41.9 MB (50.94%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -4,13 +4,9 @@
  */
 var average = function(salary) {
   const n = salary.length;
-  let [min, max, sum] = [Infinity, 0, 0];
   
-  for (let num of salary) {
-    min = Math.min(min, num);
-    max = Math.max(max, num);
-    sum += num;
-  }
-  
-  return (sum - min - max) / (n - 2);
+  return salary
+    .sort((a, b) => a - b)
+    .slice(1, -1)
+    .reduce((a, b) => a + b) / (n - 2)
 };
