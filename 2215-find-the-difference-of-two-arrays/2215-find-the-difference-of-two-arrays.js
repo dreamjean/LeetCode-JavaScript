c756b59c3900b1ff9a7b2d40e8eb7988a6cbbfeb
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-03 10:25:38 +0800

    Time: 91 ms (75.38%), Space: 48.8 MB (51.38%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -6,11 +6,9 @@
 var findDifference = function(nums1, nums2) {
   const set1 = new Set(nums1);
   const set2 = new Set(nums2);
-  const ans = [[], []];
   
-  set1.forEach((num) => !set2.has(num) && ans[0].push(num));
+  const a1 = [...set1].filter((num) => !set2.has(num));
+  const a2 = [...set2].filter((num) => !set1.has(num));
   
-  set2.forEach((num) => !set1.has(num) && ans[1].push(num));
-  
-  return ans;
+  return [a1, a2];
 };
