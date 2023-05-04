9dd7c15695451341e44114160711de1951507098
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-04 12:54:56 +0800

    Time: 202 ms (62.50%), Space: 80.7 MB (62.50%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.js
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.js
@@ -5,19 +5,16 @@
  * @return {number}
  */
 var maxTotalFruits = function(fruits, startPos, k) {
+  const n = fruits.length;
   let [j, sum, ans] = [0, 0, 0];
   
-  fruits.forEach(([pos, amount], i) => {
+  for (let [i, [pos, amount]] of fruits.entries()) {
     sum += amount;
-    while (
-      j <= i && Math.min(
-        Math.abs(startPos - pos), Math.abs(startPos - fruits[j][0])
-      ) + pos - fruits[j][0] > k
-    )
+    while (j <= i && pos - fruits[j][0] + Math.min(Math.abs(pos - startPos), Math.abs(startPos - fruits[j][0])) > k)
       sum -= fruits[j++][1];
     
     ans = Math.max(ans, sum);
-  })
+  }
   
   return ans;
 };
