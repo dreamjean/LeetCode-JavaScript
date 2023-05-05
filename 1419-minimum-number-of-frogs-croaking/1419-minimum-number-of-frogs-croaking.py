e39fbd317ee6dbc2cf48e98f40c3a0d8b4b881e6
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-06 06:27:52 +0800

    Time: 167 ms (78.28%), Space: 17.1 MB (12.37%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.py
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.py
@@ -1,20 +1,19 @@
-/**
- * @param {string} croakOfFrogs
- * @return {number}
- */
-var minNumberOfFrogs = function(croakOfFrogs) {
-  const corakPos = { 'c': 0, 'r': 1, 'o': 2, 'a': 3, 'k': 4 };
-  const count = new Array(5).fill(0);
-  let [frogs, maxFrogs] = [0, 0];
-  
-  for (let ch of croakOfFrogs) {
-    const pos = corakPos[ch];
-    count[pos]++;
-  
-    if (!pos) maxFrogs = Math.max(maxFrogs, ++frogs);
-    else if (--count[pos - 1] < 0) return -1;
-    else if (pos === 4) frogs--;
-  }
-  
-  return !frogs ? maxFrogs : -1;
-};
+class Solution:
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        count = [0] * 5
+        croak_pos = {'c': 0, 'r': 1, 'o': 2, 'a': 3, 'k': 4}
+        frogs = max_frogs = 0
+        for ch in croakOfFrogs:
+            pos = croak_pos[ch]
+            count[pos] += 1
+            if pos == 0:
+                frogs += 1
+                max_frogs = max(max_frogs, frogs)
+            else:
+                count[pos - 1] -= 1
+                if count[pos - 1] < 0:
+                    return -1                
+                if pos == 4:
+                    frogs -= 1
+        return max_frogs if frogs == 0 else -1
+      
