32434501701290b058709b3d1a6f714a2a3a12b8
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-07 06:31:01 +0800

    Time: 468 ms (47.79%), Space: 52 MB (67.26%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.js
+++ 0218-the-skyline-problem/0218-the-skyline-problem.js
@@ -2,28 +2,26 @@
  * @param {number[][]} buildings
  * @return {number[][]}
  */
-var getSkyline = function (buildings) {
-  const heights = [];
-  const maxHeap = [0];
+var getSkyline = function(buildings) {
+  const heights = [0];
+  const lines = [];
   const ans = [];
   let maxHeight = 0;
-
-  for (let [left, right, height] of buildings) {
-    heights.push([left, 0 - height], [right, height]);
-  }
-
-  heights.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
-
-  for (let [pos, height] of heights) {
-    if (height < 0) maxHeap.push(0 - height);
-    else maxHeap.splice(maxHeap.indexOf(height), 1);
-
-    const currMaxHeight = Math.max(...maxHeap);
+  
+  buildings.forEach(([left, right, height]) => lines.push([left, -height], [right, height]));
+  
+  lines.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+  
+  for (let [pos, height] of lines) {
+    if (height < 0) heights.push(-height);
+    else heights.splice(heights.indexOf(height), 1);
+    
+    let currMaxHeight = Math.max(...heights);
     if (currMaxHeight !== maxHeight) {
       ans.push([pos, currMaxHeight]);
       maxHeight = currMaxHeight;
     }
   }
-
+  
   return ans;
 };
