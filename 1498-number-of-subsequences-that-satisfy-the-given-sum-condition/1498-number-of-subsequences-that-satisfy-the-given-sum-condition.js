cb15bba87a44fa172005919df8026cc5f1151191
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-06 09:10:38 +0800

    Time: 217 ms (34.09%), Space: 53.4 MB (86.36%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -5,16 +5,15 @@
  */
 var numSubseq = function(nums, target) {
   nums.sort((a, b) => a - b);
-  const [n, mod] = [nums.length, 1e9 + 7];
+  const [n, mod] = [nums.length, 10 ** 9 + 7];
   const pows = new Array(n).fill(0);
   let [l, r, ans] = [0, n - 1, 0];
   pows[0] = 1;
   
-  for (let i = 1; i < n; i++) 
-    pows[i] = (pows[i - 1] * 2) % mod;
+  for (let i = 1; i < n; i++) pows[i] = (pows[i - 1] * 2) % mod;
   
   while (l <= r) {
-    if (nums[l] + nums[r] > target) r--;
+    if (nums[r] + nums[l] > target) r--;
     else ans = (ans + pows[r - l++]) % mod;
   }
   
