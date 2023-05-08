a2f5b9d38cb99bc6677f1d068f6b21cfb14b6348
悠悠茶靡 <58740404+dreamjean@users.noreply.github.com>
2023-05-08 09:47:19 +0800

    Time: 50 ms (96.28%) | Memory: 44 MB (40.47%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.js
+++ matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -7,9 +7,8 @@ var diagonalSum = function(mat) {
   let sum = 0;
 
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (i === j || i + j === n - 1) sum += mat[i][j];
-    }
+    sum += mat[i][i] + mat[i][n - 1 - i];
+    if (i * 2 === n - 1) sum -= mat[i][i];
   }
 
   return sum;
